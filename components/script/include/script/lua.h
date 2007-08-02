d1df9c14ef9737aed9d9cd91172b7a7ea8f2b132
Leny Kholodov <leny.kholodov@gmail.com>
2007-08-02 12:59:51 +0000

    Ревизия Lua

---
+++ components/script/include/script/lua.h
@@ -0,0 +1,2 @@
+#include <script/lua/environment.h>
+
---
+++ components/script/sources/shared.h
@@ -0,0 +1,21 @@
+#include <script/lua/environment.h>
+
+extern "C"
+{
+  #include <lua.h>
+  #include <lualib.h>
+  #include <lauxlib.h>
+}
+
+namespace script
+{
+
+namespace lua
+{
+
+extern const char* USER_DATA_TAG;
+
+}
+
+}
+
