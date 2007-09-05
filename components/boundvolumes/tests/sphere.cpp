3e5298f693e72d41c64dd2c2d122cf0bb2c27527
Leny Kholodov <leny.kholodov@gmail.com>
2007-09-05 12:45:29 +0000

    Добавленны преобразования сферы

--- components/boundvolumes/tests/sphere.cpp
+++ components/boundvolumes/tests/sphere.cpp
@@ -7,6 +7,14 @@ void dump (const vec3f& v)
   printf ("[%g %g %g]", v.x, v.y, v.z);
 }
 
+void dump (const aaboxf& box)
+{
+  printf ("min=");
+  dump (box.minimum ());
+  printf (" max=");
+  dump (box.maximum ());
+}
+
 void dump (const spheref& sph)
 {
   printf ("center=");
@@ -18,6 +26,7 @@ void dump (const spheref& sph)
 void main()
 {
    spheref sph1,sph2(vec3f(10,10,10),20);
+   aaboxf box;
    printf("sphere created by default constructor\n");
    dump(sph1);
    printf("sphere created by vector and radius constructor\n");
@@ -37,4 +46,12 @@ void main()
    printf("!=%d\n",sph1!=sph2?true:false);
 
    printf("intersects=%d\n",sph1.intersects(sph2));
+   
+   box=sph1*translatef (10, 20, 30);
+   printf ("muliply on matrix\n");
+   dump (box);
+
+   box=sph1*fromAxisAnglef (deg2rad (90.0f), 1, 0, 0);
+   printf ("\nmultiply on quaternion\n");
+   dump (box);
 }
