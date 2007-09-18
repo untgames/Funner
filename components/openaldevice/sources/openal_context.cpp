2640ffd379ad7aa40ea1ac6740ec3060e5c057a2
Leny Kholodov <leny.kholodov@gmail.com>
2007-09-18 17:25:37 +0000

    Исправления в протоколировании OpenAL

--- components/openaldevice/sources/openal_context.cpp
+++ components/openaldevice/sources/openal_context.cpp
@@ -415,6 +415,11 @@ void OpenALContext::CheckErrors (const char* function_name, const Tuple& args)
 template <class Fn, class Tuple>
 void OpenALContext::Dispatch (const char* function_name, Fn fn, const Tuple& args)
 {
+  ALenum error = alGetError ();
+  
+  if (error != AL_NO_ERROR)
+    LogPrintf ("Error before call %s!!!\n", function_name);
+
   apply<void> (fn, args);
   CheckErrors (function_name, args);
 }
@@ -422,6 +427,11 @@ void OpenALContext::Dispatch (const char* function_name, Fn fn, const Tuple& arg
 template <class Ret, class Fn, class Tuple>
 Ret OpenALContext::Dispatch (const char* function_name, Fn fn, const Tuple& args)
 {
+  ALenum error = alGetError ();
+  
+  if (error != AL_NO_ERROR)
+    LogPrintf ("Error before call %s!!!\n", function_name);
+
   Ret result = apply<Ret> (fn, args);
 
   CheckErrors (function_name, args);
