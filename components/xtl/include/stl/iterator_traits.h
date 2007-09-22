1ae882cb733f9b25beb574617b3128fdf66a8f21
Leny Kholodov <leny.kholodov@gmail.com>
2007-09-22 11:08:13 +0000

    Изменены таблицы конфигурации итераторов

--- components/xtl/include/stl/iterator_traits.h
+++ components/xtl/include/stl/iterator_traits.h
@@ -20,6 +20,19 @@ struct int_iterator_tag {};
 struct float_iterator_tag {};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+///Описание итератора
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template <class Category, class T, class Distance=ptrdiff_t, class Pointer=T*, class Reference=T&>
+struct iterator 
+{
+  typedef Category  iterator_category;
+  typedef T         value_type;
+  typedef Distance  difference_type;
+  typedef Pointer   pointer;
+  typedef Reference reference;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 ///Таблица конфигурации итераторов
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 template <class Iterator> struct iterator_traits 
@@ -34,63 +47,28 @@ template <class Iterator> struct iterator_traits
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///Специализации конфигураций итераторов
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-template <class T, class Category>  struct iterator_traits_base
-{
-  typedef Category  iterator_category;
-  typedef T         value_type;
-  typedef ptrdiff_t difference_type;
-  typedef T*        pointer;
-  typedef T&        reference;
-};
+template <class T> struct iterator_traits<T*>:                public iterator<random_access_iterator_tag, T> {};
+template <class T> struct iterator_traits<const T*>:          public iterator<random_access_iterator_tag, T, ptrdiff_t, const T*, const T&> {};
+template <class T> struct iterator_traits<volatile T*>:       public iterator<random_access_iterator_tag, T, ptrdiff_t, volatile T*, volatile T&> {};
+template <class T> struct iterator_traits<const volatile T*>: public iterator<random_access_iterator_tag, T, ptrdiff_t, const volatile T*, const volatile T&> {};
 
-template <class T> struct iterator_traits<T*> 
-{
-  typedef random_access_iterator_tag iterator_category;
-  typedef T                          value_type;
-  typedef ptrdiff_t                  difference_type;
-  typedef T*                         pointer;
-  typedef T&                         reference;
-};
-
-template <class T> struct iterator_traits<const T*> 
-{
-  typedef random_access_iterator_tag iterator_category;
-  typedef T                          value_type;
-  typedef ptrdiff_t                  difference_type;
-  typedef const T*                   pointer;
-  typedef const T&                   reference;
-};
-
-template <> struct iterator_traits<bool>:           public iterator_traits_base<bool, int_iterator_tag> {};
-template <> struct iterator_traits<char>:           public iterator_traits_base<char, int_iterator_tag> {};
-template <> struct iterator_traits<signed char>:    public iterator_traits_base<signed char, int_iterator_tag> {};
-template <> struct iterator_traits<unsigned char>:  public iterator_traits_base<unsigned char, int_iterator_tag> {};
-template <> struct iterator_traits<short>:          public iterator_traits_base<short, int_iterator_tag> {};
-template <> struct iterator_traits<unsigned short>: public iterator_traits_base<unsigned short, int_iterator_tag> {};
-template <> struct iterator_traits<int>:            public iterator_traits_base<int, int_iterator_tag> {};
-template <> struct iterator_traits<unsigned int>:   public iterator_traits_base<unsigned int, int_iterator_tag> {};
-template <> struct iterator_traits<long>:           public iterator_traits_base<long, int_iterator_tag> {};
-template <> struct iterator_traits<unsigned long>:  public iterator_traits_base<unsigned long, int_iterator_tag> {};
-template <> struct iterator_traits<float>:          public iterator_traits_base<float, float_iterator_tag> {};
-template <> struct iterator_traits<double>:         public iterator_traits_base<double, float_iterator_tag> {};
-template <> struct iterator_traits<long double>:    public iterator_traits_base<long double, float_iterator_tag> {};
+template <> struct iterator_traits<bool>:           public iterator<int_iterator_tag, bool> {};
+template <> struct iterator_traits<char>:           public iterator<int_iterator_tag, char> {};
+template <> struct iterator_traits<signed char>:    public iterator<int_iterator_tag, signed char> {};
+template <> struct iterator_traits<unsigned char>:  public iterator<int_iterator_tag, unsigned char> {};
+template <> struct iterator_traits<short>:          public iterator<int_iterator_tag, short> {};
+template <> struct iterator_traits<unsigned short>: public iterator<int_iterator_tag, unsigned short> {};
+template <> struct iterator_traits<int>:            public iterator<int_iterator_tag, int> {};
+template <> struct iterator_traits<unsigned int>:   public iterator<int_iterator_tag, unsigned int> {};
+template <> struct iterator_traits<long>:           public iterator<int_iterator_tag, long> {};
+template <> struct iterator_traits<unsigned long>:  public iterator<int_iterator_tag, unsigned long> {};
+template <> struct iterator_traits<float>:          public iterator<float_iterator_tag, float> {};
+template <> struct iterator_traits<double>:         public iterator<float_iterator_tag, double> {};
+template <> struct iterator_traits<long double>:    public iterator<float_iterator_tag, long double> {};
 
 #ifndef NO_WCHAR
-template <> struct iterator_traits<wchar_t>: public iterator_traits_base<wchar_t, int_iterator_tag> {};
+template <> struct iterator_traits<wchar_t>: public iterator<int_iterator_tag, wchar_t> {};
 #endif
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-///Описание итератора
-///////////////////////////////////////////////////////////////////////////////////////////////////
-template <class Category, class T, class Distance=ptrdiff_t, class Pointer=T*, class Reference=T&>
-struct iterator 
-{
-  typedef Category  iterator_category;
-  typedef T         value_type;
-  typedef Distance  difference_type;
-  typedef Pointer   pointer;
-  typedef Reference reference;
-};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///Категория итератора
