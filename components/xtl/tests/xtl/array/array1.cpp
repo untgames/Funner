c6f9bc3767f49a4d4b32b3be6aec9cba9027d6b7
Leny Kholodov <leny.kholodov@gmail.com>
2007-10-20 12:14:20 +0000

    Добавлен тест tr1::array

---
+++ components/xtl/tests/xtl/array/array1.cpp
@@ -0,0 +1,45 @@
+#include "shared.h"
+
+int main()
+{
+    printf ("Results of array1_test:\n");
+
+    // define special type name
+    typedef boost::array<float,6> Array;
+
+    // create and initialize an array
+    Array a = { { 42 } };
+
+    // access elements
+    for (unsigned i=1; i<a.size(); ++i) {
+        a[i] = a[i-1]+1;
+    }
+
+    // use some common STL container operations
+    printf ("size:     %u\n", a.size());
+    printf ("empty:    %s\n", a.empty() ? "true" : "false");
+    printf ("max_size: %u\n", a.max_size());
+    printf ("front:    %g\n", a.front());
+    printf ("back:     %g\n", a.back());
+    printf ("elems:    ");
+
+    // iterate through all elements
+    for (Array::const_iterator pos=a.begin(); pos<a.end(); ++pos) {
+        printf ("%g ", *pos);
+    }
+
+    printf ("\n");
+
+    // check copy constructor and assignment operator
+    Array b(a);
+    Array c;
+    c = a;
+    if (a==b && a==c) {
+        printf ("copy construction and copy assignment are OK\n");
+    }
+    else {
+        printf ("copy construction and copy assignment FAILED\n");
+    }
+
+    return 0;
+}
---
+++ components/xtl/tests/xtl/array/shared.h
@@ -0,0 +1,5 @@
+#include <xtl/array>
+#include <stl/string>
+#include <stdio.h>
+
+using namespace xtl; //tr1!!!
