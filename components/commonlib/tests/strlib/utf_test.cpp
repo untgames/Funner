7960642175a7c12f870f67554f6c5d68bcba2443
Leny Kholodov <leny.kholodov@gmail.com>
2007-11-14 13:19:51 +0000

    добавленно преобразование win_t -> ... Работает криво

--- components/commonlib/tests/strlib/utf_test.cpp
+++ components/commonlib/tests/strlib/utf_test.cpp
@@ -4,13 +4,14 @@
 
 int main()
 {
-  char in[128]={0x7A,0x78,0x63,0xD0,0xB,0xD1,0x86,0xD1,0x83,0x0};
+  char in[128]={0xFF,0xFE,0x7A,0x00,0x78,0x00,0x63,0x00,0x39,0x04,0x46,0x04,0x43,0x04};
   wint_t out[128];
   FILE *f=fopen("zzz.txt","w+");
   memset(out,0,sizeof(out));
-  common::utf_decode(in,16,common::Encoding::Encoding_Utf8,(char*)out,128);
+  common::utf_decode(in,16,common::Encoding::Encoding_Utf16,(char*)out,128);
+  common::utf_encode(out,32,(char*)in,128,common::Encoding::Encoding_Utf8);
   for(int i=0;i<32;i++)
-     putwc(out[i],f);
+     putc(in[i],f);
   fclose(f);
   return 0;
 }
