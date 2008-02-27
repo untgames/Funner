f113ce90b72256fa842e68161107ed72d9c037ed
Leny Kholodov <leny.kholodov@gmail.com>
2008-02-27 16:58:51 +0000

    Тесты загрузки моделей

--- components/medialib/tests/collada-convert/collada_convert_geometry.cpp
+++ components/medialib/tests/collada-convert/collada_convert_geometry.cpp
@@ -2,8 +2,7 @@
 
 using namespace media::geometry;
 
-const char* COLLADA_FILE_NAME = "../collada/data/teapot1.dae";
-//const char* COLLADA_FILE_NAME = "../collada/data/torus.dae";
+const char* COLLADA_FILE_NAME = "../collada/data/ape.dae";
 const char* DST_FILE_NAME     = "/io/stdout/test.xmesh";
 
 void print (const char* message)
@@ -23,10 +22,10 @@ int main ()
     
     printf ("Converting...\n");
     
-    MeshLibrary mesh_library;
-    
+    MeshLibrary mesh_library;        
+       
     convert (collada_model, mesh_library);
-    
+
     printf ("Save XMesh library '%s'\n", DST_FILE_NAME);
     
     mesh_library.Save (DST_FILE_NAME);
