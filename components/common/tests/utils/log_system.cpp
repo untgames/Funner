8e088abf0e8df518620f78844b352dc06e0446d3
Ievgen Mukhin <muhineg@gmail.com>
2008-06-09 14:27:58 +0000

    Система логгирования. Тест автоматического удаления эмиттеров по окончании проигрывания.

---
+++ components/common/tests/utils/log_system.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include <xtl/function.h>
+#include <xtl/connection.h>
+
+#include <common/log.h>
+#include <common/exception.h>
+
+using namespace common;
+
+void critical_log_handler (const char* log_name, const char* message)
+{
+  printf ("New log message received in 'critical' log handler from log '%s': '%s'\n", log_name, message);
+}
+
+void all_log_handler (const char* log_name, const char* message)
+{
+  printf ("New log message received in 'all' log handler from log '%s': '%s'\n", log_name, message);
+}
+
+int main ()
+{
+  printf ("Results of log_system_test:\n");
+  
+  LogSystem::RegisterLogHandler ("*critical*", &critical_log_handler);
+  LogSystem::RegisterLogHandler ("*", &all_log_handler);
+  
+  LogSystem::Print ("file_system_critical", "Can't acquire file system access");
+  LogSystem::Printf ("critical_system_log", "System fail at %u::%u", 17, 15);
+  LogSystem::Printf ("trivial_log", "System start...");
+
+  return 0;
+}
---
+++ components/sound/scene_player/tests/emitter_auto_delete.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <xtl/intrusive_ptr.h>
+#include <xtl/function.h>
+#include <xtl/bind.h>
+
+#include <syslib/window.h>
+#include <syslib/timer.h>
+#include <syslib/application.h>
+
+#include <sg/node.h>
+#include <sg/scene.h>
+#include <sg/listener.h>
+#include <sg/sound_emitter.h>
+
+#include <sound/manager.h>
+#include <sound/scene_player.h>
+#include <sound/device.h>
+
+using namespace sound;
+using namespace sound::low_level;
+using namespace syslib;
+using namespace scene_graph;
+
+const char* library_file = "data/test.snddecl";
+
+const size_t TEST_WORK_TIME = 8000;  //время работы теста (в милисекундах)
+
+typedef scene_graph::Listener::Pointer     ListenerPtr;
+typedef scene_graph::SoundEmitter::Pointer SoundEmitterPtr;
+
+SoundEmitterPtr play_sound (const char* declaration_name, scene_graph::Scene& scene)
+{
+  SoundEmitterPtr sound_emitter = scene_graph::SoundEmitter::Create (declaration_name);
+
+  sound_emitter->BindToParent (scene.Root (), NodeBindMode_Capture);
+
+  sound_emitter->Play ();
+
+  return sound_emitter;
+}
+
+int main ()
+{
+  printf ("EmitterAutoDelete test.\n");
+
+  try
+  {
+    scene_graph::Scene scene;
+    Window             window;
+    SoundManager       manager (window, SoundSystem::FindConfiguration ("OpenAL", "*"));
+    ListenerPtr        listener (scene_graph::Listener::Create ());
+    
+    listener->BindToParent (scene.Root ());
+
+    manager.LoadSoundLibrary (library_file);
+
+    ScenePlayer scene_player;
+
+    scene_player.SetListener (listener.get ());
+
+    scene_player.SetManager (&manager);
+
+    ((Node*)play_sound ("declaration2", scene).get ())->RegisterEventHandler (NodeEvent_AfterDestroy, xtl::bind (&Application::Exit, 0));
+
+    Application::Run ();
+  }
+  catch (std::exception& exception)
+  {                                               
+    printf ("exception: %s\n",exception.what ()); 
+  }                                               
+  catch (...)
+  {
+    printf ("unknown exception\n");
+  }
+  
+  printf ("exit\n");
+
+  return Application::GetExitCode ();
+}
