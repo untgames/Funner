0e180310cb291b5c65f50ce08f7fac0d61df878a
Leny Kholodov <leny.kholodov@gmail.com>
2008-07-13 15:14:21 +0000

    Добавление управления мышью

--- components/demos/lua_sg/sources/script/sg.lua
+++ components/demos/lua_sg/sources/script/sg.lua
@@ -1,5 +1,9 @@
 velocity = {['+x']=0; ['-x']=0; ['+y']=0; ['-y']=0}
 
+velocity_x = 3
+velocity_y = 3
+mouse_sens = 5	
+
 root_node = Scene.Node.Create ()
 sprite    = Scene.Sprite.Create ()
 
@@ -13,13 +17,28 @@ function set_root_node (new_root_node)
 end
 
 function onBeginMove(ort)
-  velocity [ort] = velocity [ort] + 3
+  velocity [ort] = velocity [ort] + 1
 end
 
 function onEndMove(ort)
-  velocity [ort] = velocity [ort] - 3
+  velocity [ort] = velocity [ort] - 1
+end
+
+function onMove(dx, dy)
+  sprite:Translate (dx * velocity_x * mouse_sens, dy * velocity_y * mouse_sens, 0)
+end
+
+function sign (x)
+  if x < 0 then
+    return -1
+  elseif x > 0 then
+    return 1
+  else
+    return 0
+  end
 end
 
 function idle(dt)
-  sprite:Translate ((velocity ['+x'] - velocity ['-x'])*dt, (velocity ['+y'] - velocity ['-y'])*dt, 0)
+  sprite:Translate (sign (velocity ['+x'] - velocity ['-x']) * dt * velocity_x,
+                    sign (velocity ['+y'] - velocity ['-y']) * dt * velocity_y, 0)
 end
