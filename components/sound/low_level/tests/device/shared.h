9ca69345ea8a861eda589b59bc347179227634cc
Ievgen Mukhin <muhineg@gmail.com>
2008-07-16 10:20:17 +0000

    Забытые файлы предыдущего коммита.

---
+++ components/sound/low_level/tests/device/shared.h
@@ -0,0 +1,71 @@
+#ifndef SOUND_LOW_LEVEL_TESTS_SHARED_HEADER
+#define SOUND_LOW_LEVEL_TESTS_SHARED_HEADER
+
+#include <xtl/intrusive_ptr.h>
+#include <xtl/function.h>
+#include <xtl/reference_counter.h>
+#include <xtl/common_exceptions.h>
+
+#include <sound/driver.h>
+#include <sound/device.h>
+
+using namespace sound::low_level;
+
+//тестовый драйвер
+class TestDriver: virtual public IDriver, public xtl::reference_counter
+{
+  public:
+    TestDriver () {}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Описание драйвера
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    const char* GetDescription () { return "TestDriver"; }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Перечисление доступных устройств
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    size_t GetDevicesCount ()
+    {
+      throw xtl::make_not_implemented_exception ("TestDriver::GetDevicesCount");
+      return 0;
+    }
+    const char* GetDeviceName (size_t index)
+    {
+      throw xtl::make_not_implemented_exception ("TestDriver::GetDeviceName");
+      return 0;
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Создаение устройства
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    IDevice* CreateDevice (const char* name, const char* init_string) 
+    {
+      throw xtl::make_not_implemented_exception ("TestDriver::CreateDevice");
+      return 0;
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Установка функции отладочного протоколирования драйвера
+///////////////////////////////////////////////////////////////////////////////////////////////////   
+    void SetDebugLog (const LogHandler& in_log)
+    {
+      log_fn = in_log;
+    }
+    
+    const LogHandler& GetDebugLog ()
+    {
+      return log_fn;
+    }
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Подсчёт ссылок
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    void AddRef () { addref (this); }  
+    void Release () { release (this); }
+
+  private:
+    LogHandler log_fn;
+};
+
+#endif
