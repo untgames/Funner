1848b4fff737ccb22b5884f58e3f31f36cbeb32e
Ievgen Mukhin <muhineg@gmail.com>
2008-11-22 16:55:05 +0000

    Реализация listbox'ов в PropertyGrid.

--- components/tools/ui/windows_forms/sources/plugins/property_grid/property_grid.cs
+++ components/tools/ui/windows_forms/sources/plugins/property_grid/property_grid.cs
@@ -102,6 +102,7 @@ public class UIPropertyGridPlugin: IPlugin
     type_conversion_match = new System.Collections.Hashtable ();
 
     type_conversion_match.Add ("string",               "ToString");
+    type_conversion_match.Add ("list",                 "ToString");
     type_conversion_match.Add ("bool",                 "ToBool");
     type_conversion_match.Add ("int",                  "ToInt");
     type_conversion_match.Add ("float",                "ToFloat");
@@ -134,12 +135,7 @@ public class UIPropertyGridPlugin: IPlugin
   {
     assembly = null;
 
-    XmlNodeList property_maps_node_list = xml_configuration.SelectNodes ("PropertyMaps");
-
-    if (property_maps_node_list.Count != 1)
-      throw new System.Exception ("Invalid plugin 'PropertyGrid' configuration - must contain one 'PropertyMaps' sub-tag");
-
-    StringWriter generated_code = GenerateCode (property_maps_node_list[0]);
+    StringWriter generated_code = GenerateCode (xml_configuration);
 
 //    System.Console.WriteLine (generated_code.ToString ());
 
@@ -175,6 +171,7 @@ public class UIPropertyGridPlugin: IPlugin
     XmlNode title_node     = property.Attributes ["Title"];
     XmlNode type_node      = property.Attributes ["Type"];
     XmlNode read_only_node = property.Attributes ["ReadOnly"];
+    XmlNode list_box_node  = property.Attributes ["ListBox"];
 
     if (name_node == null)
       throw new System.Exception ("Invalid 'Property' node, must contain 'Name' attribute");
@@ -196,23 +193,43 @@ public class UIPropertyGridPlugin: IPlugin
       }
     }
 
+    if (type == "list" && list_box_node == null)
+      throw new System.Exception ("Property with type 'list' must contain 'ListBox' attribute");
+    
     writer.Write ("\tprivate System.String {0}_var_name;\n\n", name_node.InnerText);
 
     writer.Write ("\t[System.ComponentModel.Description (\"{0}\"),\n", (tip_node != null) ? tip_node.InnerText : "");
+    
+    if (type == "list")
+      writer.Write ("\tSystem.ComponentModel.TypeConverter (typeof ({0}Converter)),\n", list_box_node.InnerText);
+    
     writer.Write ("\t System.ComponentModel.DisplayName (\"{0}\"),\n", (title_node != null) ? title_node.InnerText : name_node.InnerText);
     writer.Write ("\t System.ComponentModel.Category    (\"{0}\")]\n", (category_node != null) ? category_node.InnerText : "Common");
 
-    writer.Write ("\tpublic {0} {1}\n\t{{\n", type, name_node.InnerText);
+    if (type == "list")
+      writer.Write ("\tpublic string {0}\n\t{{\n", name_node.InnerText);
+    else
+      writer.Write ("\tpublic {0} {1}\n\t{{\n", type, name_node.InnerText);
     
     writer.Write ("\t\tget\n\t\t{\n");
     writer.Write ("\t\t\tif (!application_server.IsPropertyPresent ({0}_var_name))\n", var_name_node.InnerText);
     writer.Write ("\t\t\t\tthrow new System.Exception (\"Variable '\" + {0}_var_name + \"' not found!\");\n", var_name_node.InnerText);
     writer.Write ("\t\t\t\tSystem.String return_value = null;\n");
     writer.Write ("\t\t\tapplication_server.GetProperty ({0}_var_name, ref return_value);\n", name_node.InnerText);
-    writer.Write ("\t\t\treturn Converter.{0} (return_value);\n\t\t}}\n", type_conversion_match[type]);
+
+    if (type == "list")
+      writer.Write ("\t\t\treturn List{0}Table.GetItemByValue (return_value);\n\t\t}}\n", list_box_node.InnerText);
+    else
+      writer.Write ("\t\t\treturn Converter.{0} (return_value);\n\t\t}}\n", type_conversion_match[type]);
 
     if ((read_only_node == null) || (read_only_node.InnerText == "0"))
-      writer.Write ("\t\tset\n\t\t{{\n\t\t\tapplication_server.SetProperty ({0}_var_name, Converter.ToString (value));\n\t\t}}\n", name_node.InnerText);
+    {
+      if (type == "list")
+        writer.Write ("\t\tset\n\t\t{{\n\t\t\tapplication_server.SetProperty ({0}_var_name, List{1}Table.GetValueByItem (value));\n\t\t}}\n", 
+                      name_node.InnerText, list_box_node.InnerText);
+      else
+        writer.Write ("\t\tset\n\t\t{{\n\t\t\tapplication_server.SetProperty ({0}_var_name, Converter.ToString (value));\n\t\t}}\n", name_node.InnerText);
+    }
 
     writer.Write ("\t}\n\n");
   }
@@ -225,8 +242,8 @@ public class UIPropertyGridPlugin: IPlugin
     if (name_node == null)
       throw new System.Exception ("Invalid 'PropertyMap' node, must contain 'Name' attribute");
 
-    if (inherit_node != null) writer.Write ("public class {0}: {1}, IPropertyListener\n{{\n", name_node.InnerText, inherit_node.InnerText);
-    else                      writer.Write ("public class {0}: IPropertyListener\n{{\n", name_node.InnerText);  
+    if (inherit_node != null) writer.Write ("public class {0}: {1}\n{{\n", name_node.InnerText, inherit_node.InnerText);
+    else                      writer.Write ("public class {0}\n{{\n", name_node.InnerText);  
 
     writer.Write ("\tprivate IApplicationServer application_server;\n\n");
     writer.Write ("\tpublic {0} (IApplicationServer in_application_server, System.String var_name_prefix)\n", name_node.InnerText);
@@ -249,9 +266,61 @@ public class UIPropertyGridPlugin: IPlugin
     
     writer.Write ("\t}\n\n");
 
-    writer.Write ("\tpublic new void OnAddProperty (System.String property_name)\n\t{\n\t}\n\n");
-    writer.Write ("\tpublic new void OnChangeProperty (System.String property_name)\n\t{\n\t}\n\n");
-    writer.Write ("\tpublic new void OnRemoveProperty (System.String property_name)\n\t{\n\t}\n\n");
+    writer.Write ("};\n\n");
+  }
+
+  private void ParseListBox (XmlNode list_box, StringWriter writer)
+  {
+    XmlNode name_node = list_box.Attributes ["Name"];
+
+    if (name_node == null)
+      throw new System.Exception ("Invalid 'ListBox' node, must contain 'Name' attribute");
+
+    writer.Write ("public class {0}Converter: System.ComponentModel.StringConverter\n", name_node.InnerText);
+    writer.Write ("{\n\tpublic override bool GetStandardValuesSupported (System.ComponentModel.ITypeDescriptorContext dummy)\n\t{\n\t\treturn true;\n\t}\n\n");
+    writer.Write ("\tpublic override System.ComponentModel.TypeConverter.StandardValuesCollection GetStandardValues (System.ComponentModel.ITypeDescriptorContext dummy)\n\t{\n");
+
+    XmlNodeList list = list_box.SelectNodes ("Item");
+    
+    writer.Write ("\t\tstring [] list_items = new string [{0}];\n", list.Count);
+
+    for (int i = 0; i < list.Count; i++)
+    {
+      XmlNode item_name_node  = list[i].Attributes ["Name"];
+      XmlNode item_value_node = list[i].Attributes ["Value"];
+
+      if (item_name_node == null || item_value_node == null)
+        throw new System.Exception ("Invalid 'ListBox' 'Item' sub-tag, must contain 'Name' and 'Value' attributes");
+
+      writer.Write ("\t\tlist_items[{0}] = \"{1}\";\n", i, item_name_node.InnerText);
+    }
+
+    writer.Write ("\t\treturn new System.ComponentModel.TypeConverter.StandardValuesCollection (list_items);\n\t}\n");
+
+    writer.Write ("};\n\n");
+
+    writer.Write ("class List{0}Table\n", name_node.InnerText);
+    
+    writer.Write ("{{\n\tstatic private ListBoxItem [] items = new ListBoxItem [{0}] {{", list.Count);
+    for (int i = 0; i < list.Count; i++)
+    {
+      writer.Write (" new ListBoxItem (\"{0}\", \"{1}\")", list[i].Attributes["Name"].InnerText, list[i].Attributes["Value"].InnerText);
+      if (i < (list.Count - 1))
+        writer.Write (",");
+    }
+    writer.Write (" };\n\n");
+
+    writer.Write ("\tstatic public string GetItemByValue (string value)\n\t{\n");
+    writer.Write ("\t\tfor (int i = 0; i < items.Length; i++)\n");
+    writer.Write ("\t\t{\n\t\t\tif (value == items[i].item_value) return items[i].item_name;\n\t\t}\n");
+    writer.Write ("\t\tthrow new System.Exception (\"ListBox '{0}' doesn't contain item with value '\" + value + \"'\");\n", name_node.InnerText);
+    writer.Write ("\t}\n\n");
+
+    writer.Write ("\tstatic public string GetValueByItem (string item)\n\t{\n");
+    writer.Write ("\t\tfor (int i = 0; i < items.Length; i++)\n");
+    writer.Write ("\t\t{\n\t\t\tif (item == items[i].item_name) return items[i].item_value;\n\t\t}\n");
+    writer.Write ("\t\tthrow new System.Exception (\"ListBox '{0}' doesn't contain item with name '\" + item + \"'\");\n", name_node.InnerText);
+    writer.Write ("\t}\n\n");
 
     writer.Write ("};\n\n");
   }
@@ -259,10 +328,19 @@ public class UIPropertyGridPlugin: IPlugin
   private StringWriter GenerateCode (XmlNode property_maps)
   {
     StringWriter string_writer = new StringWriter ();
-
-    XmlNodeList list = property_maps.SelectNodes ("PropertyMap");
     
     string_writer.Write ("namespace tools\n{\n\nnamespace ui\n{\n\nnamespace windows_forms\n{\n\nnamespace ui_property_grid_plugin\n{\n\n");
+
+    string_writer.Write ("struct ListBoxItem\n{\n\tpublic string item_name;\n\tpublic string item_value;\n\n");
+    string_writer.Write ("\tpublic ListBoxItem (string in_item_name, string in_item_value)\n\t{\n\t\titem_name = in_item_name;\n\t\titem_value = in_item_value;\n\t}\n");
+    string_writer.Write ("};\n\n");
+
+    XmlNodeList list = property_maps.SelectNodes ("ListBox");
+    
+    for (int i = 0; i < list.Count; i++)
+      ParseListBox (list [i], string_writer);
+
+    list = property_maps.SelectNodes ("PropertyMap");
     
     for (int i = 0; i < list.Count; i++)
       ParsePropertyMap (list [i], string_writer);
