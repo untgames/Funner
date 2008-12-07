1fc625df8b5d13de5e7f7fa03220a3a55fe5b930
Ievgen Mukhin <muhineg@gmail.com>
2008-12-07 18:15:06 +0000

    Тест обхода сцены после удаления узла.

---
+++ components/scene_graph/tests/core/scene_traverse2.cpp
@@ -0,0 +1,36 @@
+#include "shared.h"
+
+class MyVisitor: public xtl::visitor<void, TestEntity>
+{
+  public:
+    void visit (TestEntity& object)  { printf ("  test_entity '%s'\n", object.Name ()); }
+};
+
+int main ()
+{
+  printf ("Results of scene_traverse2_test:\n");
+
+  Scene scene;
+
+  TestEntity* entity1 = TestEntity::Create (), *entity2 = TestEntity::Create ();
+
+  entity1->SetName ("entity1");
+  entity1->SetBoundBox (aaboxf (-10.0f, 10.0f));
+  entity1->BindToScene (scene, NodeBindMode_Capture);
+
+  entity2->SetName ("entity2");
+  entity2->SetBoundBox (aaboxf (-10.0f, 10.0f));
+  entity2->BindToScene (scene, NodeBindMode_Capture);
+
+  MyVisitor visitor;
+
+  printf ("Visit two entities:\n");
+  scene.VisitEach (visitor);
+
+  entity1->Unbind ();
+
+  printf ("Visit one entity:\n");
+  scene.VisitEach (visitor);
+
+  return 0;
+}
