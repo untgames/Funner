9d0a0ed8c4f14452263a095028af6d2e1cfca2e7
Ievgen Mukhin <muhineg@gmail.com>
2008-12-08 18:17:43 +0000

    При конвертировании дробных чисел в строку и из строки в качестве разделителя теперь используется '.'

--- components/tools/ui/windows_forms/sources/plugins/type_converter/type_converter.cs
+++ components/tools/ui/windows_forms/sources/plugins/type_converter/type_converter.cs
@@ -1,3 +1,15 @@
+class NumberFormatInfoHolder
+{
+  public System.Globalization.NumberFormatInfo provider;
+  
+  public NumberFormatInfoHolder ()
+  {
+    provider = new System.Globalization.NumberFormatInfo ();
+    
+    provider.NumberDecimalSeparator = ".";
+  }
+};
+
 namespace tools
 {
 
@@ -28,12 +40,12 @@ public class Converter
 
   public static string ToString (float value)
   {
-    return System.Convert.ToString (value);
+    return System.Convert.ToString (value, number_format_provider.provider);
   }
 
   public static string ToString (double value)
   {
-    return System.Convert.ToString (value);
+    return System.Convert.ToString (value, number_format_provider.provider);
   }
 
   public static string ToString (System.DateTime value)
@@ -43,7 +55,9 @@ public class Converter
 
   public static string ToString (System.Drawing.Color value)
   {
-    return System.Convert.ToString (value.R / 255.0) + "; " + System.Convert.ToString (value.G / 255.0) + "; "  + System.Convert.ToString (value.B / 255.0);
+    return System.Convert.ToString (value.R / 255.0, number_format_provider.provider) + "; " + 
+           System.Convert.ToString (value.G / 255.0, number_format_provider.provider) + "; " +
+           System.Convert.ToString (value.B / 255.0, number_format_provider.provider);
   }
 
   public static bool ToBool (string value)
@@ -60,12 +74,12 @@ public class Converter
 
   public static float ToFloat (string value)
   {
-    return System.Convert.ToSingle (value);
+    return System.Convert.ToSingle (value, number_format_provider.provider);
   }
 
   public static double ToDouble (string value)
   {
-    return System.Convert.ToDouble (value);
+    return System.Convert.ToDouble (value, number_format_provider.provider);
   }
 
   public static System.DateTime ToDateTime (string value)
@@ -78,20 +92,21 @@ public class Converter
     string [] color_components = value.Split (color_separators, System.StringSplitOptions.RemoveEmptyEntries);
 
     if (color_components.Length == 3)
-      return System.Drawing.Color.FromArgb (255, (int)(System.Convert.ToSingle (color_components[0]) * 255.0), 
-                                            (int)(System.Convert.ToSingle (color_components[1]) * 255.0), 
-                                            (int)(System.Convert.ToSingle (color_components[2]) * 255.0));
+      return System.Drawing.Color.FromArgb (255, (int)(System.Convert.ToSingle (color_components[0], number_format_provider.provider) * 255.0), 
+                                            (int)(System.Convert.ToSingle (color_components[1], number_format_provider.provider) * 255.0), 
+                                            (int)(System.Convert.ToSingle (color_components[2], number_format_provider.provider) * 255.0));
 
     if (color_components.Length == 4)
-      return System.Drawing.Color.FromArgb ((int)(System.Convert.ToSingle (color_components[3]) * 255.0), 
-                                            (int)(System.Convert.ToSingle (color_components[0]) * 255.0), 
-                                            (int)(System.Convert.ToSingle (color_components[1]) * 255.0), 
-                                            (int)(System.Convert.ToSingle (color_components[2]) * 255.0));
+      return System.Drawing.Color.FromArgb ((int)(System.Convert.ToSingle (color_components[3], number_format_provider.provider) * 255.0), 
+                                            (int)(System.Convert.ToSingle (color_components[0], number_format_provider.provider) * 255.0), 
+                                            (int)(System.Convert.ToSingle (color_components[1], number_format_provider.provider) * 255.0), 
+                                            (int)(System.Convert.ToSingle (color_components[2], number_format_provider.provider) * 255.0));
 
     throw new System.Exception ("Invalid color format");
   }
 
-  private static System.Char [] color_separators = {';', ' '};
+  private static System.Char []         color_separators = {';', ' '};
+  private static NumberFormatInfoHolder number_format_provider = new NumberFormatInfoHolder ();
 };
 
 }
