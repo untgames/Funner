d5367d88ec12f4a174e3c017b21e444472961f97
Leny Kholodov <leny.kholodov@gmail.com>
2008-12-15 18:16:23 +0000

    Исправление неработающей ревизии

--- components/tools/ui/windows_forms/sources/plugins/property_grid/property_grid.cs
+++ components/tools/ui/windows_forms/sources/plugins/property_grid/property_grid.cs
@@ -27,6 +27,7 @@ class Constants
   public static System.String TypeConverterDllName ()
   {
     return "funner.tools.ui.windows_forms.TypeConverter.dll";
+//    return "funner.tools.ui.windows_forms.Helpers.dll";
   }
 };
 
