fd6755bd23cac4e09f0b6b4c8ebb7acb33d8eef8
Ievgen Mukhin <muhineg@gmail.com>
2009-05-12 12:42:47 +0000

    Добавлена функция проверки наличия файла.

--- components/engine/script_binds/sources/bind_common_file.cpp
+++ components/engine/script_binds/sources/bind_common_file.cpp
@@ -24,11 +24,11 @@ const char* COMMON_FILE_LIBRARY = "Common.File";
 stl::string load_string_filter_out (const char* file_name, const char* out_filter_string)
 {
   stl::string s, filter = common::compress (out_filter_string);
-  
+
   FileSystem::LoadTextFile (file_name, s);
-  
-  stl::string::size_type pos = 0;  
-  
+
+  stl::string::size_type pos = 0;
+
   while ((pos = s.find_first_of (filter, pos)) != stl::string::npos)
     s.erase (pos, 1);
 
@@ -46,6 +46,7 @@ void bind_common_file_library (Environment& environment)
 
   lib.Register ("LoadString",          make_invoker (implicit_cast<stl::string (*)(const char*)> (&FileSystem::LoadTextFile)));
   lib.Register ("LoadStringFilterOut", make_invoker (&load_string_filter_out));
+  lib.Register ("IsFileExist",         make_invoker (&FileSystem::IsFileExist));
 }
 
 }
--- components/engine/script_binds/tests/lua/data/common.lua
+++ components/engine/script_binds/tests/lua/data/common.lua
@@ -1,3 +1,6 @@
+local EXISTING_FILE     = "data/text_file.txt" 
+local NOT_EXISTING_FILE = "data/not_existing_file.txt" 
+
 function test_files()
   local data = Common.File.LoadString ("data/text_file.txt")
 
@@ -6,6 +9,18 @@ function test_files()
   data = Common.File.LoadStringFilterOut ("data/text_file1.txt", "\n\r")
 
   print ("Filtered file data: '" .. data .. "'")
+  
+  if Common.File.IsFileExist (EXISTING_FILE) then
+    print ("File '" .. EXISTING_FILE .. "' exist")
+  else
+    print ("File '" .. EXISTING_FILE .. "' not exist")
+  end
+  
+  if Common.File.IsFileExist (NOT_EXISTING_FILE) then
+    print ("File '" .. NOT_EXISTING_FILE .. "' exist")
+  else
+    print ("File '" .. NOT_EXISTING_FILE .. "' not exist")
+  end
 end
 
 function test ()
