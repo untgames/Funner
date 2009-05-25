983c8135fb587a6e26e5f364029b30fc52faf04d
Ievgen Mukhin <muhineg@gmail.com>
2009-05-25 20:11:44 +0000

    Модификация теста с тем, чтобы не удалялись библиотеки материлов.

--- components/media/rfx/tests/xmtl/crash_test.cpp
+++ components/media/rfx/tests/xmtl/crash_test.cpp
@@ -9,29 +9,25 @@ const char* SRC_LIBRARY3_FILE_NAME = "data/Story.xmtl";
 #include <xtl/iterator.h>
 
 typedef SpriteMaterial::Pointer            SpriteMaterialPtr;
-typedef stl::hash_map<stl::hash_key<const char*>, SpriteMaterialPtr> MaterialMap;
+typedef stl::hash_map<stl::hash_key<const char*>, int> MaterialMap;
 
 void log_handler (const char*)
 {
 }
 
-void load_library (const char* file_name, MaterialMap& map)
+void load_library (MaterialLibrary& library, MaterialMap& map)
 {
-  MaterialLibrary library (file_name, log_handler);
-
   for (media::rfx::MaterialLibrary::Iterator iter=library.CreateIterator (); iter; ++iter)
   {
     media::rfx::Material::Pointer material = *iter;
     media::rfx::SpriteMaterial*   sprite_material = dynamic_cast<media::rfx::SpriteMaterial*> (&*material);
 
-    map [library.ItemId (iter)] = sprite_material;
+    map [library.ItemId (iter)] = (int)sprite_material;
   }
 }
 
-void unload_library (const char* file_name, MaterialMap& map)
+void unload_library (MaterialLibrary& library, MaterialMap& map)
 {
-  MaterialLibrary library (file_name, log_handler);
-
   for (media::rfx::MaterialLibrary::Iterator iter=library.CreateIterator (); iter; ++iter)
   {
     media::rfx::Material::Pointer material = *iter;
@@ -49,10 +45,19 @@ int main ()
 
     MaterialMap materials;
 
-    load_library (SRC_LIBRARY1_FILE_NAME, materials);
-    load_library (SRC_LIBRARY2_FILE_NAME, materials);
-    unload_library (SRC_LIBRARY1_FILE_NAME, materials);
-    load_library (SRC_LIBRARY3_FILE_NAME, materials);
+    MaterialLibrary library (SRC_LIBRARY1_FILE_NAME, log_handler);
+    load_library (library, materials);
+
+    MaterialLibrary library2 (SRC_LIBRARY2_FILE_NAME, log_handler);
+    load_library (library2, materials);
+
+    MaterialLibrary library3 (SRC_LIBRARY1_FILE_NAME, log_handler);
+    unload_library (library3, materials);
+
+    MaterialLibrary library4 (SRC_LIBRARY3_FILE_NAME, log_handler);
+    load_library (library4, materials);
+
+    printf ("pudum\n");
   }
   catch (std::exception& exception)
   {
