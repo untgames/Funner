2965b73521cd8d02bfb0732f31c094c4e0993bcb
Ievgen Mukhin <muhineg@gmail.com>
2009-09-13 07:40:07 +0300

    Небольшая оптимизация для случая отсутствия alpha-clamped спрайтов.

--- components/render/mid_level_window_driver/sources/renderer2d/renderer2d_frame.cpp
+++ components/render/mid_level_window_driver/sources/renderer2d/renderer2d_frame.cpp
@@ -101,11 +101,11 @@ size_t Frame::PrimitivesCount ()
 //добавление примитива
 void Frame::AddPrimitive (IPrimitive* in_primitive)
 {
-  static const char* METHOD_NAME = "render::mid_level::window_driver::renderer2d::Frame::AddPrimitive";  
+  static const char* METHOD_NAME = "render::mid_level::window_driver::renderer2d::Frame::AddPrimitive";
 
   if (!in_primitive)
     throw xtl::make_null_argument_exception (METHOD_NAME, "primitive");
-    
+
      //приведение типа примитива
 
   Primitive* casted_primitive = dynamic_cast<Primitive*> (in_primitive);
@@ -113,7 +113,7 @@ void Frame::AddPrimitive (IPrimitive* in_primitive)
   if (!casted_primitive)
     throw xtl::make_argument_exception (METHOD_NAME, "primitive", typeid (in_primitive).name (),
       "Primitive type incompatible with render::mid_level::window_driver::renderer2d::Primitive");
-      
+
     //добавление примитива в список примитивов кадра (для захвата ресурсов примитива)
 
   primitives.push_back (casted_primitive);
@@ -144,10 +144,10 @@ bool not_blended_sprite_sort_predicate (const RenderableSprite* sprite1, const R
 }
 
 bool blended_sprite_sort_predicate (const RenderableSprite* sprite1, const RenderableSprite* sprite2)
-{  
+{
     //возможно добавить сортировку по блендингу и текстурам??
-    
-//  if (sprite1->vertices [0].position.z != sprite2->vertices [0].position.z)    
+
+//  if (sprite1->vertices [0].position.z != sprite2->vertices [0].position.z)
     return sprite1->vertices [0].position.z > sprite2->vertices [0].position.z;
 
      //сортировка нужна для визуализации изометрии, в будущем убрать в настройки
@@ -160,16 +160,16 @@ bool blended_sprite_sort_predicate (const RenderableSprite* sprite1, const Rende
 void Frame::UpdateVertexBuffers (render::low_level::IDevice& device)
 {
     //очистка списков вывода
-    
+
   not_blended_sprites.Clear ();
-  blended_sprites.Clear ();    
-  
+  blended_sprites.Clear ();
+
     //добавление спрайтов
-    
+
   for (PrimitiveArray::iterator iter=primitives.begin (), end=primitives.end (); iter!=end; ++iter)
   {
     Primitive& primitive = **iter;
-    
+
     switch (primitive.GetBlendMode ())
     {
       case BlendMode_None:
@@ -179,13 +179,13 @@ void Frame::UpdateVertexBuffers (render::low_level::IDevice& device)
       default:
         primitive.AddSprites (blended_sprites);
         break;
-    }    
+    }
   }
 
     //сортировка геометрии
 
   not_blended_sprites.Sort (not_blended_sprite_sort_predicate);
-  blended_sprites.Sort     (blended_sprite_sort_predicate);  
+  blended_sprites.Sort     (blended_sprite_sort_predicate);
 
     //обновление вершинных буферов
 
@@ -209,7 +209,7 @@ void draw_solid_sprites
 {
   for (const RenderableSprite** sprite=first_sprite; sprite != last_sprite;)
   {
-    render::low_level::ITexture* texture = (*sprite)->primitive->texture;    
+    render::low_level::ITexture* texture = (*sprite)->primitive->texture;
 
     size_t base_sprite_index = sprite++ - base_sprite;
 
@@ -232,7 +232,7 @@ void draw_alpha_clamped_sprites
   IBuffer&                 constant_buffer)
 {
   render::low_level::ITexture* current_texture         = device.SSGetTexture (0);
-  DynamicProgramParameters     dynamic_program_parameters; //динамические параметры программы рендеринга  
+  DynamicProgramParameters     dynamic_program_parameters; //динамические параметры программы рендеринга
 
   dynamic_program_parameters.alpha_reference = 0.0f;
 
@@ -246,7 +246,7 @@ void draw_alpha_clamped_sprites
     for (; sprite != last_sprite && (*sprite)->primitive->texture == texture && (*sprite)->primitive->alpha_reference == alpha_reference; sprite++);
 
     size_t sprites_count = sprite - base_sprite - base_sprite_index;
-    
+
     if (dynamic_program_parameters.alpha_reference != alpha_reference)
     {
       dynamic_program_parameters.alpha_reference = alpha_reference;
@@ -263,7 +263,7 @@ void draw_alpha_clamped_sprites
 
     device.DrawIndexed (render::low_level::PrimitiveType_TriangleList, base_sprite_index * SPRITE_INDICES_COUNT,
       sprites_count * SPRITE_INDICES_COUNT, 0);
-  }  
+  }
 }
 
 //рисование спрайтов с прозрачностью
@@ -280,8 +280,8 @@ void draw_blend_sprites
   for (const RenderableSprite** sprite=first_sprite; sprite != last_sprite;)
   {
     render::low_level::ITexture*             texture    = (*sprite)->primitive->texture;
-    render::mid_level::renderer2d::BlendMode blend_mode = (*sprite)->primitive->blend_mode;    
-    
+    render::mid_level::renderer2d::BlendMode blend_mode = (*sprite)->primitive->blend_mode;
+
     size_t base_sprite_index = sprite++ - base_sprite;
 
     for (; sprite != last_sprite && (*sprite)->primitive->texture == texture && (*sprite)->primitive->blend_mode == blend_mode; sprite++);
@@ -316,8 +316,8 @@ void Frame::DrawCore (IDevice* device)
     //установка области вывода
 
   BasicFrame::BindViewport (device);
-  
-    //обновление парметров шейдинга    
+
+    //обновление парметров шейдинга
 
   common_constant_buffer->SetData (0, sizeof (common_program_parameters), &common_program_parameters);
 
@@ -329,7 +329,7 @@ void Frame::DrawCore (IDevice* device)
 
   device->ISSetInputLayout             (common_resources->GetInputLayout ());
   device->SSSetConstantBuffer          (0, common_constant_buffer.get ());
-  device->SSSetConstantBuffer          (1, dynamic_constant_buffer.get ());  
+  device->SSSetConstantBuffer          (1, dynamic_constant_buffer.get ());
   device->SSSetProgramParametersLayout (common_resources->GetProgramParametersLayout ());
   device->SSSetSampler                 (0, common_resources->GetSamplerState ());
 
@@ -345,14 +345,17 @@ void Frame::DrawCore (IDevice* device)
                          **first_solid_sprite;
 
     //поиск первого спрайта без альфа-отсечения
-    
-  device->SSSetProgram (common_resources->GetAlphaClampProgram ());
 
   for (first_solid_sprite=first_sprite; first_solid_sprite != last_sprite && (*first_solid_sprite)->primitive->blend_mode == BlendMode_AlphaClamp; ++first_solid_sprite);
 
     //отрисовка спрайтов с альфа-отсечением
 
-  draw_alpha_clamped_sprites (*device, first_sprite, first_sprite, first_solid_sprite, *dynamic_constant_buffer);  
+  if (first_solid_sprite - first_sprite)
+  {
+    device->SSSetProgram (common_resources->GetAlphaClampProgram ());
+
+    draw_alpha_clamped_sprites (*device, first_sprite, first_sprite, first_solid_sprite, *dynamic_constant_buffer);
+  }
 
     //отрисовка спрайтов без альфа-отсечения
 
@@ -360,7 +363,7 @@ void Frame::DrawCore (IDevice* device)
 
   draw_solid_sprites (*device, first_sprite, first_solid_sprite, last_sprite);
 
-    //отрисовка спрайтов с блендингом    
+    //отрисовка спрайтов с блендингом
 
   device->ISSetVertexBuffer      (0, blended_sprites.GetVertexBuffer ());
   device->ISSetIndexBuffer       (blended_sprites.GetIndexBuffer ());
