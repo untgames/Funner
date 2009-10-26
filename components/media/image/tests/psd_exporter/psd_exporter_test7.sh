3acb596d716cb7addd36f59d48039e5cad565029
Leny Kholodov <leny.kholodov@gmail.com>
2009-10-26 14:21:28 +0000

    Добавление конфигурирования имени каталога с изображениями в файле разметки

---
+++ components/media/image/tests/psd_exporter/psd_exporter_test7.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+echo Results of psd_exporter_test7:
+psd-exporter data/test.psd --silent --layers-dir=my_layers --layout-layers-dir=test --layers-format=my_image_%04u.tga -o my.xlayermap
+cat my.xlayermap
+rm -rf my_layers/* my_layers  my.xlayermap
