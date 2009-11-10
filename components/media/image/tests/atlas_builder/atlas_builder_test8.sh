58455cef5a069d2941ee162c2e7352b9dc8ac0c3
Ievgen Mukhin <muhineg@gmail.com>
2009-11-10 15:33:35 +0000

    Обновление тестов.

--- components/media/image/tests/atlas_builder/atlas_builder_test8.sh
+++ components/media/image/tests/atlas_builder/atlas_builder_test8.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 echo Results of atlas_builder_test5:
 mkdir results
-atlas-builder data/1.bmp --atlas='results/swap-axises.png' data/2.tga data/1.bmp data/3.jpg --swap-axises || exit 0
+atlas-builder --atlas='results/swap-axises.png' data/1.bmp data/3.jpg data/2.tga --swap-axises --pot || exit 0
 echo 'results/swap-axises.xatlas'
 cat 'results/swap-axises.xatlas'
