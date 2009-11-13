da8a10bdcb0366d4ecdbe5647696d491da1e85d8
Ievgen Mukhin <muhineg@gmail.com>
2009-11-13 21:52:52 +0000

    Обновление jpeg до версии 7.

--- extern/jpeg/include/jconfig.h
+++ extern/jpeg/include/jconfig.h
@@ -1,5 +1,5 @@
 /* jconfig.vc --- jconfig.h for Microsoft Visual C++ on Windows 95 or NT. */
-/* see jconfig.doc for explanations */
+/* see jconfig.txt for explanations */
 
 #define HAVE_PROTOTYPES
 #define HAVE_UNSIGNED_CHAR
@@ -8,26 +8,19 @@
 /* #define const */
 #undef CHAR_IS_UNSIGNED
 #define HAVE_STDDEF_H
-
-#ifndef HAVE_STDLIB_H
-  #define HAVE_STDLIB_H
-#endif
-
+#define HAVE_STDLIB_H
 #undef NEED_BSD_STRINGS
 #undef NEED_SYS_TYPES_H
-#undef NEED_FAR_POINTERS  /* we presume a 32-bit flat memory model */
+#undef NEED_FAR_POINTERS	/* we presume a 32-bit flat memory model */
 #undef NEED_SHORT_EXTERNAL_NAMES
 #undef INCOMPLETE_TYPES_BROKEN
 
-#ifndef _TIFFIOP_
-
 /* Define "boolean" as unsigned char, not int, per Windows custom */
-#ifndef __RPCNDR_H__    /* don't conflict if rpcndr.h already read */
+#ifndef __RPCNDR_H__		/* don't conflict if rpcndr.h already read */
 typedef unsigned char boolean;
 #endif
-#define HAVE_BOOLEAN    /* prevent jmorecfg.h from redefining it */
+#define HAVE_BOOLEAN		/* prevent jmorecfg.h from redefining it */
 
-#endif
 
 #ifdef JPEG_INTERNALS
 
@@ -37,13 +30,13 @@ typedef unsigned char boolean;
 
 #ifdef JPEG_CJPEG_DJPEG
 
-#define BMP_SUPPORTED   /* BMP image file format */
-#define GIF_SUPPORTED   /* GIF image file format */
-#define PPM_SUPPORTED   /* PBMPLUS PPM/PGM image file format */
-#undef RLE_SUPPORTED    /* Utah RLE image file format */
-#define TARGA_SUPPORTED   /* Targa image file format */
+#define BMP_SUPPORTED		/* BMP image file format */
+#define GIF_SUPPORTED		/* GIF image file format */
+#define PPM_SUPPORTED		/* PBMPLUS PPM/PGM image file format */
+#undef RLE_SUPPORTED		/* Utah RLE image file format */
+#define TARGA_SUPPORTED		/* Targa image file format */
 
-#define TWO_FILE_COMMANDLINE  /* optional */
+#define TWO_FILE_COMMANDLINE	/* optional */
 
 #ifdef MSC_VER
 #define USE_SETMODE   /* Microsoft has setmode() */
@@ -51,6 +44,6 @@ typedef unsigned char boolean;
 
 #undef NEED_SIGNAL_CATCHER
 #undef DONT_USE_B_MODE
-#undef PROGRESS_REPORT    /* optional */
+#undef PROGRESS_REPORT		/* optional */
 
 #endif /* JPEG_CJPEG_DJPEG */
