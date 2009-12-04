395eadc1edd697ef38ecaef291031a38c01f8045
Leny Kholodov <leny.kholodov@gmail.com>
2009-12-04 14:30:05 +0000

    Первый вариант редактора

---
+++ components/tools/ui/coolwork/sources/builders.framework/PropertyGrid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace builders.framework
+{
+  /// <summary>
+  /// View for edit properties of selected objects
+  /// </summary>
+  public partial class PropertyGrid : DockContent
+  {
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public PropertyGrid ()
+    {
+      InitializeComponent ();
+    }
+  }
+}
