03cd7409b5c5ae43c52037526539241754ebef6e
Ievgen Mukhin <muhineg@gmail.com>
2009-12-27 20:11:57 +0000

    Исправление определения размера пикселя.

--- components/media/font/utils/image2xfont/image2xfont.cpp
+++ components/media/font/utils/image2xfont/image2xfont.cpp
@@ -19,7 +19,7 @@ size_t GetBpp (PixelFormat pf)
   switch (pf)
   {
     case PixelFormat_RGB8:   return 3;
-    case PixelFormat_RGB16:  return 4;
+    case PixelFormat_RGB16:  return 6;
     case PixelFormat_BGR8:   return 3;
     case PixelFormat_RGBA8:  return 4;
     case PixelFormat_RGBA16: return 8;
@@ -84,60 +84,55 @@ int main (int argc, char *argv[])
     
     font.Rename            (xfont_file_name.c_str ());
     font.SetImageName      (argv[1]);
-    font.SetFirstGlyphCode (0);
+    font.SetFirstGlyphCode (32);
 
     mask [0] = 0;
 
-    for (size_t i=0; i<img.Height (); i++)
-    {
-      for (size_t j=0; j<img.Width (); j++)
-      {
-        if (mask [i * img.Width () + j])
-          if (!memcmp (border_color, &border_color[(i * img.Width () + j ) * bpp], bpp))
+    for (size_t i = 0; i < img.Height (); i++)
+      for (size_t j = 0; j < img.Width (); j++)
+        if (mask [i * img.Width () + j] && !memcmp (border_color, &border_color[(i * img.Width () + j ) * bpp], bpp))
+        {
+          size_t width = 0, height = 0;
+
+          for (; !memcmp(border_color, &border_color[(i * img.Width () + j + width) * bpp], bpp) && (j + width < img.Width ()); width++);
+          for (; !memcmp(border_color, &border_color[((i + height) * img.Width () + j) * bpp], bpp) && (i + height < img.Height ()); height++);
+
+          size_t width2 = 0, height2 = 0;
+
+          for (; !memcmp(border_color, &border_color[((i + height - 1) * img.Width () + j + width2) * bpp], bpp) && (j + width2 < img.Width ()); width2++);
+          for (; !memcmp(border_color, &border_color[((i + height2) * img.Width () + j + width - 1) * bpp], bpp) && (i + height2 < img.Height ()); height2++);
+
+          if (width != width2 || height != height2)
           {
-            size_t width = 0, height = 0;
-
-            for (; !memcmp(border_color, &border_color[(i * img.Width () + j + width) * bpp], bpp)  && (j + width < img.Width ()); width++);
-            for (; !memcmp(border_color, &border_color[((i + height) * img.Width () + j) * bpp], bpp) && (i + height < img.Height ()); height++);
-
-            size_t width2 = 0, height2 = 0;
-
-            for (; !memcmp(border_color, &border_color[((i + height - 1) * img.Width () + j + width2) * bpp], bpp) && (j + width2 < img.Width ()); width2++);
-            for (; !memcmp(border_color, &border_color[((i + height2) * img.Width () + j + width - 1) * bpp], bpp) && (i + height2 < img.Height ()); height2++);
-
-            if (width != width2 || height != height2)
-            {
-              printf ("incorrect image format, not square bbox detected\n");
-              continue;
-            }
-
-            if (width < 3 || height < 3)
-            {
-              printf ("incorrect image format, to small bbox detected\n");
-              continue;
-            }
-
-            for (size_t m = 0; m < height; m++)
-              for (size_t n = 0; n < width; n++)
-                mask [(i + m) * img.Width () + j + n] = 0;
-                
-            GlyphInfo glyph_info;
-
-            memset (&glyph_info, 0, sizeof (glyph_info));
-
-            glyph_info.x_pos  = j + 1;
-            glyph_info.y_pos  = i + 1;
-            glyph_info.width  = width - 2;
-            glyph_info.height = height - 2;
-            glyph_info.bearing_y = glyph_info.height;
-            glyph_info.advance_x = glyph_info.width;
-
-            glyphs_buffer.push_back (glyph_info);
-
-            j += width - 1;
+            printf ("incorrect image format, not square bbox detected\n");
+            continue;
           }
-      }
-    }
+
+          if (width < 3 || height < 3)
+          {
+            printf ("incorrect image format, to small bbox detected\n");
+            continue;
+          }
+
+          for (size_t m = 0; m < height; m++)
+            for (size_t n = 0; n < width; n++)
+              mask [(i + m) * img.Width () + j + n] = 0;
+
+          GlyphInfo glyph_info;
+
+          memset (&glyph_info, 0, sizeof (glyph_info));
+
+          glyph_info.x_pos  = j + 1;
+          glyph_info.y_pos  = i + 1;
+          glyph_info.width  = width - 2;
+          glyph_info.height = height - 2;
+          glyph_info.bearing_y = glyph_info.height;
+          glyph_info.advance_x = glyph_info.width;
+
+          glyphs_buffer.push_back (glyph_info);
+
+          j += width - 1;
+        }
 
     font.ResizeGlyphsTable (glyphs_buffer.size ());
 
