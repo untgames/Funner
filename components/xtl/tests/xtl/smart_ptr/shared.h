9e2dd7d910515c2b0fd8e44e8dd8bd18cc51c309
Leny Kholodov <leny.kholodov@gmail.com>
2010-02-19 12:14:01 +0000

    Добавление lock_ptr

--- components/xtl/tests/xtl/smart_ptr/shared.h
+++ components/xtl/tests/xtl/smart_ptr/shared.h
@@ -1,5 +1,7 @@
-#include <xtl/stride_ptr.h>
 #include <stdio.h>
+#include <xtl/lock_ptr.h>
+#include <xtl/stride_ptr.h>
+#include <xtl/intrusive_ptr.h>
 
 #define TEST(X) printf ((X)?"expression '%s' is true\n":"failed expression '%s'\n",#X);
 
