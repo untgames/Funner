a833061c3c6938b277f7e6a66d7cefae9a2a42e1
Leny Kholodov <leny.kholodov@gmail.com>
2010-03-06 21:35:40 +0000

    Реструктуризация

--- components/system/sources/platform/bada/startup/test.bat
+++
@@ -1,3 +0,0 @@
-@set BADA_STDOUT_FILE_NAME=D:\work\UNT\funner\components\system\sources\platform\bada\startup\test.txt
-@copy  D:\work\unt\funner\tmp\bada-simulator\SYSTEMLIB.TESTS\tests\wrappers\application_exit.exe %BADA_SDK_HOME%\Rsrc\bada_WVGA\Win32FS\Osp\Applications\93bt1p123e\Bin\firstapp.exe
-@%BADA_SDK_HOME%\Lib\WinSgpp\simulator --app-id=93bt1p123e --app-file=firstapp.exe -s %BADA_SDK_HOME%\Lib\WinSgpp\PhoneShell.dll -d S8000_Generic.dbi
