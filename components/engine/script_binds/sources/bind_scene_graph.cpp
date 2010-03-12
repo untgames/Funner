3c90f99414b69aed5786776982e6d1f137dcb73e
Leny Kholodov <leny.kholodov@gmail.com>
2010-03-12 18:34:24 +0000

    Адаптация к изменениям в интерфейсе контроллера воды

--- components/engine/script_binds/sources/bind_scene_graph.cpp
+++ components/engine/script_binds/sources/bind_scene_graph.cpp
@@ -1109,9 +1109,17 @@ void bind_controller_water_library (Environment& environment)
     
   lib.Register ("set_Viscosity", make_invoker (&Water::SetViscosity));
   lib.Register ("get_Viscosity", make_invoker (&Water::Viscosity));
-  lib.Register ("PutStorm",      make_invoker (make_invoker (&Water::PutStorm), make_invoker<void (Water&, const math::vec3f&)> (xtl::bind (&Water::PutStorm, _1, _2, 0.05f))));
-  lib.Register ("PutWorldStorm", make_invoker (make_invoker (&Water::PutWorldStorm), make_invoker<void (Water&, const math::vec3f&)> (xtl::bind (&Water::PutWorldStorm, _1, _2, 0.05f))));  
-  
+  lib.Register ("PutStorm",      make_invoker (
+                                   make_invoker (&Water::PutStorm), 
+                                   make_invoker<void (Water&, const math::vec3f&, float)> (xtl::bind (&Water::PutStorm, _1, _2, _3, 0.05f)),
+                                   make_invoker<void (Water&, const math::vec3f&)> (xtl::bind (&Water::PutStorm, _1, _2, 0.0005f, 0.05f))
+  ));
+  lib.Register ("PutWorldStorm",   make_invoker (
+                                   make_invoker (&Water::PutWorldStorm), 
+                                   make_invoker<void (Water&, const math::vec3f&, float)> (xtl::bind (&Water::PutWorldStorm, _1, _2, _3, 0.05f)),
+                                   make_invoker<void (Water&, const math::vec3f&)> (xtl::bind (&Water::PutWorldStorm, _1, _2, 0.0005f, 0.05f))
+  ));
+
     //регистрация типа данных
 
   environment.RegisterType<Water> (SCENE_CONTROLLER_WATER_LIBRARY);
