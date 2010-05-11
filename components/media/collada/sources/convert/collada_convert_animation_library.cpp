978b2b5838d2528fc6489615e0ad2f20a1380878
Ievgen Mukhin <muhineg@gmail.com>
2010-05-11 19:26:57 +0000

    Коррекция конвертера для анимаций.

--- components/media/collada/sources/convert/collada_convert_animation_library.cpp
+++ components/media/collada/sources/convert/collada_convert_animation_library.cpp
@@ -12,7 +12,7 @@ namespace
 class Converter
 {
   private:
-    void ConvertAnimationChannel (const AnimationChannel& collada_channel, media::animation::Animation& parent_animation, bool as_animation)
+    void ConvertAnimationChannel (const AnimationChannel& collada_channel, media::animation::Animation& parent_animation)
     {
       media::animation::Channel channel;
 
@@ -35,57 +35,34 @@ class Converter
           throw xtl::format_operation_exception ("::convert_animation_channel", "Unsupported collada animation channel semantic");
       }
 
-      if (as_animation)
-      {
-        media::animation::Animation sub_animation;
-
-        sub_animation.SetTargetName (collada_channel.TargetName ());
-
-        sub_animation.AddChannel (channel);
-
-        parent_animation.AddSubAnimation (sub_animation);
-      }
-      else
-        parent_animation.AddChannel (channel);
+      parent_animation.AddChannel (collada_channel.TargetName (), channel);
     }
 
-    void ConvertAnimation (const Animation& collada_animation, media::animation::Animation& animation)
+    void ConvertAnimation (const Animation& collada_animation, media::animation::Animation& animation, bool sub_animation)
     {
-      animation.Rename (collada_animation.Id ());
+      if (!sub_animation)
+        animation.Rename (collada_animation.Id ());
 
       const Animation::AnimationChannelList& channels = collada_animation.Channels ();
 
-      if (!channels.IsEmpty ())
-      {
-        const char* target_name = channels [0].TargetName ();
-
-        animation.SetTargetName (target_name);
-
-        for (Animation::AnimationChannelList::ConstIterator channel_iter = channels.CreateIterator (); channel_iter; ++channel_iter)
-          ConvertAnimationChannel (*channel_iter, animation, xtl::xstrcmp (target_name, channel_iter->TargetName ()) != 0);
-      }
+      for (Animation::AnimationChannelList::ConstIterator channel_iter = channels.CreateIterator (); channel_iter; ++channel_iter)
+        ConvertAnimationChannel (*channel_iter, animation);
 
       for (AnimationList::ConstIterator sub_animation_iter = collada_animation.Animations ().CreateIterator (); sub_animation_iter; ++sub_animation_iter)
-      {
-        media::animation::Animation sub_animation;
-
-        ConvertAnimation (*sub_animation_iter, sub_animation);
-
-        animation.AddSubAnimation (sub_animation);
-      }
+        ConvertAnimation (*sub_animation_iter, animation, true);
     }
 
     void AddAnimationToLibrary (const Animation& collada_animation, media::animation::AnimationLibrary& library)
     {
       media::animation::Animation animation;
 
-      ConvertAnimation (collada_animation, animation);
+      ConvertAnimation (collada_animation, animation, false);
 
       stl::string id (collada_animation.Id ());
 
       //копируем имя объекта в id анимации, если при экспорте id не был задан
-      if (id.empty ())
-        id = animation.TargetName ();
+      if (id.empty () && animation.TargetsCount ())
+        id = animation.TargetName (0);
 
       //если для одного объекта существует несколько анимаций, добавляем символ '_' чтобы не затереть предыдущие
       while (library.Find (id.c_str ()))
