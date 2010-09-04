06a5cdd5619568705319df5b64c851244d5cbb8f
Leny Kholodov <leny.kholodov@gmail.com>
2010-09-04 20:13:56 +0000

    Win32 condition error checks

--- components/system/sources/platform/win32/thread/win32_condition.cpp
+++ components/system/sources/platform/win32/thread/win32_condition.cpp
@@ -29,44 +29,68 @@ typedef struct
   // allows us to optimize the code if we're just signaling.
 } pthread_cond_t;
 
-void
-pthread_cond_init (pthread_cond_t *cv,
-                   const void *)
+void pthread_cond_init (pthread_cond_t *cv, const void *)
 {
   cv->waiters_count_ = 0;
   cv->was_broadcast_ = 0;
+
   cv->sema_ = CreateSemaphore (NULL,       // no security
                                 0,          // initially 0
                                 0x7fffffff, // max count
                                 NULL);      // unnamed 
+                                
+  if (!cv->sema_)
+    raise_error ("::CreateSemaphore");
+    
   InitializeCriticalSection (&cv->waiters_count_lock_);
+  
   cv->waiters_done_ = CreateEvent (NULL,  // no security
                                    FALSE, // auto-reset
                                    FALSE, // non-signaled initially
                                    NULL); // unnamed
+                                   
+  if (!cv->waiters_done_)
+  {
+    DeleteCriticalSection (&cv->waiters_count_lock_);
+    CloseHandle (cv->sema_);
+    
+    raise_error ("::CreateEvent");
+  }
 }
 
-void
-pthread_cond_destroy (pthread_cond_t* cv)
+void pthread_cond_destroy (pthread_cond_t* cv)
 {
   CloseHandle (cv->waiters_done_);
   CloseHandle (cv->sema_);  
   DeleteCriticalSection (&cv->waiters_count_lock_);
 }
 
-void
-pthread_cond_wait (pthread_cond_t *cv, 
-                   pthread_mutex_t *external_mutex)
+void pthread_cond_wait (pthread_cond_t *cv, pthread_mutex_t *external_mutex)
 {
   // Avoid race conditions.
+
   EnterCriticalSection (&cv->waiters_count_lock_);
+  
   cv->waiters_count_++;
+  
   LeaveCriticalSection (&cv->waiters_count_lock_);
 
   // This call atomically releases the mutex and waits on the
   // semaphore until <pthread_cond_signal> or <pthread_cond_broadcast>
   // are called by another thread.
-  SignalObjectAndWait (*external_mutex, cv->sema_, INFINITE, FALSE);
+  
+  DWORD result = SignalObjectAndWait (*external_mutex, cv->sema_, INFINITE, FALSE);
+  
+  if (result != WAIT_OBJECT_0)
+  {
+    EnterCriticalSection (&cv->waiters_count_lock_);
+    
+    cv->waiters_count_--;
+    
+    LeaveCriticalSection (&cv->waiters_count_lock_);
+
+    raise_error ("::SignalObjectAndWait#1");
+  }
 
   // Reacquire lock to avoid race conditions.
   EnterCriticalSection (&cv->waiters_count_lock_);
@@ -82,33 +106,47 @@ pthread_cond_wait (pthread_cond_t *cv,
   // If we're the last waiter thread during this particular broadcast
   // then let all the other threads proceed.
   if (last_waiter)
+  {
     // This call atomically signals the <waiters_done_> event and waits until
     // it can acquire the <external_mutex>.  This is required to ensure fairness. 
-    SignalObjectAndWait (cv->waiters_done_, *external_mutex, INFINITE, FALSE);
+    DWORD result = SignalObjectAndWait (cv->waiters_done_, *external_mutex, INFINITE, FALSE);
+    
+    if (result != WAIT_OBJECT_0)
+      raise_error ("::SignalObjectAndWait#2");
+  }
   else
+  {
     // Always regain the external mutex since that's the guarantee we
     // give to our callers. 
-    WaitForSingleObject (*external_mutex, 0);
+    DWORD result = WaitForSingleObject (*external_mutex, 0);
+    
+    if (result != WAIT_OBJECT_0)
+      raise_error ("::WaitForSingleObject");
+  }
 }
 
-void
-pthread_cond_signal (pthread_cond_t *cv)
+void pthread_cond_signal (pthread_cond_t *cv)
 {
   EnterCriticalSection (&cv->waiters_count_lock_);
+  
   int have_waiters = cv->waiters_count_ > 0;
+  
   LeaveCriticalSection (&cv->waiters_count_lock_);
 
   // If there aren't any waiters, then this is a no-op.  
   if (have_waiters)
-    ReleaseSemaphore (cv->sema_, 1, 0);
+  {
+    if (!ReleaseSemaphore (cv->sema_, 1, 0))
+      raise_error ("::ReleaseSemaphore");
+  }
 }
 
-void
-pthread_cond_broadcast (pthread_cond_t *cv)
+void pthread_cond_broadcast (pthread_cond_t *cv)
 {
   // This is needed to ensure that <waiters_count_> and <was_broadcast_> are
   // consistent relative to each other.
   EnterCriticalSection (&cv->waiters_count_lock_);
+  
   int have_waiters = 0;
 
   if (cv->waiters_count_ > 0) {
@@ -116,24 +154,34 @@ pthread_cond_broadcast (pthread_cond_t *cv)
     // Record that we are broadcasting, which helps optimize
     // <pthread_cond_wait> for the non-broadcast case.
     cv->was_broadcast_ = 1;
-    have_waiters = 1;
+    have_waiters       = 1;
   }
 
   if (have_waiters) {
     // Wake up all the waiters atomically.
-    ReleaseSemaphore (cv->sema_, cv->waiters_count_, 0);
+    BOOL result = ReleaseSemaphore (cv->sema_, cv->waiters_count_, 0);
 
     LeaveCriticalSection (&cv->waiters_count_lock_);
+    
+    if (!result)
+      raise_error ("::ReleaseSemaphore");
 
     // Wait for all the awakened threads to acquire the counting
     // semaphore. 
-    WaitForSingleObject (cv->waiters_done_, INFINITE);
+    
+    DWORD wait_result = WaitForSingleObject (cv->waiters_done_, INFINITE);
+    
     // This assignment is okay, even without the <waiters_count_lock_> held 
     // because no other waiter threads can wake up to access it.
     cv->was_broadcast_ = 0;
+    
+    if (wait_result != WAIT_OBJECT_0)
+      raise_error ("::WaitForSingleObject");
   }
   else
+  {
     LeaveCriticalSection (&cv->waiters_count_lock_);
+  }
 }
 
 }
