50dcae1643edecb6ffebc6c28842d85fd1bcd5af
Leny Kholodov <leny.kholodov@gmail.com>
2010-11-07 21:25:02 +0000

    Работа над очередью событий для X11

--- components/system/sources/platform/x11/shared.h
+++ components/system/sources/platform/x11/shared.h
@@ -4,12 +4,108 @@
 #include <X11/Xatom.h>
 #include <X11/keysym.h>
 
+#ifdef DestroyAll
+#undef DestroyAll
+#endif
+
+#include <stl/hash_set>
+#include <stl/queue>
+
 #include <xtl/common_exceptions.h>
+#include <xtl/intrusive_ptr.h>
 #include <xtl/reference_counter.h>
 #include <xtl/string.h>
 
 #include <common/property_map.h>
+#include <common/singleton.h>
 #include <common/strlib.h>
 #include <common/utf_converter.h>
 
+#include <syslib/mutex.h>
+#include <syslib/condition.h>
+
 #include <platform/platform.h>
+
+namespace syslib
+{
+
+namespace x11
+{
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Очередь сообщений приложения
+///////////////////////////////////////////////////////////////////////////////////////////////////
+class MessageQueue
+{
+  public:
+    typedef void* handler_t;
+      
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Интерфейс сообщения
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    class Message: public xtl::reference_counter
+    {
+      friend class MessageQueue;
+      public:
+        Message () : handler (0) {}
+      
+        virtual ~Message () {}      
+      
+        virtual void Dispatch () = 0;
+        
+      private:
+        handler_t handler;
+    };
+    
+    typedef xtl::intrusive_ptr<Message> MessagePtr;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Конструктор / деструктор
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    MessageQueue  ();
+    ~MessageQueue ();
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Регистрация доступных дескрипторов обработчиков
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    void RegisterHandler   (handler_t);
+    void UnregisterHandler (handler_t);
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Помещение сообщения в очередь
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    void PushMessage      (handler_t, const MessagePtr& message);
+    void PushEmptyMessage (); //используется для пробуждения нити обработки сообщений
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Диспетчеризация первого доступного сообщения его обработчику
+///////////////////////////////////////////////////////////////////////////////////////////////////    
+    void DispatchFirstMessage ();
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///Проверка количества сообщений в очереди / ожидание сообщений
+///////////////////////////////////////////////////////////////////////////////////////////////////    
+    bool IsEmpty     () const;
+    void WaitMessage ();
+    
+  private:
+    MessageQueue (const MessageQueue&); //no impl
+    MessageQueue& operator = (const MessageQueue&); //no impl
+    
+  private:
+    typedef stl::queue<MessagePtr>   MessageQueueImpl;
+    typedef stl::hash_set<handler_t> HandlerSet;
+
+  private:
+    Mutex            mutex;
+    Condition        condition;
+    MessageQueueImpl messages;
+    HandlerSet       handlers;
+};
+
+typedef common::Singleton<MessageQueue> MessageQueueSingleton;
+
+}
+
+}
+
--- components/system/sources/platform/x11/x11_application.cpp
+++ components/system/sources/platform/x11/x11_application.cpp
@@ -1,6 +1,7 @@
 #include "shared.h"
 
 using namespace syslib;
+using namespace syslib::x11;
 
 /*
     Работа с очередью сообщений
@@ -14,30 +15,74 @@ class X11ApplicationDelegate: public IApplicationDelegate, public xtl::reference
   public:
 ///Конструктор
     X11ApplicationDelegate ()
+      : message_queue (*MessageQueueSingleton::Instance ())
     {
       idle_enabled = false;
       is_exited    = false;
       listener     = 0;
+            
+      message_queue.RegisterHandler (this);
+    }
+    
+///Деструктор
+    ~X11ApplicationDelegate ()
+    {
+      try
+      {
+        message_queue.UnregisterHandler (this);
+      }
+      catch (...)
+      {
+      }
     }
 
 ///Запуск цикла обработки сообщений
     void Run ()
     {
-      if (listener)
-        listener->OnInitialized ();
-      
-      while (!is_exited)
+      try
       {
-        if (idle_enabled && listener)
-          listener->OnIdle ();
+        if (listener)
+          listener->OnInitialized ();
+        
+        while (!is_exited)
+        {
+          while (!IsMessageQueueEmpty ())
+            DoNextEvent ();
+
+           //если нет обработчиков OnIdle - приостанавливаем приложение
+
+          if (!idle_enabled)
+          {
+            if (IsMessageQueueEmpty () && !is_exited)
+            {
+              WaitMessage ();
+            }
+          }
+          else
+          {
+            if (listener)
+              listener->OnIdle ();
+          }
+        }
+      }
+      catch (xtl::exception& e)
+      {
+        e.touch ("syslib::X11ApplicationDelegate::Run");
+        throw;
       }
     }
 
 ///Выход из приложения
     void Exit (int code)
     {
+      message_queue.PushMessage (this, MessageQueue::MessagePtr (new ExitMessage (*this, code), false));
+    }
+    
+///Обработка события выхода из приложения
+    void OnExit (int code)
+    {
       is_exited = true;
-
+      
       if (listener)
         listener->OnExit (code);
     }
@@ -46,6 +91,11 @@ class X11ApplicationDelegate: public IApplicationDelegate, public xtl::reference
     void SetIdleState (bool state)
     {
       idle_enabled = state;
+      
+      if (state)
+      {
+        message_queue.PushEmptyMessage ();
+      }      
     }
 
 ///Установка слушателя событий приложения
@@ -63,12 +113,65 @@ class X11ApplicationDelegate: public IApplicationDelegate, public xtl::reference
     void Release ()
     {
       release (this);
+    }
+
+  private:
+///Отложенное события выхода из приложения
+    struct ExitMessage: public MessageQueue::Message
+    {
+      ExitMessage (X11ApplicationDelegate& in_delegate, int in_code) : delegate (in_delegate), code (in_code) {}
+      
+      void Dispatch ()
+      {
+        delegate.OnExit (code);
+      }
+      
+      X11ApplicationDelegate& delegate;
+      int                     code;
+    };
+    
+///Проверка очереди событий на пустоту
+    bool IsMessageQueueEmpty ()
+    {
+      if (is_exited)
+        return true;
+        
+      return message_queue.IsEmpty ();
+    }
+    
+///Ожидание события
+    void WaitMessage ()
+    {
+      try
+      {
+        message_queue.WaitMessage ();
+      }
+      catch (xtl::exception& exception)
+      {
+        exception.touch ("syslib::X11ApplicationDelegate::WaitMessage");
+        throw;
+      }
+    }
+    
+///Обработка следующего события
+    void DoNextEvent ()
+    {
+      try
+      {
+        message_queue.DispatchFirstMessage ();
+      }
+      catch (xtl::exception& exception)
+      {
+        exception.touch ("syslib::X11ApplicationDelegate::DoNextEvent");
+        throw;
+      }
     }    
 
   private:
     bool                  idle_enabled;
     bool                  is_exited;
     IApplicationListener* listener;
+    MessageQueue&         message_queue;
 };
 
 }
