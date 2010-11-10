3d675ff772f9ee813717183cd1512de89dba5a3a
Leny Kholodov <leny.kholodov@gmail.com>
2010-11-10 23:14:29 +0000

    Get time on Linux

--- components/common/sources/platform/unistd/unistd_time.cpp
+++ components/common/sources/platform/unistd/unistd_time.cpp
@@ -8,7 +8,7 @@ size_t UnistdPlatform::GetMilliseconds ()
 {
   timespec time;
   
-  if (!clock_gettime (CLOCK_REALTIME, &time))
+  if (clock_gettime (CLOCK_REALTIME, &time) != 0)
     return 0;
     
   return size_t (time.tv_sec * 1000.0 + time.tv_nsec / 1000000.0);
