09e85d92dc55c7e36ea86b0cad4920626d2d0183
Leny Kholodov <leny.kholodov@gmail.com>
2010-11-14 14:37:37 +0000

    Fix timer compilation bug for non BeagleBoard

--- components/common/sources/platform/unistd/unistd_time.cpp
+++ components/common/sources/platform/unistd/unistd_time.cpp
@@ -6,10 +6,15 @@ using namespace common;
 
 size_t UnistdPlatform::GetMilliseconds ()
 {
+#ifdef BEAGLEBOARD
+  return DefaultPlatform::GetMilliseconds ();  
+
+#else
   timespec time;
   
   if (clock_gettime (CLOCK_REALTIME, &time) != 0)
     return 0;
     
   return size_t (time.tv_sec * 1000.0 + time.tv_nsec / 1000000.0);
+#endif
 }
