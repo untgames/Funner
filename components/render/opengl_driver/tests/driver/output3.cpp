d44e876a96cb00ba07bc94404db14161b073b76d
Leny Kholodov <leny.kholodov@gmail.com>
2010-12-20 17:12:04 +0000

    Correct output test 3

--- components/render/opengl_driver/tests/driver/output3.cpp
+++ components/render/opengl_driver/tests/driver/output3.cpp
@@ -18,50 +18,48 @@ int main ()
     printf ("  description:   '%s'\n", driver->GetDescription ());
     printf ("  adapters count: %u\n", driver->GetAdaptersCount ());
 
-    for (size_t i=0; i<driver->GetAdaptersCount (); i++)
+    if (driver->GetAdaptersCount () == 0)
     {
-      IAdapter*      adapter    = driver->GetAdapter (i);
-      IPropertyList* properties = adapter->GetProperties ();
-
-      printf ("  adapter #%u:\n", i);
-      printf ("    name:        '%s'\n", adapter->GetName ());
-      printf ("    path:        '%s'\n", adapter->GetPath ());
-      printf ("    description: '%s'\n", adapter->GetDescription ());
-      
-      for (size_t j=0; j<properties->GetSize (); j++)
-        printf ("    %s: '%s'\n", properties->GetKey (j), properties->GetValue (j));
-
-      printf ("    outputs count: %u\n", adapter->GetOutputsCount ());
-
-      for (size_t j=0; j<adapter->GetOutputsCount (); j++)
-      {
-        IOutput*       output     = adapter->GetOutput (j);
-        IPropertyList* properties = output->GetProperties ();
-
-        printf ("    output #%u:\n", j);
-        printf ("      name: '%s'\n", output->GetName ());
-
-        for (size_t j=0; j<properties->GetSize (); j++)
-          printf ("      %s: '%s'\n", properties->GetKey (j), properties->GetValue (j));
-
-        printf ("    modes count: %u\n", output->GetModesCount ());
-
-        OutputModeDesc mode_desc;
-
-        for (size_t k = 0; k < output->GetModesCount (); k++)
-        {
-          output->GetModeDesc (k, mode_desc);
-
-          printf ("       mode #%u:\n", k);
-          printf ("         width:        %u\n", mode_desc.width);
-          printf ("         height:       %u\n", mode_desc.height);
-          printf ("         color bits:   %u\n", mode_desc.color_bits);
-          printf ("         refresh rate: %u\n", mode_desc.refresh_rate);
-          
-          output->SetCurrentMode (mode_desc);
-        }
-      }
+      printf ("Adapter not found\n");
+      return 0;
     }
+        
+    IAdapter*      adapter    = driver->GetAdapter (0);
+    IPropertyList* properties = adapter->GetProperties ();
+
+    printf ("Adapter [name: '%s'\t", adapter->GetName ());
+    printf ("path: '%s'\t", adapter->GetPath ());
+    printf ("description: '%s']\n", adapter->GetDescription ());
+    
+    for (size_t i=0; i<properties->GetSize (); i++)
+      printf ("    %s: '%s'\n", properties->GetKey (i), properties->GetValue (i));
+
+    printf ("    outputs count: %u\n", adapter->GetOutputsCount ());
+
+    for (size_t j=0; j<adapter->GetOutputsCount (); j++)
+    {
+      IOutput*       output     = adapter->GetOutput (j);
+      IPropertyList* properties = output->GetProperties ();
+
+      printf ("    output #%u:", j);
+      printf (" name: '%s'\t", output->GetName ());
+
+      for (size_t j=0; j<properties->GetSize (); j++)
+        printf ("      %s: '%s'\n", properties->GetKey (j), properties->GetValue (j));
+
+      printf ("    modes count: %u\n", output->GetModesCount ());
+
+      OutputModeDesc mode_desc;
+
+      for (size_t k = 0; k < output->GetModesCount (); k++)
+      {
+        output->GetModeDesc (k, mode_desc);
+
+        printf ("       mode #%u: %dx%dx%d@%d\n", k, mode_desc.width, mode_desc.height, 
+                                                     mode_desc.color_bits, mode_desc.refresh_rate);
+        output->SetCurrentMode (mode_desc);
+      }
+    }    
   }
   catch (std::exception& exception)
   {
