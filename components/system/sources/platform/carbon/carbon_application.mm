2c55c35fa0ef55c84bc19ed80cd8c11e6ee7c103
Ievgen Mukhin <muhineg@gmail.com>
2011-04-22 12:35:04 +0000

    OpenURL под mac

--- components/system/sources/platform/carbon/carbon_application.mm
+++ components/system/sources/platform/carbon/carbon_application.mm
@@ -200,3 +200,21 @@ IApplicationDelegate* Platform::CreateDefaultApplicationDelegate ()
 {
   return new CarbonApplicationDelegate;
 }
+
+/*
+   Открытие URL во внешнем браузере
+*/
+
+void Platform::OpenUrl (const char* url)
+{
+  NSAutoreleasePool *pool          = [[NSAutoreleasePool alloc] init];
+  NSString          *ns_url_string = [NSString stringWithUTF8String:url];
+  NSURL             *ns_url        = [NSURL URLWithString:[ns_url_string stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+
+  BOOL result = [[NSWorkspace sharedWorkspace] openURL:ns_url];
+
+  [pool release];
+
+  if (!result)
+    throw xtl::format_operation_exception ("::NSWorkspace::openURL", "Can't open URL '%s'", url);
+}
