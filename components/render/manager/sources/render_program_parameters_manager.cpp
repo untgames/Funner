6440e24d95076054637500c9dd8128c884ccd322
Leny Kholodov <leny.kholodov@gmail.com>
2011-04-28 10:28:05 +0000

    ProgramParametersManager update

--- components/render/manager/sources/render_program_parameters_manager.cpp
+++ components/render/manager/sources/render_program_parameters_manager.cpp
@@ -7,195 +7,42 @@ using namespace render::low_level;
     Описание реализации менеджера параметров программ шэйдинга
 */
 
-typedef stl::hash_map<size_t, LowLevelProgramParametersLayoutPtr> LayoutMap;
-typedef stl::hash_map<size_t, LowLevelBufferPtr>                  BufferMap;
+typedef stl::hash_multimap<size_t, ProgramParametersLayout*> LayoutMap;
+typedef stl::hash_map<size_t, ProgramParametersLayout*>      PairLayoutMap;
 
 struct ProgramParametersManager::Impl: public xtl::trackable
 {
-  LowLevelDevicePtr device;  //устройство визуализации
-  LayoutMap         layouts; //лэйауты параметров различной конфигурации
-  BufferMap         buffers; //константные буферы
-  
+  LowLevelDevicePtr device;       //устройство визуализации
+  LayoutMap         layouts;      //лэйауты параметров различной конфигурации
+  PairLayoutMap     pair_layouts; //парные лэйауты параметров различной конфигурации  
+
 ///Конструктор
   Impl (const LowLevelDevicePtr& in_device) : device (in_device) {}
-  
+
 ///Деструктор
   ~Impl ()
   {
-      //TODO: проверить рекурсивный вызов RemoveLayout/RemoveConstantBuffer
-    
     layouts.clear ();
-    buffers.clear ();
+    pair_layouts.clear ();
   }
-  
-///Получение лэйаута
-  LowLevelProgramParametersLayoutPtr GetLayout (const common::PropertyMap* properties [ProgramParametersSlot_Num])
-  {
-    try
-    {
-        //расчёт хэша конфигурации параметров
-      
-      size_t hash = 0xffffffff;
-      
-      for (size_t i=0; i<ProgramParametersSlot_Num; i++)
-      {
-        if (!properties [i])
-          throw xtl::make_null_argument_exception ("", common::format ("parameters[%u]", i).c_str ());
-          
-        size_t parameters_hash = properties [i]->LayoutHash ();
-          
-        hash = common::crc32 (&parameters_hash, sizeof (parameters_hash), hash);
-      }
-      
-        //поиск существующего лэйаута
-        
-      LayoutMap::iterator iter = layouts.find (hash);
-      
-      if (iter != layouts.end ())
-        return iter->second;
-        
-        //резервирование числа параметров
-      
-      size_t parameters_count = 0;
-      
-      for (size_t i=0; i<ProgramParametersSlot_Num; i++)
-        parameters_count += properties [i]->Size ();
 
-      stl::vector<ProgramParameter> parameters;
-      
-      parameters.reserve (parameters_count);
-      
-        //преобразование параметров
-        
-      for (size_t i=0; i<ProgramParametersSlot_Num; i++)
-      {
-        const common::PropertyLayout& layout   = properties [i]->Layout ();
-        const common::PropertyDesc*   property = layout.Properties ();
-        
-        for (size_t j=0, count=layout.Size (); j<count; j++, property++)
-        {
-          ProgramParameter parameter;
-          
-          memset (&parameter, 0, sizeof (parameter));
-          
-          parameter.name   = property->name;          
-          parameter.slot   = i;
-          parameter.count  = property->elements_count;
-          parameter.offset = property->offset;
-          
-          switch (property->type)
-          {            
-            case common::PropertyType_String:
-              continue; //строки не поддерживаются рендером
-            case common::PropertyType_Integer:
-              parameter.type = ProgramParameterType_Int;
-              break;
-            case common::PropertyType_Float:
-              parameter.type = ProgramParameterType_Float;
-              break;            
-            case common::PropertyType_Vector:
-              parameter.type = ProgramParameterType_Float4;
-              break;            
-            case common::PropertyType_Matrix:
-              parameter.type = ProgramParameterType_Float4x4;
-              break;            
-            default:
-              throw xtl::format_operation_exception ("", "Unexpected property[%u] type %s at parsing", j, get_name (property->type));
-          }
-          
-          parameters.push_back (parameter);
-        }
-      }
-      
-        //создание нового лэйаута
-        
-      ProgramParametersLayoutDesc desc;
-      
-      memset (&desc, 0, sizeof (desc));
-
-      desc.parameters_count = parameters.size ();
-      desc.parameters       = parameters.empty () ? (ProgramParameter*)0 : &parameters [0];
-      
-      LowLevelProgramParametersLayoutPtr layout (device->CreateProgramParametersLayout (desc), false);      
-      
-        //регистрация обработчиков события удаления лэйаута / параметров
-        
-      xtl::trackable::function_type remover = xtl::bind (&Impl::RemoveLayout, this, hash);
-        
-      layout->RegisterDestroyHandler (remover, *this);
-      
-      for (size_t i=0; i<ProgramParametersSlot_Num; i++)
-      {
-        const common::PropertyLayout& layout = properties [i]->Layout ();
-        
-        layout.Trackable ().connect_tracker (remover, *this);
-      }
-      
-        //регистрация лэйаута
-      
-      layouts.insert_pair (hash, layout);
-      
-      return layout;      
-    }
-    catch (xtl::exception& e)
-    {
-      e.touch ("render::ProgramParametersManager::Impl::GetLayout");
-      throw;
-    }
-  }
-  
 ///Удаление лэйаута
-  void RemoveLayout (size_t hash)
+  void RemoveLayout (size_t hash, ProgramParametersLayout* layout)
   {
-    layouts.erase (hash);
-  }
-  
-///Получение константного буфера
-  LowLevelBufferPtr GetConstantBuffer (const common::PropertyMap& properties)
-  {
-    try
-    {
-        //поиск существующего буфера
-      
-      BufferMap::iterator iter = buffers.find (properties.Id ());
-      
-      if (iter != buffers.end ())
-        return iter->second;
-        
-        //создание нового буфера
-        
-      BufferDesc desc;
-      
-      memset (&desc, 0, sizeof (desc));
-      
-      desc.size         = properties.Layout ().BufferSize ();
-      desc.usage_mode   = UsageMode_Default;
-      desc.bind_flags   = BindFlag_ConstantBuffer;
-      desc.access_flags = AccessFlag_ReadWrite;
-      
-      LowLevelBufferPtr buffer (device->CreateBuffer (desc), false);
-      
-        //регистрация обработчика удаления
-        
-      buffer->RegisterDestroyHandler (xtl::bind (&Impl::RemoveConstantBuffer, this, properties.Id ()), *this);
-      
-        //регистрация буфера
+    stl::pair<LayoutMap::iterator, LayoutMap::iterator> range = layouts.equal_range (hash);
+    
+    for (; range.first!=range.second; ++range.first)
+      if (range.first->second == layout)
+      {
+        layouts.erase (range.first);
 
-      buffers.insert_pair (properties.Id (), buffer);
-      
-      return buffer;
-    }
-    catch (xtl::exception& e)
-    {
-      e.touch ("render::ProgramParametersManager::Impl::GetConstantBuffer");
-      throw;
-    }  
-  }
+        return;        
+      }
+  }  
   
-///Удаление константного буфера
-  void RemoveConstantBuffer (size_t id)
+  void RemovePairLayout (size_t hash)
   {
-    buffers.erase (id);
+    pair_layouts.erase (hash);
   }
 };
 
@@ -213,28 +60,63 @@ ProgramParametersManager::~ProgramParametersManager ()
 }
 
 /*
-    Преобразование параметров
+    Получение объекта параметров
 */
 
-void ProgramParametersManager::Convert
- (const common::PropertyMap*          in_parameters [ProgramParametersSlot_Num],
-  LowLevelProgramParametersLayoutPtr& out_layout,
-  LowLevelBufferPtr                   out_constant_buffers [ProgramParametersSlot_Num])
+ProgramParametersLayoutPtr ProgramParametersManager::GetParameters (ProgramParametersSlot slot, const common::PropertyLayout& layout)
 {
   try
-  {
-      //получение лэйаута
-     
-    out_layout = impl->GetLayout (in_parameters);
+  {        
+    stl::pair<LayoutMap::iterator, LayoutMap::iterator> range = impl->layouts.equal_range (layout.Hash ());    
     
-      //получение константных буферов
+    for (size_t parameters_count=layout.Size (); range.first!=range.second; ++range.first)
+      if (range.first->second->Parameters ().parameters_count == parameters_count)
+        return range.first->second;
+
+    ProgramParametersLayoutPtr result_layout (new ProgramParametersLayout (impl->device), false);
+
+    result_layout->SetSlot (slot, layout);
+
+    result_layout->connect_tracker (xtl::bind (&Impl::RemoveLayout, &*impl, layout.Hash (), &*result_layout), *impl);
     
-    for (size_t i=0; i<ProgramParametersSlot_Num; i++)
-      out_constant_buffers [i] = impl->GetConstantBuffer (*in_parameters [i]);
+    impl->layouts.insert_pair (layout.Hash (), result_layout.get ());
+
+    return result_layout;
   }
   catch (xtl::exception& e)
   {
-    e.touch ("render::ProgramParametersManager::Convert");
+    e.touch ("render::ProgramParametersManager::GetParameters(const common::PropertyLayout&)");
+    throw;
+  }
+}
+
+ProgramParametersLayoutPtr ProgramParametersManager::GetParameters (const ProgramParametersLayout& layout1, const ProgramParametersLayout& layout2)
+{
+  try
+  {
+    const ProgramParametersLayout *layout1_ptr = &layout1, *layout2_ptr = &layout2;
+
+    size_t hash = common::crc32 (&layout1_ptr, sizeof (layout1_ptr), common::crc32 (&layout2_ptr, sizeof (layout2_ptr)));
+    
+    PairLayoutMap::iterator iter = impl->pair_layouts.find (hash);
+    
+    if (iter != impl->pair_layouts.end ())
+      return iter->second;
+    
+    ProgramParametersLayoutPtr result_layout (new ProgramParametersLayout (impl->device), false);
+
+    result_layout->Attach (layout1);
+    result_layout->Attach (layout2);
+
+    result_layout->connect_tracker (xtl::bind (&Impl::RemovePairLayout, &*impl, hash), *impl);
+    
+    impl->pair_layouts.insert_pair (hash, result_layout.get ());
+
+    return result_layout;    
+  }
+  catch (xtl::exception& e)
+  {
+    e.touch ("render::ProgramParametersManager::GetParameters(const common::ProgramParametersLayout&,const ProgramParametersLayout&)");
     throw;
   }
 }
