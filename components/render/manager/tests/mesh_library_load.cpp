1b61871db62e1481bf4bfb300353897eead60d3a
Leny Kholodov <leny.kholodov@gmail.com>
2011-05-05 15:52:21 +0000

    Mesh library load

---
+++ components/render/manager/tests/mesh_library_load.cpp
@@ -0,0 +1,27 @@
+#include "shared.h"
+
+const char* LIBRARY_NAME = "data/test.xmesh";
+
+int main ()
+{
+  printf ("Results of mesh_library_load_test:\n");
+  
+  try
+  {
+    Test test (L"Mesh library test", false);
+
+    RenderManager render_manager = test.RenderManager ();
+
+    render_manager.LoadResource (LIBRARY_NAME);
+    
+    Primitive primitive = render_manager.CreateSharedPrimitive ("mesh2");
+    
+    printf ("Mesh library loaded (%u meshes in primitive 'mesh2')\n", primitive.MeshesCount ());
+  }
+  catch (std::exception& e)
+  {
+    printf ("%s\n", e.what ());
+  }
+
+  return 0;
+}
