01f1f471846b58ccfa41a6dc360dd96c0ae3c514
Leny Kholodov <leny.kholodov@gmail.com>
2011-05-25 13:28:28 +0000

    GetFolderPath для wince

--- components/common/sources/platform/win32/shared.h
+++ components/common/sources/platform/win32/shared.h
@@ -1,8 +1,11 @@
 #ifndef COMMONLIB_PLATFORM_WIN32_SHARED_HEADER
 #define COMMONLIB_PLATFORM_WIN32_SHARED_HEADER
 
-#define _WIN32_WINNT 0x0600
-#define _WIN32_IE 0x0400
+#ifndef _WIN32_WCE
+  #define _WIN32_WINNT 0x0600
+  #define _WIN32_IE 0x0400
+#endif
+
 #include <windows.h>
 #include <shlobj.h>
 
@@ -13,6 +16,10 @@
 #include <common/strlib.h>
 
 #include <platform/platform.h>
+
+#if defined(WINCE)
+   #include "common\utf_converter.h"
+#endif
 
 namespace common
 {
--- components/common/sources/platform/win32/win32_mount_paths.cpp
+++ components/common/sources/platform/win32/win32_mount_paths.cpp
@@ -7,6 +7,7 @@ namespace
 
 stl::string GetFolderPath (int csidl)
 {
+#if !defined(WINCE)
   stl::string result;
   
   result.fast_resize (MAX_PATH);
@@ -14,7 +15,17 @@ stl::string GetFolderPath (int csidl)
   if (!SHGetSpecialFolderPathA (0, &result [0], csidl, FALSE))
     raise_error ("::SHGetSpecialFolderPath");
 
-  return result;      
+  return result;
+#else
+  stl::wstring result;
+  
+  result.fast_resize (MAX_PATH);
+  
+  if (!SHGetSpecialFolderPath (0, (LPTSTR)&result [0], csidl, FALSE))
+    raise_error ("::SHGetSpecialFolderPath");
+
+  return tostring(result);
+#endif      
 }
 
 stl::string GetTempDirPath ()
@@ -34,7 +45,12 @@ void Win32Platform::MountSystemSpecificPaths ()
 {
   FileSystem::Mount ("/system/appdata",   GetFolderPath (CSIDL_APPDATA).c_str ());
   FileSystem::Mount ("/system/personal",  GetFolderPath (CSIDL_PERSONAL).c_str ());  
+#if !defined(WINCE)
   FileSystem::Mount ("/system/profile",   GetFolderPath (CSIDL_PROFILE).c_str ()); 
   FileSystem::Mount ("/system/inetcache", GetFolderPath (CSIDL_INTERNET_CACHE).c_str ());
+#else
+  FileSystem::Mount ("/system/profile",   "\Documents and Settings\Default"); 
+  FileSystem::Mount ("/system/inetcache", GetTempDirPath ().c_str ());
+#endif
   FileSystem::Mount ("/system/temp",      GetTempDirPath ().c_str ());
 }
