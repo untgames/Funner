9b8260f3ba7ac914d80f2e1602af2462d0e9fc63
Leny Kholodov <leny.kholodov@gmail.com>
2011-06-14 13:50:50 +0000

    Decompression update

--- components/render/opengl_driver/sources/texture_manager/nv_dxt/color.h
+++ components/render/opengl_driver/sources/texture_manager/nv_dxt/color.h
@@ -1,95 +1,166 @@
 // This code is in the public domain -- castanyo@yahoo.es
 
+#pragma once
 #ifndef NV_MATH_COLOR_H
 #define NV_MATH_COLOR_H
 
 namespace nv
 {
+    typedef unsigned char   uint8;
+    typedef unsigned short  uint16;
+    typedef unsigned int    uint32;
 
-typedef unsigned char   uint8;
-typedef unsigned short  uint16;
-typedef unsigned int    uint32;
+    #ifdef _MSC_VER
+    typedef unsigned __int64  uint64;
+    #else
+    typedef unsigned long long uint64;
+    #endif
 
-#ifdef _MSC_VER
-typedef unsigned __int64  uint64;
-#else
-typedef unsigned long long uint64;
-#endif
+    typedef uint32          uint;
 
-typedef uint32          uint;
 
-/// 32 bit color stored as BGRA.
-class Color32
-{
-public:
-  Color32() { }
-  Color32(const Color32 & c) : u(c.u) { }
-  Color32(uint8 R, uint8 G, uint8 B) { setRGBA(R, G, B, 0xFF); }
-  Color32(uint8 R, uint8 G, uint8 B, uint8 A) { setRGBA( R, G, B, A); }
-  //Color32(uint8 c[4]) { setRGBA(c[0], c[1], c[2], c[3]); }
-  //Color32(float R, float G, float B) { setRGBA(uint(R*255), uint(G*255), uint(B*255), 0xFF); }
-  //Color32(float R, float G, float B, float A) { setRGBA(uint(R*255), uint(G*255), uint(B*255), uint(A*255)); }
-  explicit Color32(uint32 U) : u(U) { }
+    /// 64 bit color stored as BGRA.
+    class Color64 
+    {
+    public:
+        Color64() { }
+        Color64(const Color64 & c) : u(c.u) { }
+        Color64(uint16 R, uint16 G, uint16 B, uint16 A) { setRGBA(R, G, B, A); }
+        explicit Color64(uint64 U) : u(U) { }
 
-  void setRGBA(uint8 R, uint8 G, uint8 B, uint8 A)
-  {
-    r = R;
-    g = G;
-    b = B;
-    a = A;
-  }
+        void setRGBA(uint16 R, uint16 G, uint16 B, uint16 A)
+        {
+            r = R;
+            g = G;
+            b = B;
+            a = A;
+        }
 
-  void setBGRA(uint8 B, uint8 G, uint8 R, uint8 A = 0xFF)
-  {
-    r = R;
-    g = G;
-    b = B;
-    a = A;
-  }
-  
-  operator uint32 () const {
-    return u;
-  }
-  
-  union {
-    struct {
+        operator uint64 () const {
+            return u;
+        }
+
+        union {
+            struct {
 #if NV_LITTLE_ENDIAN
-      uint8 b, g, r, a;
+                uint16 r, a, b, g;
 #else
-      uint8 a: 8;
-      uint8 r: 8;
-      uint8 g: 8;
-      uint8 b: 8;
+                uint16 a: 16;
+                uint16 r: 16;
+                uint16 g: 16;
+                uint16 b: 16;
 #endif
+            };
+            uint64 u;
+        };
     };
-    uint32 u;
-  };
-};
 
+    /// 32 bit color stored as BGRA.
+    class Color32
+    {
+    public:
+        Color32() { }
+        Color32(const Color32 & c) : u(c.u) { }
+        Color32(uint8 R, uint8 G, uint8 B) { setRGBA(R, G, B, 0xFF); }
+        Color32(uint8 R, uint8 G, uint8 B, uint8 A) { setRGBA( R, G, B, A); }
+        //Color32(uint8 c[4]) { setRGBA(c[0], c[1], c[2], c[3]); }
+        //Color32(float R, float G, float B) { setRGBA(uint(R*255), uint(G*255), uint(B*255), 0xFF); }
+        //Color32(float R, float G, float B, float A) { setRGBA(uint(R*255), uint(G*255), uint(B*255), uint(A*255)); }
+        explicit Color32(uint32 U) : u(U) { }
 
-/// 16 bit 565 BGR color.
-class Color16
-{
-public:
-  Color16() { }
-  Color16(const Color16 & c) : u(c.u) { }
-  explicit Color16(uint16 U) : u(U) { }
-  
-  union {
-    struct {
+        void setRGBA(uint8 R, uint8 G, uint8 B, uint8 A)
+        {
+            r = R;
+            g = G;
+            b = B;
+            a = A;
+        }
+
+        void setBGRA(uint8 B, uint8 G, uint8 R, uint8 A = 0xFF)
+        {
+            r = R;
+            g = G;
+            b = B;
+            a = A;
+        }
+
+        operator uint32 () const {
+            return u;
+        }
+
+        union {
+            struct {
 #if NV_LITTLE_ENDIAN
-      uint16 b : 5;
-      uint16 g : 6;
-      uint16 r : 5;
+                uint8 b, g, r, a;
 #else
-      uint16 r : 5;
-      uint16 g : 6;
-      uint16 b : 5;
+                uint8 a: 8;
+                uint8 r: 8;
+                uint8 g: 8;
+                uint8 b: 8;
 #endif
+            };
+            uint32 u;
+        };
     };
-    uint16 u;
-  };
-};
+
+
+    /// 16 bit 565 BGR color.
+    class Color16
+    {
+    public:
+        Color16() { }
+        Color16(const Color16 & c) : u(c.u) { }
+        explicit Color16(uint16 U) : u(U) { }
+
+        union {
+            struct {
+#if NV_LITTLE_ENDIAN
+                uint16 b : 5;
+                uint16 g : 6;
+                uint16 r : 5;
+#else
+                uint16 r : 5;
+                uint16 g : 6;
+                uint16 b : 5;
+#endif
+            };
+            uint16 u;
+        };
+    };
+
+    /// Convert Color32 to Color16.
+    inline Color16 toColor16(Color32 c)
+    {
+        Color16 color;
+        //         rrrrrggggggbbbbb
+        // rrrrr000gggggg00bbbbb000
+        //  color.u = (c.u >> 3) & 0x1F;
+        //  color.u |= (c.u >> 5) & 0x7E0;
+        //  color.u |= (c.u >> 8) & 0xF800;
+
+        color.r = c.r >> 3;
+        color.g = c.g >> 2;
+        color.b = c.b >> 3;
+        return color; 
+    }
+
+
+    /// Promote 16 bit color to 32 bit using regular bit expansion.
+    inline Color32 toColor32(Color16 c)
+    {
+        Color32 color;
+        //  c.u = ((col0.u << 3) & 0xf8) | ((col0.u << 5) & 0xfc00) | ((col0.u << 8) & 0xf80000);
+        //  c.u |= (c.u >> 5) & 0x070007;
+        //  c.u |= (c.u >> 6) & 0x000300;
+
+        color.b = (c.b << 3) | (c.b >> 2);
+        color.g = (c.g << 2) | (c.g >> 4);
+        color.r = (c.r << 3) | (c.r >> 2);
+        color.a = 0xFF;
+
+        return color;
+    }
+
 
 } // nv namespace
 
--- components/render/opengl_driver/sources/texture_manager/nv_dxt/colorblock.h
+++ components/render/opengl_driver/sources/texture_manager/nv_dxt/colorblock.h
@@ -1,5 +1,6 @@
 // This code is in the public domain -- castanyo@yahoo.es
 
+#pragma once
 #ifndef NV_IMAGE_COLORBLOCK_H
 #define NV_IMAGE_COLORBLOCK_H
 
@@ -7,59 +8,72 @@
 
 namespace nv
 {
-  /// Uncompressed 4x4 color block.
-  struct ColorBlock
-  {
-    ColorBlock();
-    ColorBlock(const uint * linearImage);
-    ColorBlock(const ColorBlock & block);
-    
-    // Accessors
-    const Color32 * colors() const;
+    class Image;
+    class FloatImage;
 
-    Color32 color(uint i) const;
-    Color32 & color(uint i);
-    
-    Color32 color(uint x, uint y) const;
-    Color32 & color(uint x, uint y);
-    
-  private:
-    
-    Color32 m_color[4*4];
-    
-  };
-  
+    /// Uncompressed 4x4 color block.
+    struct ColorBlock
+    {
+        ColorBlock();
+        ColorBlock(const uint * linearImage);
+        ColorBlock(const ColorBlock & block);
 
-  /// Get pointer to block colors.
-  inline const Color32 * ColorBlock::colors() const
-  {
-    return m_color;
-  }
-  
-  /// Get block color.
-  inline Color32 ColorBlock::color(uint i) const
-  {
-    return m_color[i];
-  }
-  
-  /// Get block color.
-  inline Color32 & ColorBlock::color(uint i)
-  {
-    return m_color[i];
-  }
-  
-  /// Get block color.
-  inline Color32 ColorBlock::color(uint x, uint y) const
-  {
-    return m_color[y * 4 + x];
-  }
-  
-  /// Get block color.
-  inline Color32 & ColorBlock::color(uint x, uint y)
-  {
-    return m_color[y * 4 + x];
-  }
-  
+        void init(uint w, uint h, const uint * data, uint x, uint y);
+        void init(uint w, uint h, const float * data, uint x, uint y);
+
+        void swizzle(uint x, uint y, uint z, uint w); // 0=r, 1=g, 2=b, 3=a, 4=0xFF, 5=0
+
+        bool isSingleColor(Color32 mask = Color32(0xFF, 0xFF, 0xFF, 0x00)) const;
+        bool hasAlpha() const;
+
+
+        // Accessors
+        const Color32 * colors() const;
+
+        Color32 color(uint i) const;
+        Color32 & color(uint i);
+
+        Color32 color(uint x, uint y) const;
+        Color32 & color(uint x, uint y);
+
+    private:
+
+        Color32 m_color[4*4];
+
+    };
+
+
+    /// Get pointer to block colors.
+    inline const Color32 * ColorBlock::colors() const
+    {
+        return m_color;
+    }
+
+    /// Get block color.
+    inline Color32 ColorBlock::color(uint i) const
+    {
+        return m_color[i];
+    }
+
+    /// Get block color.
+    inline Color32 & ColorBlock::color(uint i)
+    {
+        return m_color[i];
+    }
+
+    /// Get block color.
+    inline Color32 ColorBlock::color(uint x, uint y) const
+    {
+        return m_color[y * 4 + x];
+    }
+
+    /// Get block color.
+    inline Color32 & ColorBlock::color(uint x, uint y)
+    {
+        return m_color[y * 4 + x];
+    }
+
+
 } // nv namespace
 
 #endif // NV_IMAGE_COLORBLOCK_H
