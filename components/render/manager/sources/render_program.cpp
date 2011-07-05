4cfa2a574dd4c9169a8a26b075fac74be4f8f5f0
Leny Kholodov <leny.kholodov@gmail.com>
2011-07-05 21:19:50 +0000

    Working on program with dynamic options

--- components/render/manager/sources/render_program.cpp
+++ components/render/manager/sources/render_program.cpp
@@ -12,15 +12,17 @@ namespace
 */
 
 typedef stl::vector<media::rfx::Shader> ShaderArray;
-typedef stl::vector<TexmapDesc> TexmapDescArray;
+typedef stl::vector<TexmapDesc>         TexmapDescArray;
 
 struct ProgramCommonData: public xtl::reference_counter
 {
-  ShaderArray     shaders;              //шейдеры
-  bool            need_shaders_rebuild; //шейдеры должны быть перекомпилированы
-  TexmapDescArray texmaps;              //текстурные карты  
-  
-  ProgramCommonData () : need_shaders_rebuild (true) {}
+  stl::string         name;                   //имя программы
+  ShaderArray         shaders;                //шейдеры
+  TexmapDescArray     texmaps;                //текстурные карты  
+  stl::string         static_options;         //статические опции компиляции шейдеров
+  stl::string         dynamic_options;        //имена динамических опций
+  ShaderOptionsLayout dynamic_options_layout; //расположение динамических опций
+  Log                 log;                    //протокол
 };
 
 typedef xtl::intrusive_ptr<ProgramCommonData> ProgramCommonDataPtr;
@@ -28,24 +30,58 @@ typedef xtl::intrusive_ptr<ProgramCommonData> ProgramCommonDataPtr;
 }
 
 /*
-    Описание реализации 
+    Описание реализации программы
 */
+
+typedef stl::hash_map<size_t, ProgramPtr> ProgramMap;
 
 struct Program::Impl
 {
-  ProgramCommonDataPtr common_data; //общие данные программы
+  ProgramCommonDataPtr common_data;           //общие данные программы
+  ShaderOptions        options;               //опции данного экземпляра программы
+  ProgramMap           derived_programs;      //производные программы  
+  LowLevelProgramPtr   low_level_program;     //низкоуровневая программа
   
 ///Конструктор
-  Impl ()
+  Impl (const char* name, const char* static_options, const char* dynamic_options)
     : common_data (new ProgramCommonData, false)
   {
+    try
+    {
+      if (!name)
+        throw xtl::make_null_argument_exception ("", "name");
+      
+      if (!static_options)
+        throw xtl::make_null_argument_exception ("", "static_options");
+        
+      if (!dynamic_options)
+        throw xtl::make_null_argument_exception ("", "dynamic_options");
+        
+      common_data->name            = name;
+      common_data->static_options  = static_options;      
+      common_data->dynamic_options = dynamic_options;
+      
+      common::StringArray option_array = common::split (dynamic_options);
+      
+      ShaderOptionsLayout& layout = common_data->dynamic_options_layout;
+      
+      for (size_t i=0, count=option_array.Size (); i<count; i++)
+        layout.Add (option_array [i]);
+    }
+    catch (xtl::exception& e)
+    {
+      e.touch ("render::Program::Impl::Impl");
+      throw;
+    }
   }
   
-  Impl (const Impl& impl, const ShaderOptions& options)
+  Impl (const Impl& impl, const ShaderOptions& in_options)
     : common_data (impl.common_data)
+    , options (impl.options)
   {
-    //??????????????
-    throw xtl::make_not_implemented_exception ("render::Program::Impl::Impl");    
+    options.options      += " ";
+    options.options      += in_options.options;
+    options.options_hash  = common::strhash (options.options.c_str ());
   }
 };
 
@@ -53,8 +89,8 @@ struct Program::Impl
     Конструкторы / деструктор
 */
 
-Program::Program ()
-  : impl (new Impl)
+Program::Program (const char* name, const char* static_options, const char* dynamic_options)
+  : impl (new Impl (name, static_options, dynamic_options))
 {
 }
 
@@ -96,6 +132,20 @@ void Program::Detach (const media::rfx::Shader& shader)
 void Program::DetachAllShaders ()
 {
   impl->common_data->shaders.clear ();
+}
+
+/*
+    Опции данной программы
+*/
+
+const char* Program::StaticOptions ()
+{
+  return impl->common_data->static_options.c_str ();
+}
+
+const char* Program::DynamicOptions ()
+{
+  return impl->common_data->dynamic_options.c_str ();
 }
 
 /*
@@ -160,21 +210,140 @@ void Program::RemoveAllTexmaps ()
     Получение производной программы
 */
 
-Program& Program::DerivedProgram (const ShaderOptions&)
+Program& Program::DerivedProgram (const ShaderOptions& options)
 {
-  throw xtl::make_not_implemented_exception ("render::Program::DerivedProgram");
+  try
+  {
+    if (options.options.empty ())
+      return *this;
+      
+    ProgramMap::iterator iter = impl->derived_programs.find (options.options_hash);
+    
+    if (iter != impl->derived_programs.end ())
+      return *iter->second;
+      
+    ProgramPtr program (new Program (*this, options), false);
+    
+    impl->derived_programs.insert_pair (options.options_hash, program);
+    
+    return *program;
+  }
+  catch (xtl::exception& e)
+  {
+    e.touch ("render::Program::DerivedProgram(const ShaderOptions&)");
+    throw;
+  }
 }
 
-Program& Program::DerivedProgram (const ShaderOptionsCache&)
+Program& Program::DerivedProgram (ShaderOptionsCache& cache)
 {
-  throw xtl::make_not_implemented_exception ("render::Program::DerivedProgram");
+  try
+  {
+    const ShaderOptions& options = cache.GetShaderOptions (impl->common_data->dynamic_options_layout);
+    
+    return DerivedProgram (options);
+  }
+  catch (xtl::exception& e)
+  {
+    e.touch ("render::Program::DerivedProgram(const ShaderOptionsCache&)");
+    throw;
+  }
 }
 
 /*
     Получение низкоуровневой программы шейдинга
 */
 
-const LowLevelProgramPtr& Program::LowLevelProgram (render::low_level::IDevice&)
+namespace
 {
-  throw xtl::make_not_implemented_exception ("render::Program::LowLevelProgram");
+
+struct ShaderCompilerLog
+{
+  ShaderCompilerLog (ProgramCommonData& in_common_data)
+    : common_data (in_common_data)
+  {
+  }
+  
+  void operator () (const char* message)
+  {
+      //отсечение стандартных сообщений об отсутствии ошибок
+      
+    static const char* IGNORE_MESSAGES [] = {
+      "*: No errors.",
+      "Fragment shader(s) linked, vertex shader(s) linked*",
+      "*shader was successfully compiled to run on hardware."
+    };
+    
+    static const size_t IGNORE_MESSAGES_COUNT = sizeof (IGNORE_MESSAGES) / sizeof (*IGNORE_MESSAGES);
+    
+    for (size_t i=0; i<IGNORE_MESSAGES_COUNT; i++)
+      if (common::wcimatch (message, IGNORE_MESSAGES [i]))
+        return;
+      
+      //вывод сообщения
+    
+    static const char* LINKER_PREFIX        = "linker: ";
+    static size_t      LINKER_PREFIX_LENGTH = strlen (LINKER_PREFIX);
+    
+    if (common::wcimatch (message, "linker:*"))
+    {
+      common_data.log.Printf ("%s: %s", common_data.name.c_str (), message + LINKER_PREFIX_LENGTH);
+      return;
+    }
+    
+    common_data.log.Printf ("%s", message);
+  }
+  
+  ProgramCommonData& common_data;
+};    
+
+}
+
+const LowLevelProgramPtr& Program::LowLevelProgram (render::low_level::IDevice& device)
+{
+  try
+  {
+    if (impl->low_level_program)
+      return impl->low_level_program;
+      
+    stl::string options = impl->common_data->static_options + " " + impl->options.options;
+      
+    static const size_t SHADERS_RESERVE_COUNT = 4;
+
+    stl::vector<render::low_level::ShaderDesc> shaders;
+    
+    shaders.reserve (SHADERS_RESERVE_COUNT);        
+
+    for (ShaderArray::iterator iter=impl->common_data->shaders.begin (), end=impl->common_data->shaders.end (); iter!=end; ++iter)
+    {
+      media::rfx::Shader& shader = *iter;      
+      
+      render::low_level::ShaderDesc desc;
+      
+      memset (&desc, 0, sizeof (desc));      
+      
+      desc.name             = shader.Name ();  
+      desc.source_code_size = shader.SourceCodeSize ();
+      desc.source_code      = shader.SourceCode ();
+      desc.options          = options.c_str ();
+      
+      shaders.push_back (desc);
+    }
+
+    if (shaders.empty ())
+      throw xtl::format_operation_exception ("", "No shaders found for shading");
+      
+    impl->common_data->log.Printf ("Build program '%s'...", impl->common_data->name.c_str ());
+
+    LowLevelProgramPtr program (device.CreateProgram (shaders.size (), &shaders [0], ShaderCompilerLog (*impl->common_data)), false);
+      
+    impl->low_level_program = program;
+  
+    return impl->low_level_program;
+  }
+  catch (xtl::exception& e)
+  {
+    e.touch ("render::Program::LowLevelProgram");
+    throw;
+  }
 }
