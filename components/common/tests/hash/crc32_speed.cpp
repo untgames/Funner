f47a6496169685edcdf13e5d352810d064779687
Ievgen Mukhin <muhineg@gmail.com>
2011-07-28 22:59:18 +0000

    Подключение алгоритмов рассчета crc32 - slicing by 4 и slicing by 8. Тест скорости рассчета crc32 для блоков различного объема.

--- components/common/tests/hash/crc32_speed.cpp
+++ components/common/tests/hash/crc32_speed.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <cstdio>
 
 #include <common/hash.h>
@@ -5,31 +6,39 @@
 
 using namespace common;
 
-const size_t LOOPS_COUNT     = 1024 * 8;
-const size_t TEST_ARRAY_SIZE = 1024 * 1024 + 3;
-const size_t CORRECT_HASH    = 1984837353;
+const size_t TEST_SIZE           = 1024 * 1024 * 1024;
+const size_t TEST_BUFFER_SIZE    = 1024 * 1024 + 3;
+const size_t TEST_ARRAY_SIZES [] = { 4, 16, 35, 64, 256, 1024, 1024 * 16, 1024 * 256, TEST_BUFFER_SIZE };
+const size_t TESTS_COUNT         = sizeof (TEST_ARRAY_SIZES) / sizeof (*TEST_ARRAY_SIZES);
+const size_t CORRECT_HASHES []   = { 2842971831, 871311357, 3201633541, 2382920724, 2258954437, 3615731820, 3751447993, 1430708165, 2975383147 };
 
 int main ()
 {
   printf ("Results of crc32_speed_test:\n");
 
-  unsigned char* test_array = new unsigned char [TEST_ARRAY_SIZE];
+  unsigned char* test_array = new unsigned char [TEST_BUFFER_SIZE];
 
   unsigned char* data = test_array;
 
-  for (size_t i = 0; i < TEST_ARRAY_SIZE; i++, data++)
+  for (size_t i = 0; i < TEST_BUFFER_SIZE; i++, data++)
     *data = i % 255;
 
-  size_t start_time = common::milliseconds ();
+  for (size_t i = 0; i < TESTS_COUNT; i++)
+  {
+    size_t block_size  = TEST_ARRAY_SIZES [i],
+           loops_count = (size_t)ceil (TEST_SIZE / (float)block_size);
 
-  size_t crc_value = 0;
+    size_t start_time = common::milliseconds ();
 
-  for (size_t i = 0; i < LOOPS_COUNT; i++)
-    crc_value = crc32 (test_array, TEST_ARRAY_SIZE, crc_value);
+    size_t crc_value = 0;
 
-  size_t end_time = common::milliseconds ();
+    for (size_t j = 0; j < loops_count; j++)
+      crc_value = crc32 (test_array, block_size, crc_value);
 
-  printf ("Data hash correct %c, crc speed = %f MB/s\n", crc_value == CORRECT_HASH ? 'y' : 'n', TEST_ARRAY_SIZE / ((end_time - start_time) / 1000.f) / 1024 / 1024 * LOOPS_COUNT);
+    size_t end_time = common::milliseconds ();
+
+    printf ("Data hash correct %c, crc speed for block size %u = %f MB/s\n", crc_value == CORRECT_HASHES [i] ? 'y' : 'n', block_size, block_size * loops_count / ((end_time - start_time) / 1000.f) / 1024 / 1024);
+  }
 
   delete [] test_array;
 
