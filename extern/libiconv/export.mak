1677c13fc54bb3ef7b46d47e604d12f09bb90746
Leny Kholodov <leny.kholodov@gmail.com>
2011-08-01 16:08:11 +0000

    Build on Mac

--- extern/libiconv/export.mak
+++ extern/libiconv/export.mak
@@ -1,5 +1,14 @@
+ifeq (,$(filter has_iconv,$(PROFILES)))
+
 # Compile exports
 export.compile.extern.iconv.INCLUDE_DIRS := include
 
 # Link exports
 export.link.extern.iconv.LIBS := funner.extern.iconv
+
+else
+
+# Link exports
+export.link.extern.iconv.LIBS := iconv
+
+endif
--- extern/libiconv/tests/iconv01.sh
+++ extern/libiconv/tests/iconv01.sh
@@ -2,9 +2,9 @@
 echo Resutls of iconv01_test:
 for name in CP1255 CP1258 ISO-2022-CN-EXT ISO-2022-CN ISO-2022-JP-1 ISO-2022-JP-2 ISO-2022-JP ISO-2022-KR UTF-16BE UTF-16LE UTF-32 UTF-32BE UTF-32LE UTF-7
 do
-  echo -n "Test $name... "
+  printf "Test $name... "
   iconv -f $name -t UTF-8 < data/$name-snippet > $name.tmp-snippet
-  diff data/$name-snippet.UTF-8 $name.tmp-snippet || exit 1
+  diff --strip-trailing-cr data/$name-snippet.UTF-8 $name.tmp-snippet || exit 1
   rm $name.tmp-snippet
   echo Ok
 done
