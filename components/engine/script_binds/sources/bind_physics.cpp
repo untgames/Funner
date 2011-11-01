c54f96eade4169899c39ebf3c367454eb9dc5492
Ievgen Mukhin <muhineg@gmail.com>
2011-11-01 17:12:25 +0000

    Биндинг физики.

--- components/engine/script_binds/sources/bind_physics.cpp
+++ components/engine/script_binds/sources/bind_physics.cpp
@@ -15,8 +15,11 @@ const char* JOINT_BIND_LIBRARY           = "Physics.JointBind";
 const char* JOINT_LIBRARY                = "Physics.Joint";
 const char* MANAGER_LIBRARY              = "Physics.Manager";
 const char* MATERIAL_LIBRARY             = "Physics.Material";
+const char* RIGID_BODY_LIBRARY           = "Physics.RigidBody";
+const char* RIGID_BODY_FLAGS_LIBRARY     = "Physics.RigidBodyFlag";
 const char* SCENE_LIBRARY                = "Physics.Scene";
 const char* SHAPE_LIBRARY                = "Physics.Shape";
+const char* SHAPE_LIST_LIBRARY           = "Physics.ShapeList";
 const char* COMPONENT_NAME               = "script.binds.Physics";
 const char* BINDER_NAME                  = "Physics";
 
@@ -30,11 +33,12 @@ void bind_manager_library (Environment& environment)
       make_invoker (xtl::implicit_cast<Material (PhysicsManager::*)(const char*)> (&PhysicsManager::CreateMaterial)),
       make_invoker (xtl::implicit_cast<Material (PhysicsManager::*)()> (&PhysicsManager::CreateMaterial))
   ));
-  lib.Register ("CreateBoxShape",     make_invoker (&PhysicsManager::CreateBoxShape));
-  lib.Register ("CreateSphereShape",  make_invoker (&PhysicsManager::CreateSphereShape));
-  lib.Register ("CreateCapsuleShape", make_invoker (&PhysicsManager::CreateCapsuleShape));
-  lib.Register ("CreatePlaneShape",   make_invoker (&PhysicsManager::CreatePlaneShape));
-  lib.Register ("CreateShape",        make_invoker (
+  lib.Register ("CreateBoxShape",      make_invoker (&PhysicsManager::CreateBoxShape));
+  lib.Register ("CreateSphereShape",   make_invoker (&PhysicsManager::CreateSphereShape));
+  lib.Register ("CreateCapsuleShape",  make_invoker (&PhysicsManager::CreateCapsuleShape));
+  lib.Register ("CreatePlaneShape",    make_invoker (&PhysicsManager::CreatePlaneShape));
+  lib.Register ("CreateCompoundShape", make_invoker (&PhysicsManager::CreateCompoundShape));
+  lib.Register ("CreateShape",         make_invoker (
       make_invoker (xtl::implicit_cast<Shape (PhysicsManager::*)(const char*, const math::vec3f&)> (&PhysicsManager::CreateShape)),
       make_invoker<Shape (PhysicsManager&, const char*)> (xtl::bind (xtl::implicit_cast<Shape (PhysicsManager::*)(const char*, const math::vec3f&)> (&PhysicsManager::CreateShape), _1, _2, math::vec3f (1.f)))
   ));
@@ -163,6 +167,47 @@ void bind_shape_library (Environment& environment)
   environment.RegisterType<Shape> (SHAPE_LIBRARY);
 }
 
+ShapeList create_shape_list ()
+{
+  return ShapeList ();
+}
+
+const Shape& get_shape_list_shape (const ShapeList& list, size_t index)
+{
+  return list [index];
+}
+
+const math::vec3f& get_shape_list_shape_position (const ShapeList& list, size_t index)
+{
+  return list.Transform (index).position;
+}
+
+const math::quatf& get_shape_list_shape_orientation (const ShapeList& list, size_t index)
+{
+  return list.Transform (index).orientation;
+}
+
+void bind_shape_list_library (Environment& environment)
+{
+  InvokerRegistry lib = environment.Library (SHAPE_LIST_LIBRARY);
+
+  lib.Register ("get_Size", make_invoker (&ShapeList::Size));
+  lib.Register ("Create",   make_invoker (&create_shape_list));
+  lib.Register ("Add",      make_invoker (
+      make_invoker (xtl::implicit_cast<void (ShapeList::*)(const Shape&, const math::vec3f&, const math::quatf&)> (&ShapeList::Add)),
+      make_invoker (xtl::implicit_cast<void (ShapeList::*)(const Shape&, const math::quatf&)> (&ShapeList::Add)),
+      make_invoker<void (ShapeList&, const Shape&, const math::vec3f&)> (xtl::bind (xtl::implicit_cast<void (ShapeList::*)(const Shape&, const math::vec3f&, const math::quatf&)> (&ShapeList::Add), _1, _2, _3, math::quatf ())),
+      make_invoker<void (ShapeList&, const Shape&)> (xtl::bind (xtl::implicit_cast<void (ShapeList::*)(const Shape&, const math::vec3f&, const math::quatf&)> (&ShapeList::Add), _1, _2, math::vec3f (), math::quatf ()))
+  ));
+  lib.Register ("Remove",              make_invoker (&ShapeList::Remove));
+  lib.Register ("Clear",               make_invoker (&ShapeList::Clear));
+  lib.Register ("Shape",            make_invoker (&get_shape_list_shape));
+  lib.Register ("ShapePosition",    make_invoker (&get_shape_list_shape_position));
+  lib.Register ("ShapeOrientation", make_invoker (&get_shape_list_shape_orientation));
+
+  environment.RegisterType<ShapeList> (SHAPE_LIST_LIBRARY);
+}
+
 void bind_material_library (Environment& environment)
 {
   InvokerRegistry lib = environment.Library (MATERIAL_LIBRARY);
@@ -183,6 +228,101 @@ void bind_material_library (Environment& environment)
   environment.RegisterType<Material> (MATERIAL_LIBRARY);
 }
 
+const math::vec3f& get_rigid_body_world_position (const RigidBody& body)
+{
+  return body.WorldTransform ().position;
+}
+
+const math::quatf& get_rigid_body_world_orientation (const RigidBody& body)
+{
+  return body.WorldTransform ().orientation;
+}
+
+void set_rigid_body_world_position (RigidBody& body, const math::vec3f& position)
+{
+  Transform transform = body.WorldTransform ();
+
+  transform.position = position;
+
+  body.SetWorldTransform (transform);
+}
+
+void set_rigid_body_world_orientation (RigidBody& body, const math::quatf& orientation)
+{
+  Transform transform = body.WorldTransform ();
+
+  transform.orientation = orientation;
+
+  body.SetWorldTransform (transform);
+}
+
+bool get_rigid_body_flag (RigidBody& body, size_t flag)
+{
+  return body.CheckFlags (flag) == flag;
+}
+
+void bind_rigid_body_library (Environment& environment)
+{
+  InvokerRegistry lib = environment.Library (RIGID_BODY_LIBRARY);
+
+  lib.Register ("get_Id",                     make_invoker (&RigidBody::Id));
+  lib.Register ("get_Mass",                   make_invoker (&RigidBody::Mass));
+  lib.Register ("get_MassSpaceInertiaTensor", make_invoker (&RigidBody::MassSpaceInertiaTensor));
+  lib.Register ("get_WorldPosition",          make_invoker (&get_rigid_body_world_position));
+  lib.Register ("get_WorldOrientation",       make_invoker (&get_rigid_body_world_orientation));
+  lib.Register ("get_SleepLinearVelocity",    make_invoker (&RigidBody::SleepLinearVelocity));
+  lib.Register ("get_SleepAngularVelocity",   make_invoker (&RigidBody::SleepAngularVelocity));
+  lib.Register ("get_CcdMotionThreshold",     make_invoker (&RigidBody::CcdMotionThreshold));
+  lib.Register ("get_Shape",                  make_invoker (xtl::implicit_cast<const Shape& (RigidBody::*) () const> (&RigidBody::Shape)));
+  lib.Register ("get_Material",               make_invoker (xtl::implicit_cast<const Material& (RigidBody::*) () const> (&RigidBody::Material)));
+  lib.Register ("get_CollisionGroup",         make_invoker (&RigidBody::CollisionGroup));
+  lib.Register ("get_LinearVelocity",         make_invoker (&RigidBody::LinearVelocity));
+  lib.Register ("get_AngularVelocity",        make_invoker (&RigidBody::AngularVelocity));
+  lib.Register ("set_Mass",                   make_invoker (&RigidBody::SetMass));
+  lib.Register ("set_MassSpaceInertiaTensor", make_invoker (&RigidBody::SetMassSpaceInertiaTensor));
+  lib.Register ("set_WorldPosition",          make_invoker (&set_rigid_body_world_position));
+  lib.Register ("set_WorldOrientation",       make_invoker (&set_rigid_body_world_orientation));
+  lib.Register ("set_SleepLinearVelocity",    make_invoker (&RigidBody::SetSleepLinearVelocity));
+  lib.Register ("set_SleepAngularVelocity",   make_invoker (&RigidBody::SetSleepAngularVelocity));
+  lib.Register ("set_CcdMotionThreshold",     make_invoker (&RigidBody::SetCcdMotionThreshold));
+  lib.Register ("set_Material",               make_invoker (&RigidBody::SetMaterial));
+  lib.Register ("set_CollisionGroup",         make_invoker (&RigidBody::SetCollisionGroup));
+  lib.Register ("set_LinearVelocity",         make_invoker (&RigidBody::SetLinearVelocity));
+  lib.Register ("set_AngularVelocity",        make_invoker (&RigidBody::SetAngularVelocity));
+  lib.Register ("GetFlag",                    make_invoker (&get_rigid_body_flag));
+  lib.Register ("SetFlag",                    make_invoker (&RigidBody::ChangeFlags));
+  lib.Register ("AddForce",                   make_invoker (
+      make_invoker (&RigidBody::AddForce),
+      make_invoker<void (RigidBody&, const math::vec3f&)> (xtl::bind (&RigidBody::AddForce, _1, _2, math::vec3f (0.f)))
+  ));
+  lib.Register ("AddImpulse",                 make_invoker (
+      make_invoker (&RigidBody::AddImpulse),
+      make_invoker<void (RigidBody&, const math::vec3f&)> (xtl::bind (&RigidBody::AddImpulse, _1, _2, math::vec3f (0.f)))
+  ));
+  lib.Register ("AddTorque",                       make_invoker (&RigidBody::AddTorque));
+  lib.Register ("CreateTransformUpdateCallback",   make_callback_invoker<RigidBody::TransformUpdateCallback::signature_type> ());
+  lib.Register ("RegisterTransformUpdateCallback", make_invoker (&RigidBody::RegisterTransformUpdateCallback));
+  lib.Register ("CreateCollisionCallback",         make_callback_invoker<CollisionCallback::signature_type> ());
+  lib.Register ("RegisterCollisionCallback",       make_invoker (&RigidBody::RegisterCollisionCallback));
+
+  environment.RegisterType<RigidBody> (RIGID_BODY_LIBRARY);
+}
+
+void bind_rigid_body_flags_library (Environment& environment)
+{
+  InvokerRegistry lib = environment.Library (RIGID_BODY_FLAGS_LIBRARY);
+
+  lib.Register ("get_FrozenPositionX",   make_const (RigidBodyFlag_FrozenPositionX));
+  lib.Register ("get_FrozenPositionY",   make_const (RigidBodyFlag_FrozenPositionY));
+  lib.Register ("get_FrozenPositionZ",   make_const (RigidBodyFlag_FrozenPositionZ));
+  lib.Register ("get_FrozenPosition",    make_const (RigidBodyFlag_FrozenPosition));
+  lib.Register ("get_FrozenRotationX",   make_const (RigidBodyFlag_FrozenRotationX));
+  lib.Register ("get_FrozenRotationY",   make_const (RigidBodyFlag_FrozenRotationY));
+  lib.Register ("get_FrozenRotationZ",   make_const (RigidBodyFlag_FrozenRotationZ));
+  lib.Register ("get_FrozenRotation",    make_const (RigidBodyFlag_FrozenRotation));
+  lib.Register ("get_Kinematic",         make_const (RigidBodyFlag_Kinematic));
+}
+
 void bind_physics_library (Environment& environment)
 {
   bind_manager_library (environment);
@@ -191,7 +331,10 @@ void bind_physics_library (Environment& environment)
   bind_joint_library (environment);
   bind_collision_event_type_library (environment);
   bind_shape_library (environment);
+  bind_shape_list_library (environment);
   bind_material_library (environment);
+  bind_rigid_body_library (environment);
+  bind_rigid_body_flags_library (environment);
 }
 
 }
--- components/engine/script_binds/tests/lua/data/physics.lua
+++ components/engine/script_binds/tests/lua/data/physics.lua
@@ -4,6 +4,7 @@ end
 
 function collision_filter (body1, body2)
   print ("Collision filter called")
+  return true
 end
 
 function collision_event_name (event)
@@ -22,9 +23,47 @@ function collision_callback (event, body1, body2, point)
   print ("Collision callback called for event " .. collision_event_name (event) .. " at point " .. tostring (point))
 end
 
+function body_collision_callback (event, body1, body2, point)
+  print ("Body collision callback called for event " .. collision_event_name (event) .. " at point " .. tostring (point))
+end
+
 function print_material (material)
   print (string.format ("Material linear damping = %.2f, angular damping = %.2f, friction = %.2f, anisotropic friction = %.2f %.2f %.2f, restitution = %.2f", material.LinearDamping, material.AngularDamping, material.Friction,
                         material.AnisotropicFriction.x, material.AnisotropicFriction.y, material.AnisotropicFriction.z, material.Restitution))
+end
+
+function print_shape_list_shape_info (list, index)
+  print (string.format ("Shape list %d shape margin = %.2f position is %s orientation is %s", index, list:Shape (index).Margin, tostring (list:ShapePosition (index)), tostring (list:ShapeOrientation (index))))
+end
+
+function body_update_transform_callback (body)
+  print ("body transform updated")
+end
+
+function print_rigid_body (body)
+  print ("Rigid body:")
+  print (string.format ("  mass %.2f", body.Mass))
+  print ("  mass space inertia tensor " .. tostring (body.MassSpaceInertiaTensor))
+  print ("  world position " .. tostring (body.WorldPosition))
+  print ("  world orientation " .. tostring (body.WorldOrientation))
+  print (string.format ("  sleep linear velocity %.2f", body.SleepLinearVelocity))
+  print (string.format ("  sleep angular velocity %.2f", body.SleepAngularVelocity))
+  print (string.format ("  ccd motion threshold %.2f", body.CcdMotionThreshold))
+  print (string.format ("  shape margin %.2f", body.Shape.Margin))
+  print (string.format ("  material linear damping %.2f", body.Material.LinearDamping))
+  print ("  collision group '" .. body.CollisionGroup .. "'")
+  print ("  linear velocity " .. tostring (body.LinearVelocity))
+  print ("  angular velocity " .. tostring (body.AngularVelocity))
+  print ("  flags:")
+  print ("    frozen position x = " .. tostring (body:GetFlag (Physics.RigidBodyFlag.FrozenPositionX)))
+  print ("    frozen position y = " .. tostring (body:GetFlag (Physics.RigidBodyFlag.FrozenPositionY)))
+  print ("    frozen position z = " .. tostring (body:GetFlag (Physics.RigidBodyFlag.FrozenPositionZ)))
+  print ("    frozen position = " .. tostring (body:GetFlag (Physics.RigidBodyFlag.FrozenPosition)))
+  print ("    frozen rotation x = " .. tostring (body:GetFlag (Physics.RigidBodyFlag.FrozenRotationX)))
+  print ("    frozen rotation y = " .. tostring (body:GetFlag (Physics.RigidBodyFlag.FrozenRotationY)))
+  print ("    frozen rotation z = " .. tostring (body:GetFlag (Physics.RigidBodyFlag.FrozenRotationZ)))
+  print ("    frozen rotation = " .. tostring (body:GetFlag (Physics.RigidBodyFlag.FrozenRotation)))
+  print ("    kinematic = " .. tostring (body:GetFlag (Physics.RigidBodyFlag.Kinematic)))
 end
 
 function test_physics()
@@ -39,7 +78,33 @@ function test_physics()
   local box_shape     = manager:CreateBoxShape (vec3 (1, 2, 3))
   local sphere_shape  = manager:CreateSphereShape (1)
   local capsule_shape = manager:CreateCapsuleShape (1, 2)
-  local plane_shape   = manager:CreatePlaneShape (vec3 (1, 2, 3), 4)
+  local plane_shape   = manager:CreatePlaneShape (vec3 (0, 1, 0), 10)
+  
+  local shape_list = Physics.ShapeList.Create ()
+  
+  print ("Shape list size = " .. shape_list.Size)
+  
+  shape_list:Add (box_shape)
+  shape_list:Add (sphere_shape, vec3 (1, 2, 3))
+  shape_list:Add (capsule_shape, quat (1, 2, 3, 4))
+  shape_list:Add (plane_shape, vec3 (5, 6, 7), quat (1, 2, 3, 4))
+
+  local compound_shape = manager:CreateCompoundShape (shape_list)
+  
+  print ("Shape list size = " .. shape_list.Size)
+
+  print_shape_list_shape_info (shape_list, 0)
+  print_shape_list_shape_info (shape_list, 1)
+  print_shape_list_shape_info (shape_list, 2)
+  print_shape_list_shape_info (shape_list, 3)
+
+  shape_list:Remove (1)
+  
+  print ("Shape list size = " .. shape_list.Size)
+  
+  shape_list:Clear ()
+  
+  print ("Shape list size = " .. shape_list.Size)
   
   print_material (material)
 
@@ -67,8 +132,42 @@ function test_physics()
   print (string.format ("Scene simulation step = %0.2f", scene.SimulationStep))
   
   local body1 = scene:CreateRigidBody (box_shape, 1)
-  local body2 = scene:CreateRigidBody (box_shape, 2)
+  local body2 = scene:CreateRigidBody (plane_shape, 2)
   
+  print_rigid_body (body1)
+  print_rigid_body (body2)
+
+  body1.Mass = 4
+  body1.MassSpaceInertiaTensor = vec3 (1, 2, 3)
+  body1.WorldPosition = vec3 (2, 3, 4)
+  body1.WorldOrientation = quat (1, 2, 3, 4)
+  body1.SleepLinearVelocity = 5
+  body1.SleepAngularVelocity = 6
+  body1.CcdMotionThreshold = 7
+  body1.CollisionGroup = "object"
+  body1.LinearVelocity = vec3 (7, 8, 9)
+  body1.AngularVelocity = vec3 (8, 9, 10)
+  body1.Material = material  
+  body1:SetFlag (Physics.RigidBodyFlag.FrozenPositionX, true)  
+  body1:SetFlag (Physics.RigidBodyFlag.FrozenRotationY, true)  
+  body1:SetFlag (Physics.RigidBodyFlag.Kinematic, true)  
+  
+  body2.CollisionGroup = "ground"
+  
+  print_rigid_body (body1)
+  
+  body1:SetFlag (Physics.RigidBodyFlag.FrozenPositionX, true)  
+  body1:SetFlag (Physics.RigidBodyFlag.FrozenPositionY, true)  
+  body1:SetFlag (Physics.RigidBodyFlag.FrozenPositionZ, true)  
+  body1:SetFlag (Physics.RigidBodyFlag.FrozenRotation, true)  
+  body1:SetFlag (Physics.RigidBodyFlag.FrozenRotationY, false)  
+  
+  print_rigid_body (body1)
+
+  body1:SetFlag (Physics.RigidBodyFlag.FrozenPosition, false)  
+  body1:SetFlag (Physics.RigidBodyFlag.FrozenRotation, false)  
+  body1:SetFlag (Physics.RigidBodyFlag.Kinematic, false)  
+
   local joint_bind_1 = Physics.JointBind.Create (body1)
   local joint_bind_2 = Physics.JointBind.Create (body1, vec3 (1, 2, 3))
   local joint_bind_3 = Physics.JointBind.Create (body2, vec3 (2, 3, 4), vec3 (1, 0, 0))
@@ -100,7 +199,11 @@ function test_physics()
 
   local filter3 = scene:AddCollisionFilter ("*", "*", true, Physics.Scene.CreateCollisionFilter (collision_filter))
   
-  local callback_connection = scene:RegisterCollisionCallback ("*", "*", Physics.CollisionEventType.Begin, Physics.Scene.CreateCollisionCallback (collision_callback))
+  local callback_connection       = scene:RegisterCollisionCallback ("*", "*", Physics.CollisionEventType.Begin, Physics.Scene.CreateCollisionCallback (collision_callback))
+  local body_transform_connection = body1:RegisterTransformUpdateCallback (Physics.RigidBody.CreateTransformUpdateCallback (body_update_transform_callback))
+  local body_collision_connection = body1:RegisterCollisionCallback ("*", Physics.CollisionEventType.Begin, Physics.RigidBody.CreateCollisionCallback (body_collision_callback))
+  
+  scene:PerformSimulation (1)
 end
 
 function test ()
