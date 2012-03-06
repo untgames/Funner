b04d27f75aebc44e2150e0d4b96f57564f675495
Ievgen Mukhin <muhineg@gmail.com>
2012-03-06 22:55:23 +0000

    Биндинг функций по работе с сообщениями приложения.

--- components/engine/script_binds/tests/lua/data/system.lua
+++ components/engine/script_binds/tests/lua/data/system.lua
@@ -25,7 +25,19 @@ function idle2 ()
   end
 end
 
+function all_notifications_handler (notification)
+  print ("all_notifications_handler: '" .. notification .. "'")
+end
+
+function my_notifications_handler (notification)
+  print ("my_notifications_handler: '" .. notification .. "'")
+end
+
 function test ()
   connection = System.Application.RegisterEventHandler (System.ApplicationEvent.OnIdle, System.Application.CreateEventHandler ("idle1"))
   System.Application.RegisterEventHandler (System.ApplicationEvent.OnIdle, System.Application.CreateEventHandler ("idle2"))  
+  System.Application.RegisterNotificationHandler ("*", System.Application.CreateNotificationHandler ("all_notifications_handler"))  
+  System.Application.RegisterNotificationHandler ("my *", System.Application.CreateNotificationHandler ("my_notifications_handler"))  
+  System.Application.PostNotification ("notification 1")
+  System.Application.PostNotification ("my notification")
 end
