cfb12c65c5053e4b9a15182bcdc0cf784976999c
Ievgen Mukhin <muhineg@gmail.com>
2012-06-18 14:15:52 +0000

    Лаунчер для Plarium.

--- components/demos/plarium/launcher/sources/shared/application.cpp
+++ components/demos/plarium/launcher/sources/shared/application.cpp
@@ -9,10 +9,12 @@ namespace
 {
 
 //constants
-const size_t        SEND_QUEUE_SIZE       = 16;
-const size_t        KEEP_ALIVE_INTERVAL   = 30000;
-const size_t        COMPRESSION_THRESHOLD = 1024;
-const unsigned char ENCRYPTION_KEY []     = { 0xd0, 0x71, 0x73, 0x41, 0x0d, 0xee, 0xd2, 0x6f, 0x44, 0x3f, 0x7b, 0xa4, 0x42, 0x2f, 0x86, 0x03 };
+const size_t        SEND_QUEUE_SIZE          = 16;
+const size_t        KEEP_ALIVE_INTERVAL      = 30000;
+const size_t        COMPRESSION_THRESHOLD    = 1024;
+const unsigned char ENCRYPTION_KEY []        = { 0xd0, 0x71, 0x73, 0x41, 0x0d, 0xee, 0xd2, 0x6f, 0x44, 0x3f, 0x7b, 0xa4, 0x42, 0x2f, 0x86, 0x03 };
+const char*         QUOTE_REPLACEMENT        = "\\u0027";
+const size_t        QUOTE_REPLACEMENT_LENGTH = strlen (QUOTE_REPLACEMENT);
 
 }
 
@@ -26,10 +28,14 @@ struct Application::Impl : public INotificationListener, public IHsConnectionEve
   engine::IEngine*            engine;
   std::string                 host;
   unsigned short              port;
+  unsigned char*              message_buffer;
+  size_t                      message_buffer_size;
 
   Impl ()
     : engine (0)
     , port (0)
+    , message_buffer (0)
+    , message_buffer_size (0)
   {
     HsConnectionSettings connection_settings;
 
@@ -48,6 +54,11 @@ struct Application::Impl : public INotificationListener, public IHsConnectionEve
 
     connection->SetEventListener (this);
     connection->SetLogListener (this);
+  }
+
+  ~Impl ()
+  {
+    delete [] message_buffer;
   }
 
   void Run (engine::IEngine* in_engine)
@@ -112,6 +123,58 @@ struct Application::Impl : public INotificationListener, public IHsConnectionEve
 
   void OnMessageReceived (HsConnection& sender, unsigned short plugin_id, const unsigned char* message, size_t size)
   {
+    bool quote_found = false;
+
+    for (const unsigned char* m = message; *m; m++)
+    {
+      if (*m == '\'')
+      {
+        quote_found = true;
+        break;
+      }
+    }
+
+    if (quote_found)
+    {
+      size_t escaped_message_size = size * 5;
+
+      if (message_buffer_size < escaped_message_size)
+      {
+        try
+        {
+          delete [] message_buffer;
+
+          message_buffer = new unsigned char [escaped_message_size];
+
+          message_buffer_size = escaped_message_size;
+        }
+        catch (...)
+        {
+          message_buffer      = 0;
+          message_buffer_size = 0;
+          throw;
+        }
+      }
+
+      unsigned char* replaced_char = message_buffer;
+
+      for (const unsigned char* m = message; *m; m++, replaced_char++)
+      {
+        if (*m == '\'')
+        {
+          memcpy (replaced_char, QUOTE_REPLACEMENT, QUOTE_REPLACEMENT_LENGTH);
+
+          replaced_char += QUOTE_REPLACEMENT_LENGTH - 1;
+        }
+        else
+        {
+          *replaced_char = *m;
+        }
+      }
+
+      *replaced_char = 0;
+    }
+
     //TODO escape ' symbols
     engine->Execute (format ("lua: OnHsConnectionMessage (%d, '%s')", plugin_id, message).c_str ());
   }
