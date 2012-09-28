5f1eee401eea3897c523a15a442b561ab5cd8c53
Ievgen Mukhin <muhineg@gmail.com>
2012-09-28 14:57:05 +0000

    Затухание сплеша.

--- components/demos/plarium/launcher/sources/platform/iphone/engine_launcher.mm
+++ components/demos/plarium/launcher/sources/platform/iphone/engine_launcher.mm
@@ -2,12 +2,110 @@
 #import <Foundation/NSBundle.h>
 #import <Foundation/NSFileManager.h>
 
+#import <UIKit/UIApplication.h>
+#import <UIKit/UIImage.h>
+#import <UIKit/UIImageView.h>
+#import <UIKit/UIViewController.h>
+#import <UIKit/UIScreen.h>
+#import <UIKit/UIWindow.h>
+
 #include <cstdio>
 
 #include <launcher/application.h>
 
 using namespace engine;
 using namespace plarium::launcher;
+
+@interface Startup : NSObject
+{
+  UIImageView* imageView;
+  NSTimer*     fadeOutTimer;
+}
+
+-(void)onStartup;
+
+@end
+
+@implementation Startup
+
+-(void)dealloc
+{
+  [imageView release];
+
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+
+  [super dealloc];
+}
+
+-(id)init
+{
+  self = [super init];
+
+  if (!self)
+    return nil;
+
+  return self;
+}
+
+-(void)onStartup
+{
+  UIWindow* keyWindow     = [UIApplication sharedApplication].keyWindow;
+  UIView*   keyWindowView = keyWindow.rootViewController.view;
+  UIView*   parentView;
+
+  NSString* imageName;
+
+  if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+  {
+    imageName  = @"Default_ipad-Landscape@2x.png";
+    parentView = keyWindowView;
+  }
+  else
+  {
+    imageName  = [UIScreen mainScreen].scale > 1 ? @"Default-568h@2x.png" : @"Default.png";
+    parentView = keyWindow;
+  }
+
+  CGRect keyWindowFrame = parentView.frame;
+
+  [imageView removeFromSuperview];
+  [imageView release];
+  imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:imageName]];
+
+  imageView.contentMode = UIViewContentModeScaleAspectFill;
+
+  keyWindowFrame.origin.x = 0;
+  keyWindowFrame.origin.y = 0;
+
+  imageView.frame = keyWindowFrame;
+
+  [parentView addSubview:imageView];
+
+  [fadeOutTimer invalidate];
+  [fadeOutTimer release];
+  fadeOutTimer = [[NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector (startFadeout) userInfo:nil repeats:NO] retain];
+}
+
+-(void)startFadeout
+{
+  [UIView beginAnimations:nil context:nil];
+  [UIView setAnimationDuration:0.5];
+
+  [UIView setAnimationDelegate:self];
+  [UIView setAnimationDidStopSelector:@selector(onFadedOut)];
+
+  imageView.alpha = 0;
+
+  [UIView commitAnimations];
+}
+
+-(void)onFadedOut
+{
+  [imageView removeFromSuperview];
+  [self release];
+}
+
+@end
 
 //точка входа
 int main (int argc, const char* argv [], const char* env [])
@@ -45,9 +143,18 @@ int main (int argc, const char* argv [], const char* env [])
       return 1;
 
   {
+    pool = [[NSAutoreleasePool alloc] init];
+
+    Startup* startup = [[Startup alloc] init];
+
+    [[NSNotificationCenter defaultCenter] addObserver:startup selector:@selector (onStartup) name:UIApplicationDidFinishLaunchingNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:startup selector:@selector (onStartup) name:UIApplicationWillChangeStatusBarOrientationNotification object:nil];
+
     Application application;
 
     application.Run (funner);
+
+    [pool release];
   }
 
   delete funner;
