3c4210352d1d1be5eead596bff23d91ddfc27735
Ievgen Mukhin <muhineg@gmail.com>
2012-10-09 21:40:44 +0000

    Биндинг типов форматов картинок.

--- components/engine/script_binds/sources/bind_media_image.cpp
+++ components/engine/script_binds/sources/bind_media_image.cpp
@@ -14,9 +14,26 @@ namespace media_image_script_bind
     Константы (имена библиотек)
 */
 
-const char* IMAGE_LIBRARY  = "Media.Image";
-const char* BINDER_NAME    = "MediaImage";
-const char* COMPONENT_NAME = "script.binds.MediaImage";
+const char* IMAGE_LIBRARY        = "Media.Image";
+const char* IMAGE_FORMAT_LIBRARY = "Media.ImageFormat";
+const char* BINDER_NAME          = "MediaImage";
+const char* COMPONENT_NAME       = "script.binds.MediaImage";
+
+void bind_image_format_library (Environment& environment)
+{
+  InvokerRegistry lib = environment.Library (IMAGE_FORMAT_LIBRARY);
+
+  lib.Register ("get_Default", make_const (PixelFormat_Default));
+  lib.Register ("get_RGB8",    make_const (PixelFormat_RGB8));
+  lib.Register ("get_RGB16",   make_const (PixelFormat_RGB16));
+  lib.Register ("get_BGR8",    make_const (PixelFormat_BGR8));
+  lib.Register ("get_RGBA8",   make_const (PixelFormat_RGBA8));
+  lib.Register ("get_RGBA16",  make_const (PixelFormat_RGBA16));
+  lib.Register ("get_BGRA8",   make_const (PixelFormat_BGRA8));
+  lib.Register ("get_L8",      make_const (PixelFormat_L8));
+  lib.Register ("get_A8",      make_const (PixelFormat_A8));
+  lib.Register ("get_LA8",     make_const (PixelFormat_LA8));
+}
 
 ///Функции
 Image create_image (size_t width, size_t height, size_t depth, PixelFormat format)
@@ -41,14 +58,14 @@ void fill_rgba (Image& image, math::vec4f color)
 
 size_t put_image (IStack& stack)
 {
-  Image  sender   = stack.GetVariant (1).cast<Image> ();
+  Image  sender   = stack.GetVariant (1).cast<Image&> ();
   size_t x        = stack.GetInteger (2);
   size_t y        = stack.GetInteger (3);
   size_t z        = stack.GetInteger (4);
   size_t width    = stack.GetInteger (5);
   size_t height   = stack.GetInteger (6);
   size_t depth    = stack.GetInteger (7);
-  Image  source   = stack.GetVariant (8).cast<Image> ();
+  Image  source   = stack.GetVariant (8).cast<Image&> ();
   size_t target_x = stack.GetInteger (9);
   size_t target_y = stack.GetInteger (10);
   size_t target_z = stack.GetInteger (11);
@@ -108,7 +125,8 @@ class Component
   private:
     static void Bind (Environment& environment)
     {
-      bind_image_library (environment);
+      bind_image_format_library (environment);
+      bind_image_library        (environment);
     }
 };
 
