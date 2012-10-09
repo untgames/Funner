29e7ec9bbe13ee8855fd7fea1626a47109c4f97d
Leny Kholodov <leny.kholodov@gmail.com>
2012-10-09 12:09:08 +0000

    media::Image::PutImage & media::Image::Fill added

--- components/media/image/sources/image/img_utils.cpp
+++ components/media/image/sources/image/img_utils.cpp
@@ -1,6 +1,136 @@
 #include "shared.h"
 
 using namespace common;
+using namespace media;
+
+#pragma pack (1) //for fill
+
+namespace
+{
+
+/*
+    Заливка
+*/
+
+struct color_rgb8   { unsigned char red, green, blue; };
+struct color_rgb16  { unsigned short red, green, blue; };
+struct color_bgr8   { unsigned char blue, green, red; };
+struct color_rgba8  { unsigned char red, green, blue, alpha; };
+struct color_rgba16 { unsigned short red, green, blue, alpha; };
+struct color_bgra8  { unsigned char blue, green, red, alpha; };
+struct color_l8     { unsigned char luminance; };
+struct color_a8     { unsigned char alpha; };
+struct color_la8    { unsigned char luminance, alpha; };
+
+template <class SrcT, class DstT> inline void set (const SrcT& src, DstT& dst) { dst = static_cast<SrcT> (src); }
+
+template <class Color> inline void         set_red   (unsigned int value, Color& pixel) { set (value, pixel.red); }
+template <class Color> inline void         set_green (unsigned int value, Color& pixel) { set (value, pixel.green); }
+template <class Color> inline void         set_blue  (unsigned int value, Color& pixel) { set (value, pixel.blue); }
+template <class Color> inline void         set_alpha (unsigned int value, Color& pixel) { set (value, pixel.alpha); }
+template <class Color> inline unsigned int get_red   (const Color& pixel)               { return pixel.red; }
+template <class Color> inline unsigned int get_green (const Color& pixel)               { return pixel.green; }
+template <class Color> inline unsigned int get_blue  (const Color& pixel)               { return pixel.blue; }
+template <class Color> inline unsigned int get_alpha (const Color& pixel)               { return pixel.alpha; }
+
+inline unsigned int get_alpha (const color_rgb8&)      { return 0; }
+inline unsigned int get_alpha (const color_rgb16&)     { return 0; }
+inline unsigned int get_alpha (const color_bgr8&)      { return 0; }
+inline unsigned int get_alpha (const color_l8&)        { return 0; }
+inline unsigned int get_red   (const color_a8&)        { return 0; }
+inline unsigned int get_green (const color_a8&)        { return 0; }
+inline unsigned int get_blue  (const color_a8&)        { return 0; }
+inline unsigned int get_red   (const color_l8& pixel)  { return pixel.luminance; }
+inline unsigned int get_green (const color_l8& pixel)  { return pixel.luminance; }
+inline unsigned int get_blue  (const color_l8& pixel)  { return pixel.luminance; }
+inline unsigned int get_red   (const color_la8& pixel) { return pixel.luminance; }
+inline unsigned int get_green (const color_la8& pixel) { return pixel.luminance; }
+inline unsigned int get_blue  (const color_la8& pixel) { return pixel.luminance; }
+
+inline void set_alpha (unsigned int, color_rgb8&) { }
+inline void set_alpha (unsigned int, color_rgb16&) { }
+inline void set_alpha (unsigned int, color_bgr8&) { }
+inline void set_alpha (unsigned int, color_l8&) { }
+inline void set_red   (unsigned int, color_a8&) { }
+inline void set_green (unsigned int, color_a8&) { }
+inline void set_blue  (unsigned int, color_a8&) { }
+inline void set_red   (unsigned int, color_l8&) { }
+inline void set_green (unsigned int, color_l8&) { }
+inline void set_blue  (unsigned int, color_l8&) { }
+inline void set_red   (unsigned int, color_la8&) { }
+inline void set_green (unsigned int, color_la8&) { }
+inline void set_blue  (unsigned int, color_la8&) { }
+
+template <class Color> inline unsigned char get_luminance (const Color& pixel) { return (unsigned char)(((unsigned int)get_red (pixel) + get_green (pixel) + get_blue (pixel)) / 3); }
+
+inline unsigned char get_luminance (const color_l8& pixel)  { return pixel.luminance; }
+inline unsigned char get_luminance (const color_la8& pixel) { return pixel.luminance; }
+
+template <class Color> inline void set_luminance (unsigned int, Color&) {}
+
+inline void set_luminance (unsigned int value, color_l8& pixel)  { set (value, pixel.luminance); }
+inline void set_luminance (unsigned int value, color_la8& pixel) { set (value, pixel.luminance); }
+
+template <class SrcT, class DstT>
+inline void copy (const SrcT& src, DstT& dst)
+{
+  set_luminance (get_luminance (src), dst);
+  set_red       (get_red (src), dst);
+  set_green     (get_green (src), dst);
+  set_blue      (get_blue (src), dst);
+  set_alpha     (get_alpha (src), dst);
+}
+
+template <class DstT, class SrcT>
+void fill (const SrcT& src_color, Image& image)
+{
+  size_t layer_size = image.Width () * image.Height ();
+
+  for (size_t z=0, depth=image.Depth (); z<depth; z++)
+  {
+    DstT* dst_color = reinterpret_cast<DstT*> (image.Bitmap (z));
+
+    for (size_t i=0; i<layer_size; i++, dst_color++)
+      copy (src_color, *dst_color);
+  }
+}
+
+template <class SrcT>
+void fill_dispatch (const void* source_color, Image& image)
+{
+  const SrcT& color = *reinterpret_cast<const SrcT*> (source_color);
+
+  switch (image.Format ())
+  {
+    case PixelFormat_RGB8:   fill<color_rgb8> (color, image); break;
+    case PixelFormat_RGB16:  fill<color_rgb16> (color, image); break;
+    case PixelFormat_BGR8:   fill<color_bgr8> (color, image); break;
+    case PixelFormat_RGBA8:  fill<color_rgba8> (color, image); break;
+    case PixelFormat_RGBA16: fill<color_rgba16> (color, image); break;
+    case PixelFormat_BGRA8:  fill<color_bgra8> (color, image); break;
+    case PixelFormat_L8:     fill<color_l8> (color, image); break;
+    case PixelFormat_A8:     fill<color_a8> (color, image); break;
+    case PixelFormat_LA8:    fill<color_la8> (color, image); break;
+  }
+}
+
+void fill_dispatch (PixelFormat source_format, const void* source_color, Image& image)
+{
+  switch (source_format)
+  {
+    case PixelFormat_RGB8:   fill_dispatch<color_rgb8> (source_color, image); break;
+    case PixelFormat_RGB16:  fill_dispatch<color_rgb16> (source_color, image); break;
+    case PixelFormat_BGR8:   fill_dispatch<color_bgr8> (source_color, image); break;
+    case PixelFormat_RGBA8:  fill_dispatch<color_rgba8> (source_color, image); break;
+    case PixelFormat_RGBA16: fill_dispatch<color_rgba16> (source_color, image); break;
+    case PixelFormat_BGRA8:  fill_dispatch<color_bgra8> (source_color, image); break;
+    case PixelFormat_L8:     fill_dispatch<color_l8> (source_color, image); break;
+    case PixelFormat_A8:     fill_dispatch<color_a8> (source_color, image); break;
+    case PixelFormat_LA8:    fill_dispatch<color_la8> (source_color, image); break;    
+  }
+}
+
+}
 
 /*
     Получение характеристик формата
@@ -130,6 +260,15 @@ void crop_by_alpha (const Image& image, size_t crop_alpha, size_t& crop_x, size_
   
   crop_width  = max_x - crop_x + 1;
   crop_height = max_y - crop_y + 1;
+}
+
+/*
+    Заливка картинки
+*/
+
+void fill (PixelFormat format, const void* data, Image& image)
+{
+  fill_dispatch (format, data, image);
 }
 
 }
