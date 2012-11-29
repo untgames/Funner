98e7b1bd0e9bc723d7f9b4536126ef6279e6aed6
Ievgen Mukhin <muhineg@gmail.com>
2012-11-29 14:29:39 +0000

    Доработка формирования url.

--- components/social/facebook/sources/shared/facebook_utils.cpp
+++ components/social/facebook/sources/shared/facebook_utils.cpp
@@ -1,5 +1,39 @@
 #include "shared.h"
 
+namespace
+{
+
+/* Portable character check (remember EBCDIC). Do not use isalnum() because
+   its behavior is altered by the current locale.
+   See http://tools.ietf.org/html/rfc3986#section-2.3
+*/
+
+bool is_unreserved (unsigned char in)
+{
+  switch (in) {
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
+    case 'a': case 'b': case 'c': case 'd': case 'e':
+    case 'f': case 'g': case 'h': case 'i': case 'j':
+    case 'k': case 'l': case 'm': case 'n': case 'o':
+    case 'p': case 'q': case 'r': case 's': case 't':
+    case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+    case 'A': case 'B': case 'C': case 'D': case 'E':
+    case 'F': case 'G': case 'H': case 'I': case 'J':
+    case 'K': case 'L': case 'M': case 'N': case 'O':
+    case 'P': case 'Q': case 'R': case 'S': case 'T':
+    case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
+    case '-': case '.': case '_': case '~': case ':': case '/':
+    case '?': case '&': case '=':
+      return true;
+    default:
+      break;
+  }
+  return false;
+}
+
+}
+
 namespace social
 {
 
@@ -51,7 +85,7 @@ stl::string percent_escape (const char* str)
   size_t result_length = 0;
 
   for (const unsigned char* current_symbol = (unsigned char*)str; *current_symbol; current_symbol++, result_length++)
-    if (*current_symbol > 127)
+    if (!is_unreserved (*current_symbol))
       result_length += 2;
 
   return_value.resize (result_length);
@@ -60,16 +94,14 @@ stl::string percent_escape (const char* str)
 
   for (const unsigned char* current_symbol = (unsigned char*)str; *current_symbol; current_symbol++)
   {
-    if (*current_symbol > 127)
+    if (is_unreserved (*current_symbol))
+      return_value [current_out_symbols++] = *current_symbol;
+    else
     {
       xtl::xsnprintf ((char*)return_value.data () + current_out_symbols, 4, "%%%02X", *current_symbol);
 
       current_out_symbols += 3;
     }
-    else if (*current_symbol == ' ')
-      return_value [current_out_symbols++] = '+';
-    else
-      return_value [current_out_symbols++] = *current_symbol;
   }
 
   return return_value;
