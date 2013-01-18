a50fb97f81abdd657ce5b2e12d42c515005deb50
Ievgen Mukhin <muhineg@gmail.com>
2013-01-18 11:42:24 +0000

    Исправление активации звука после входящего сброшенного звонка.

--- components/engine/subsystems/sources/iphone/audio_session.mm
+++ components/engine/subsystems/sources/iphone/audio_session.mm
@@ -1,6 +1,7 @@
 #include <stl/string>
 #include <stl/vector>
 
+#include <xtl/bind.h>
 #include <xtl/common_exceptions.h>
 #include <xtl/function.h>
 #include <xtl/intrusive_ptr.h>
@@ -8,7 +9,9 @@
 #include <xtl/shared_ptr.h>
 #include <xtl/string.h>
 
+#include <common/action_queue.h>
 #include <common/component.h>
+#include <common/log.h>
 #include <common/parser.h>
 #include <common/singleton.h>
 #include <common/strlib.h>
@@ -36,10 +39,54 @@ namespace
 
 const char* SUBSYSTEM_NAME = "IPhoneAudioSession";                   //имя подсистемы
 const char* COMPONENT_NAME = "engine.subsystems.IPhoneAudioSession"; //имя компонента
+const char* LOG_NAME       = COMPONENT_NAME;
 
-class IPhoneAudioSessionSubsystem;
+const common::ActionQueue::time_t REACTIVATE_DELAY = 0.25;
 
-void audio_session_interruption_listener (void* iphone_audio_session_subsystem, UInt32 interruption_state);
+/*
+   Подсистема
+*/
+
+class IPhoneAudioSessionSubsystem : public ISubsystem, public xtl::reference_counter
+{
+  public:
+/// Конструктор/деструктор
+    IPhoneAudioSessionSubsystem (common::ParseNode& node, SubsystemManager& in_subsystem_manager);
+    ~IPhoneAudioSessionSubsystem ();
+
+    //Остановка/запуск необходимых подсистем
+    void StopAffectedSubsystems ();
+    void StartAffectedSubsystems ();
+
+/// Подсчёт ссылок
+    void AddRef ()  { addref (this); }
+    void Release () { release (this); }
+
+  private:
+    IPhoneAudioSessionSubsystem (const IPhoneAudioSessionSubsystem&);             //no impl
+    IPhoneAudioSessionSubsystem& operator = (const IPhoneAudioSessionSubsystem&); //no impl
+
+  private:
+    struct AffectedSubsystemDesc
+    {
+      stl::string name_wildcard;
+      stl::string config_file;
+
+      AffectedSubsystemDesc (const char* in_name_wildcard, const char* in_config_file)
+        : name_wildcard (in_name_wildcard), config_file (in_config_file)
+        {}
+    };
+
+    typedef xtl::shared_ptr <AffectedSubsystemDesc> AffectedSubsystemDescPtr;
+    typedef stl::vector <AffectedSubsystemDescPtr>  AffectedSubsystemsDescsArray;
+
+  private:
+    SubsystemManager&            subsystem_manager;
+    AffectedSubsystemsDescsArray affected_subsystems;
+};
+
+
+void audio_session_interruption_listener (void*, UInt32 interruption_state);
 
 //получение имени ошибки Audio Services
 const char* get_audio_session_error_name (OSStatus error)
@@ -53,6 +100,10 @@ const char* get_audio_session_error_name (OSStatus error)
     case kAudioSessionUnsupportedPropertyError: return "The audio session property is not supported.";
     case kAudioSessionBadPropertySizeError:     return "The size of the audio session property data was not correct.";
     case kAudioSessionNotActiveError:           return "The operation failed because your application’s audio session was not active.";
+    case kAudioServicesNoHardwareError:         return "The audio operation failed because the device has no audio input available.";
+    case kAudioSessionNoCategorySet:            return "The audio operation failed because it requires the audio session to have an explicitly-set category, but none was set. To use a hardware codec you must explicitly initialize the audio session and explicitly set an audio session category.";
+    case kAudioSessionIncompatibleCategory:     return "The specified audio session category cannot be used for the attempted audio operation. For example, you attempted to play or record audio with the audio session category set to kAudioSessionCategory_AudioProcessing.";
+    case kAudioSessionUnspecifiedError:         return "An unspecified audio session error has occurred. This typically results from the audio system being in an inconsistent state.";
     default:                                    return "Unknown error";
   }
 }
@@ -63,7 +114,7 @@ void check_audio_session_error (OSStatus error_code, const char* source)
   if (error_code == kAudioSessionNoError)
     return;
 
-  throw xtl::format_operation_exception (source, "Audio session error. %s", get_audio_session_error_name (error_code));
+  throw xtl::format_operation_exception (source, "Audio session error. %s. Code %d.", get_audio_session_error_name (error_code), error_code);
 }
 
 //Класс инициализирующий аудиосессию и хранящий указатель на подсистему
@@ -72,7 +123,8 @@ class AudioSessionManager
   public:
     ///Конструктор
     AudioSessionManager ()
-      : active_subsystem (0)
+      : log (LOG_NAME)
+      , active_subsystem (0)
     {
       static const char* METHOD_NAME = "engine::AudioSessionManage::AudioSessionManager";
 
@@ -104,111 +156,112 @@ class AudioSessionManager
       active_subsystem = new_active_subsystem;
     }
 
+    ///Пытаться реактивировать аудио-сессию несколько раз, поскольку с первого раза может не получиться. Аудио-сессия может быть еще заблокирована другим приложением, несмотря на событие окончания прерывания
+    void ReactivateAudioSessionWithDelay ()
+    {
+      log.Printf ("Init reactivation");
+      reactivate_action = common::ActionQueue::PushAction (xtl::bind (&AudioSessionManager::ProcessAudioSessionInterruption, this, (size_t)kAudioSessionEndInterruption), common::ActionThread_Current, REACTIVATE_DELAY);
+    }
+
+    void ProcessAudioSessionInterruption (size_t interruption_state)
+    {
+      if (interruption_state == kAudioSessionBeginInterruption)
+      {
+        log.Printf ("Interruption begin");
+
+        reactivate_action.Cancel ();
+
+        if (active_subsystem)
+          active_subsystem->StopAffectedSubsystems ();
+
+        check_audio_session_error (AudioSessionSetActive (false), "engine::audio_session_interruption_listener");
+      }
+      else if (interruption_state == kAudioSessionEndInterruption)
+      {
+        log.Printf ("Interruption end");
+
+        try
+        {
+          check_audio_session_error (AudioSessionSetActive (true), "engine::audio_session_interruption_listener");
+        }
+        catch (xtl::exception& e)
+        {
+          log.Printf ("Exception at processing audio session state change: '%s'\n", e.what ());
+          ReactivateAudioSessionWithDelay ();
+          return;
+        }
+        catch (...)
+        {
+          log.Printf ("Unknown exception at processing audio session state change\n");
+          ReactivateAudioSessionWithDelay ();
+          return;
+        }
+
+        if (active_subsystem)
+          active_subsystem->StartAffectedSubsystems ();
+      }
+    }
+
   private:
+    common::Log                        log;
     IPhoneAudioSessionSubsystem*       active_subsystem;
     VideoPlayerFinishPlaybackListener* video_player_listener;
+    common::Action                     reactivate_action;
 };
 
 typedef common::Singleton <AudioSessionManager> AudioSessionManagerSingleton;
 
-/*
-   Подсистема рендера сцены
-*/
-
-class IPhoneAudioSessionSubsystem : public ISubsystem, public xtl::reference_counter
-{
-  public:
 /// Конструктор/деструктор
-    IPhoneAudioSessionSubsystem (common::ParseNode& node, SubsystemManager& in_subsystem_manager)
-      : subsystem_manager (in_subsystem_manager)
-    {
-      try
-      {
-        for (common::ParseNamesakeIterator iter = node.First ("InterruptionAffectedSubsystems"); iter; ++iter)
-        {
-          common::StringArray wildcards        = common::split (common::get<const char*> (*iter, "Wildcards"));
-          const char*         config_file_name = common::get<const char*> (*iter, "ConfigFile");
-
-          for (size_t i = 0, count = wildcards.Size (); i < count; i++)
-            affected_subsystems.push_back (AffectedSubsystemDescPtr (new AffectedSubsystemDesc (wildcards [i], config_file_name)));
-        }
-
-        AudioSessionManagerSingleton::Instance ()->SetActiveSubsystem (this);
-      }
-      catch (xtl::exception& exception)
-      {
-        exception.touch ("engine::IPhoneAudioSessionSubsystem::IPhoneAudioSessionSubsystem");
-        throw;
-      }
-    }
-
-    ~IPhoneAudioSessionSubsystem ()
-    {
-      AudioSessionManagerSingleton::Instance ()->SetActiveSubsystem (0);
-    }
-
-    //Остановка/запуск необходимых подсистем
-    void StopAffectedSubsystems ()
-    {
-      for (AffectedSubsystemsDescsArray::iterator iter = affected_subsystems.begin (), end = affected_subsystems.end (); iter != end; ++iter)
-        subsystem_manager.Remove ((*iter)->name_wildcard.c_str ());
-    }
-
-    void StartAffectedSubsystems ()
-    {
-      StopAffectedSubsystems ();  //для обработки ситуации, когда сообщение о восстановлении аудиосессии приходит два раза (воспроизведение видео)
-
-      for (AffectedSubsystemsDescsArray::reverse_iterator iter = affected_subsystems.rbegin (), end = affected_subsystems.rend (); iter != end; ++iter)
-        subsystem_manager.Start ((*iter)->config_file.c_str (), (*iter)->name_wildcard.c_str ());
-    }
-
-/// Подсчёт ссылок
-    void AddRef ()  { addref (this); }
-    void Release () { release (this); }
-
-  private:
-    IPhoneAudioSessionSubsystem (const IPhoneAudioSessionSubsystem&);             //no impl
-    IPhoneAudioSessionSubsystem& operator = (const IPhoneAudioSessionSubsystem&); //no impl
-
-  private:
-    struct AffectedSubsystemDesc
-    {
-      stl::string name_wildcard;
-      stl::string config_file;
-
-      AffectedSubsystemDesc (const char* in_name_wildcard, const char* in_config_file)
-        : name_wildcard (in_name_wildcard), config_file (in_config_file)
-        {}
-    };
-
-    typedef xtl::shared_ptr <AffectedSubsystemDesc> AffectedSubsystemDescPtr;
-    typedef stl::vector <AffectedSubsystemDescPtr>  AffectedSubsystemsDescsArray;
-
-  private:
-    SubsystemManager&            subsystem_manager;
-    AffectedSubsystemsDescsArray affected_subsystems;
-};
-
-//обработчик событий прерывания/восстановления аудиосессии
-void audio_session_interruption_listener (void* iphone_audio_session_subsystem, UInt32 interruption_state)
+IPhoneAudioSessionSubsystem::IPhoneAudioSessionSubsystem (common::ParseNode& node, SubsystemManager& in_subsystem_manager)
+  : subsystem_manager (in_subsystem_manager)
 {
   try
   {
-    IPhoneAudioSessionSubsystem* subsystem = AudioSessionManagerSingleton::Instance ()->ActiveSubsystem ();
-
-    if (!subsystem)
-      return;
-
-    if (interruption_state == kAudioSessionBeginInterruption)
+    for (common::ParseNamesakeIterator iter = node.First ("InterruptionAffectedSubsystems"); iter; ++iter)
     {
-      subsystem->StopAffectedSubsystems ();
-      check_audio_session_error (AudioSessionSetActive (false), "engine::audio_session_interruption_listener");
+      common::StringArray wildcards        = common::split (common::get<const char*> (*iter, "Wildcards"));
+      const char*         config_file_name = common::get<const char*> (*iter, "ConfigFile");
+
+      for (size_t i = 0, count = wildcards.Size (); i < count; i++)
+        affected_subsystems.push_back (AffectedSubsystemDescPtr (new AffectedSubsystemDesc (wildcards [i], config_file_name)));
     }
-    else if (interruption_state == kAudioSessionEndInterruption)
-    {
-      check_audio_session_error (AudioSessionSetActive (true), "engine::audio_session_interruption_listener");
-      subsystem->StartAffectedSubsystems ();
-    }
+
+    AudioSessionManagerSingleton::Instance ()->SetActiveSubsystem (this);
+  }
+  catch (xtl::exception& exception)
+  {
+    exception.touch ("engine::IPhoneAudioSessionSubsystem::IPhoneAudioSessionSubsystem");
+    throw;
+  }
+}
+
+IPhoneAudioSessionSubsystem::~IPhoneAudioSessionSubsystem ()
+{
+  AudioSessionManagerSingleton::Instance ()->SetActiveSubsystem (0);
+}
+
+//Остановка/запуск необходимых подсистем
+void IPhoneAudioSessionSubsystem::StopAffectedSubsystems ()
+{
+  for (AffectedSubsystemsDescsArray::iterator iter = affected_subsystems.begin (), end = affected_subsystems.end (); iter != end; ++iter)
+    subsystem_manager.Remove ((*iter)->name_wildcard.c_str ());
+}
+
+void IPhoneAudioSessionSubsystem::StartAffectedSubsystems ()
+{
+  StopAffectedSubsystems ();  //для обработки ситуации, когда сообщение о восстановлении аудиосессии приходит два раза (воспроизведение видео)
+
+  for (AffectedSubsystemsDescsArray::reverse_iterator iter = affected_subsystems.rbegin (), end = affected_subsystems.rend (); iter != end; ++iter)
+    subsystem_manager.Start ((*iter)->config_file.c_str (), (*iter)->name_wildcard.c_str ());
+}
+
+
+//обработчик событий прерывания/восстановления аудиосессии
+void audio_session_interruption_listener (void*, UInt32 interruption_state)
+{
+  try
+  {
+    AudioSessionManagerSingleton::Instance ()->ProcessAudioSessionInterruption (interruption_state);
   }
   catch (xtl::exception& e)
   {
