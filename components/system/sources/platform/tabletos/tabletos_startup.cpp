140f0c26a7fb0065a27acfd1bf353fef143fd67e
Leny Kholodov <leny.kholodov@gmail.com>
2013-02-04 16:22:31 +0000

    TabletOS main function with visibility hidden

--- components/system/sources/platform/tabletos/tabletos_startup.cpp
+++ components/system/sources/platform/tabletos/tabletos_startup.cpp
@@ -3,15 +3,27 @@
 extern "C"
 {
 
-__attribute__ ((visibility("default"))) extern int main (...);
+extern int main (...);
+
+__attribute__ ((visibility("default"))) extern void* tabletos_get_main_function ()
+{
+  return (void*)&main;
+}
 
 __attribute__ ((visibility("default"))) extern int tabletos_startup (void* library, int argc, char** argv, char** env)
 {
   try
   {    
+    typedef void* (*get_main_fn)();
+
+    get_main_fn get_main = (get_main_fn)dlsym (library, "tabletos_get_main_function");
+    
+    if (!get_main)
+      throw xtl::format_operation_exception ("", "tabletos_get_main_function function not found");      
+
     typedef int (*main_fn)(...);
     
-    main_fn main = (main_fn)dlsym (library, "main");
+    main_fn main = (main_fn)get_main ();
     
     if (!main)
       throw xtl::format_operation_exception ("", "main function not found");
