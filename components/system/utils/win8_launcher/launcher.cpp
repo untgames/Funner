e3e8b89d61626f477f1298f2e4b44217ab750b8a
Leny Kholodov <leny.kholodov@gmail.com>
2013-03-15 16:22:53 +0000

    Working on Win8 launcher

---
+++ components/system/utils/win8_launcher/launcher.cpp
@@ -0,0 +1,7 @@
+﻿#include "pch.h"
+
+[Platform::MTAThread]
+int main(Platform::Array<Platform::String^>^)	
+{
+	return 0;
+}
---
+++ components/system/utils/win8_launcher/pch.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+#include <wrl/client.h>
+#include <memory>
+
