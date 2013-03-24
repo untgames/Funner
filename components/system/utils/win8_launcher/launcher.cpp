1199fc75b6e1d22aee4d52109a931e6cd1a4c0cd
Leny Kholodov <leny.kholodov@gmail.com>
2013-03-24 22:40:03 +0000

    Working on WinRT launcher

--- components/system/utils/win8_launcher/launcher.cpp
+++ components/system/utils/win8_launcher/launcher.cpp
@@ -1,8 +1,27 @@
-﻿#include <memory>
+﻿#include <stdio.h>
+#include <wchar.h>
+#include <memory>
+#include <string>
+
 #include <wrl/client.h>
 
 [Platform::MTAThread]
 int main(Platform::Array<Platform::String^>^)	
 {
+  auto localAppDataParh = Windows::Storage::ApplicationData::Current->LocalFolder->Path;
+
+  std::wstring path( localAppDataParh->Data() );
+ 
+  path += L"\\mylog";
+
+  FILE* file = _wfopen (path.c_str (), L"wt");
+
+  if (file)
+  {
+    fprintf (file, "Hello world!!!!!\n");
+  }
+
+  fclose (file);
+
   return 0;
 }
