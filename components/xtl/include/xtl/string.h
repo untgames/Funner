f8cd5f54fc2f98b4e0b2c39856f55f19479ccc99
Leny Kholodov <leny.kholodov@gmail.com>
2013-04-02 11:36:02 +0000

    Formatting

--- components/xtl/include/xtl/string.h
+++ components/xtl/include/xtl/string.h
@@ -11,7 +11,7 @@ namespace xtl
 {
 
 /*
-    Сравнение строк
+    ╤Ёртэхэшх ёЄЁюъ
 */
 
 int xstrcmp   (const char* s1, const char* s2);
@@ -20,24 +20,24 @@ int xstrncmp  (const char* s1, const char* s2, size_t max_count);
 int xstrnicmp (const char* s1, const char* s2, size_t max_count);
 
 /*
-    Получение длины строки
+    ╧юыєўхэшх фышэ√ ёЄЁюъш
 */
 
 size_t xstrlen (const char* s);
 size_t xstrlen (const wchar_t* s);
 
 /*
-    Копирование строк
+    ╩юяшЁютрэшх ёЄЁюъ
 */
 
 char*    xstrncpy (char* dst, const char* src, int length);
 wchar_t* xstrncpy (wchar_t* dst, const wchar_t* src, int length);
 
 /*
-    Печать в строку:
-      - если размер буфера недостаточен для печати строки возвращается -1;
-      - count - максимальное количество записываемых символов включая '\0';
-      - если buffer == NULL && count == 0, то возвращается размер форматирования.
+    ╧хўрЄ№ т ёЄЁюъє:
+      - хёыш ЁрчьхЁ сєЇхЁр эхфюёЄрЄюўхэ фы  яхўрЄш ёЄЁюъш тючтЁр∙рхЄё  -1;
+      - count - ьръёшьры№эюх ъюышўхёЄтю чряшё√трхь√ї ёшьтюыют тъы■ўр  '\0';
+      - хёыш buffer == NULL && count == 0, Єю тючтЁр∙рхЄё  ЁрчьхЁ ЇюЁьрЄшЁютрэш .
 */
 
 int xsnprintf  (char* buffer, size_t count, const char* format, ...);
