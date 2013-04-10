23d29be9dddc0514265b0e83bc36c06b6ce6ca2c
Leny Kholodov <leny.kholodov@gmail.com>
2013-04-10 12:35:45 +0000

    Working on DX11

---
+++ components/render/dx11_driver/sources/common/dx11_device_manager.cpp
@@ -0,0 +1,66 @@
+#include "shared.h"
+
+using namespace render::low_level;
+using namespace render::low_level::dx11;
+
+/*
+    DeviceManager
+*/
+
+struct DeviceManager::Impl: public xtl::reference_counter
+{
+  DxDevicePtr device;
+
+  Impl (const DxDevicePtr& in_device) : device (in_device) 
+  {
+    if (!device)
+      throw xtl::make_null_argument_exception ("render::low_level::dx11::DeviceManager::Impl::Impl", "device");
+  }
+};
+
+DeviceManager::DeviceManager (const DxDevicePtr& device)
+  : impl (new Impl (device))
+{
+}
+
+DeviceManager::DeviceManager (const DeviceManager& manager)
+  : impl (manager.impl)
+{
+  addref (impl);
+}
+
+DeviceManager::~DeviceManager ()
+{
+  release (impl);
+}
+
+DeviceManager& DeviceManager::operator = (const DeviceManager& manager)
+{
+  DeviceManager (manager).Swap (*this);
+
+  return *this;
+}
+
+IDXGIDevice& DeviceManager::GetDevice () const
+{
+  return *impl->device;
+}
+
+void DeviceManager::Swap (DeviceManager& manager)
+{
+  stl::swap (impl, manager.impl);
+}
+
+/*
+    DeviceObject
+*/
+
+DeviceObject::DeviceObject (const DeviceManager& in_manager)
+  : manager (in_manager)
+{
+}
+
+IDXGIDevice& DeviceObject::GetDevice () const
+{
+  return manager.GetDevice ();
+}
