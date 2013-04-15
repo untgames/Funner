93010ff0c8dcadcebafc10e81124f690efcdd59c
Leny Kholodov <leny.kholodov@gmail.com>
2013-04-15 18:06:42 +0000

    Working on DX11

---
+++ components/render/dx11_driver/sources/shader_manager/dx11_shader_code.cpp
@@ -0,0 +1,241 @@
+#include "shared.h"
+
+using namespace render::low_level;
+using namespace render::low_level::dx11;
+
+typedef xtl::com_ptr<ID3D10Blob> DxBlobPtr;
+
+/*
+    Константы
+*/
+
+const size_t RESERVED_MACRO_SIZE = 16;
+
+/*
+    Описание реализации кода шейдера
+*/
+
+struct ShaderCode::Impl
+{
+  DxBlobPtr data; //буфера данных шейдера
+  size_t    hash; //хэш
+};
+
+/*
+    Конструктор / деструктор
+*/
+
+namespace
+{
+
+typedef stl::vector<D3D10_SHADER_MACRO> MacroArray;
+
+/// Список макросов
+struct ShaderMacroMapper
+{
+  common::StringArray strings;
+  MacroArray          macroses;
+
+  ShaderMacroMapper ()
+  {
+    strings.Reserve (RESERVED_MACRO_SIZE * 2);
+  }
+
+  void operator () (const char* name, const char* value)
+  {
+    size_t index = strings.Size ();
+
+    strings.Add (name);
+    strings.Add (value);
+  }
+
+  D3D10_SHADER_MACRO* Build ()
+  {
+    macroses.reserve (strings.Size () / 2 + 1); 
+
+    if (strings.Size () >= 2)
+    {
+      for (size_t i=0, count=strings.Size (); i<count-1; i++)
+      {
+        size_t index = i * 2;
+
+        D3D10_SHADER_MACRO macro;
+
+        macro.Name       = strings [index];
+        macro.Definition = strings [index + 1];
+
+        macroses.push_back (macro);
+      }
+    }
+
+    D3D10_SHADER_MACRO macro;    
+
+    memset (&macro, 0, sizeof (macro));
+
+    macroses.push_back (macro);
+
+    return &macroses [0];
+  }
+};
+
+/// Интерфейс подключаемых модулей
+class IncludeManager: public ID3D10Include
+{
+  public:
+    HRESULT __stdcall Open (D3D_INCLUDE_TYPE include_type, LPCSTR file_name, LPCVOID parent_data, LPCVOID* data, UINT* data_size)
+    {
+      try
+      {
+        if (!file_name)
+          throw xtl::make_null_argument_exception ("", "file_name");
+
+        if (!data)
+          throw xtl::make_null_argument_exception ("", "data");
+
+        if (!data_size)
+          throw xtl::make_null_argument_exception ("", "data_size");
+
+        switch (include_type)
+        {
+          case D3D_INCLUDE_LOCAL: // #include "FILE"
+          case D3D_INCLUDE_SYSTEM: // #include <FILE>
+            break;
+          default:
+            throw xtl::make_argument_exception ("", "include_type", include_type);
+        }
+
+        common::InputFile file (file_name, common::FileMode_ReadOnly);
+
+        size_t file_size = file.Size ();
+
+        char* buffer = new char [file_size + 1];
+
+        size_t read_size = file.Read (buffer, file_size);
+
+        buffer [file_size] = 0;
+
+        *data      = buffer;
+        *data_size = file_size;
+
+        return S_OK;
+      }
+      catch (std::exception& e)
+      {
+        Log ().Printf ("%s\n    at render::low_level::dx11::IncludeManager::Open", e.what ());
+      }
+      catch (...)
+      {
+        Log ().Printf ("unknown exception\n    at render::low_level::dx11::IncludeManager::Open");
+      }
+
+      return E_FAIL;
+    }
+    
+    HRESULT __stdcall Close (LPCVOID data)
+    {
+      delete [] (char*)data;
+
+      return S_OK;
+    }
+};
+
+}
+
+ShaderCode::ShaderCode 
+ (const DeviceManager& device_manager,
+  const char*          name, 
+  const char*          profile,
+  const char*          source_code,
+  size_t               source_code_length,
+  const char*          options,
+  size_t               flags1,
+  size_t               flags2)
+   : DeviceObject (device_manager)
+   , impl (new Impl)
+{
+  try
+  {
+      //проверка корректности аргументов
+
+    if (!name)
+      throw xtl::make_null_argument_exception ("", "name");
+
+    if (!profile)
+      throw xtl::make_null_argument_exception ("", "profile");
+
+    if (!source_code)
+      throw xtl::make_null_argument_exception ("", "source_code");
+
+    if (source_code_length == (size_t)-1)
+      source_code_length = strlen (source_code);
+
+      //построение списка макросов
+
+    if (!options)
+      options = "";      
+
+    ShaderMacroMapper macro_mapper;
+
+    common::parse_init_string (options, xtl::ref (macro_mapper));
+
+      //подготовка интерфейса получения подключаемых модулей
+
+    IncludeManager include_manager;
+
+      //компиляция шейдера
+
+    ID3D10Blob *dx_shader_blob = 0, *dx_error_msg_blob = 0;
+
+    check_errors ("D3DX11CompileFromMemory", D3DX11CompileFromMemory (source_code, source_code_length, name, macro_mapper.Build (), &include_manager, 0,
+      profile, flags1, flags2, 0, &dx_shader_blob, &dx_error_msg_blob, 0));
+
+    if (!dx_shader_blob)
+      throw xtl::format_operation_exception ("", "D3DX11CompileFromMemory failed");
+    
+    if (dx_error_msg_blob)
+    {
+      const char* err_data = (const char*)dx_error_msg_blob->GetBufferPointer ();
+      size_t      err_size = dx_error_msg_blob->GetBufferSize ();
+
+      if (err_data && err_size)
+        Log ().Printf ("Shader '%s' compilation errors:\n%s", name, stl::string (err_data, err_size).c_str ());
+
+      dx_error_msg_blob->Release ();
+    }
+
+    impl->data = DxBlobPtr (dx_shader_blob, false);
+    impl->hash = common::crc32 (impl->data->GetBufferPointer (), impl->data->GetBufferSize ());
+  }
+  catch (xtl::exception& e)
+  {
+    e.touch ("render::low_level::dx11::ShaderCode::ShaderCode");
+    throw;
+  }
+}
+
+ShaderCode::~ShaderCode ()
+{
+}
+
+/*
+    Получение скомпилированного шейдера
+*/
+
+const void* ShaderCode::GetCompiledData () const
+{
+  return impl->data->GetBufferPointer ();
+}
+
+size_t ShaderCode::GetCompiledDataSize () const
+{
+  return impl->data->GetBufferSize ();
+}
+
+/*
+    Хэш данных
+*/
+
+size_t ShaderCode::GetCompiledDataHash () const
+{
+  return impl->hash;
+}
---
+++ components/render/dx11_driver/sources/shader_manager/shared.h
@@ -0,0 +1,36 @@
+#ifndef RENDER_DX11_DRIVER_SHADER_STAGE_SHARED_HEADER
+#define RENDER_DX11_DRIVER_SHADER_STAGE_SHARED_HEADER
+
+#include <D3Dcommon.h>
+#include <D3DX11async.h>
+
+#include <stl/vector>
+
+#include <xtl/function.h>
+#include <xtl/ref.h>
+
+#include <common/file.h>
+#include <common/string.h>
+#include <common/strlib.h>
+
+#include <shared/error.h>
+#include <shared/log.h>
+#include <shared/shader.h>
+
+namespace render
+{
+
+namespace low_level
+{
+
+namespace dx11
+{
+
+
+}
+
+}
+
+}
+
+#endif
