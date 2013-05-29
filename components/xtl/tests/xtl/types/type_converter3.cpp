00c62749fc344fcba5e19255f6b9ac9a8e07e3b2
Leny Kholodov <leny.kholodov@gmail.com>
2013-05-29 08:58:11 +0000

    Working on custom_cast

--- components/xtl/tests/xtl/types/type_converter3.cpp
+++ components/xtl/tests/xtl/types/type_converter3.cpp
@@ -10,32 +10,47 @@ struct C : public A {};
 
 template class declcast<B, A>;
 template class declcast<A, B, dynamic_caster>;
-template class declcast<B, C, dynamic_caster>;
+template class declcast<A, C, dynamic_caster>;
 
 int main ()
 {
   printf ("Results of type_converter3_test:\n");
 
-  B b;
-  
-  custom_ref_caster caster = make_custom_ref_caster<A> (b);
-  
-  #define TEST(TO) printf ("check convertion A -> %s: %s\n", #TO, caster.has_cast<TO> () ? "true" : "false");
+  try
+  {
+    B b;
+    
+    custom_ref_caster caster = make_custom_ref_caster<A> (b);
+    
+    #define TEST(TO) printf ("check convertion A -> %s: %s\n", #TO, caster.has_cast<TO> () ? "true" : "false");
 
-  TEST (B&);
-  TEST (B);
-  TEST (B*);
-  TEST (A);
-  TEST (A&);
-  TEST (A*);
+    TEST (B&);
+    TEST (B);
+    TEST (B*);
+    TEST (A);
+    TEST (A&);
+    TEST (A*);
 
-  A* a = caster.cast<A*> ();
+    A* a = caster.cast<A*> ();
 
-  printf ("a == &b: %s\n", a == &b ? "true" : "false");
+    printf ("a == &b: %s\n", a == &b ? "true" : "false");
 
-  C* c = caster.cast<C*> ();
+    B* b1 = caster.cast<B*> ();
 
-  printf ("c == &b: %s\n", a == &b ? "true" : "false");
+    printf ("b1 == &b: %s\n", b1 == &b ? "true" : "false");
+
+    C* c = caster.cast<C*> ();
+
+    printf ("c == &b: %s\n", static_cast<A*> (c) == &b ? "true" : "false");
+  }
+  catch (std::exception& e)
+  {
+    printf ("%s\n", e.what ());
+  }
+  catch (...)
+  {
+    printf ("unknown exception\n");
+  }
   
   return 0;
 }
