856e72208a13228e50b4969be80fbe3231d35e18
Leny Kholodov <leny.kholodov@gmail.com>
2013-06-17 15:44:29 +0000

    Unit tests have been passed

--- components/engine/script_binds/tests/lua/data/common_async.lua
+++ components/engine/script_binds/tests/lua/data/common_async.lua
@@ -10,7 +10,7 @@ function test_async_post()
   
   local callback = Common.File.CreateAsyncLoadHandler (OnLoad)
 
-  local FILE_NAME = "http://www.google.com"       
+  local FILE_NAME = "http://www.untgames.com"       
   local result    = Common.File.AsyncPostString (FILE_NAME, "post_string", callback)
 
   print ("Test completed")
