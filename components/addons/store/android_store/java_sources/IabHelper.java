1572003645af820691abf3ad8b00c08d1a043d42
Ievgen Mukhin <muhineg@gmail.com>
2013-08-22 15:29:43 +0000

    Fixed android store disposing on devices without playmarket.

--- components/addons/store/android_store/java_sources/IabHelper.java
+++ components/addons/store/android_store/java_sources/IabHelper.java
@@ -183,64 +183,66 @@ public class IabHelper {
 
         // Connection to IAB service
         logDebug("Starting in-app billing setup.");
-        mServiceConn = new ServiceConnection() {
-            @Override
-            public void onServiceDisconnected(ComponentName name) {
-                logDebug("Billing service disconnected.");
-                mService = null;
-            }
-
-            @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-                logDebug("Billing service connected.");
-                mService = IInAppBillingService.Stub.asInterface(service);
-                String packageName = mContext.getPackageName();
-                try {
-                    logDebug("Checking for in-app billing 3 support.");
-                    
-                    // check for in-app billing v3 support
-                    int response = mService.isBillingSupported(3, packageName, ITEM_TYPE_INAPP);
-                    if (response != BILLING_RESPONSE_RESULT_OK) {
-                        if (listener != null) listener.onIabSetupFinished(new IabResult(response,
-                                "Error checking for billing v3 support."));
-                        
-                        // if in-app purchases aren't supported, neither are subscriptions.
-                        mSubscriptionsSupported = false;
-                        return;
-                    }
-                    logDebug("In-app billing version 3 supported for " + packageName);
-                    
-                    // check for v3 subscriptions support
-                    response = mService.isBillingSupported(3, packageName, ITEM_TYPE_SUBS);
-                    if (response == BILLING_RESPONSE_RESULT_OK) {
-                        logDebug("Subscriptions AVAILABLE.");
-                        mSubscriptionsSupported = true;
-                    }
-                    else {
-                        logDebug("Subscriptions NOT AVAILABLE. Response: " + response);
-                    }
-                    
-                    mSetupDone = true;
-                }
-                catch (RemoteException e) {
-                    if (listener != null) {
-                        listener.onIabSetupFinished(new IabResult(IABHELPER_REMOTE_EXCEPTION,
-                                                    "RemoteException while setting up in-app billing."));
-                    }
-                    e.printStackTrace();
-                    return;
-                }
-
-                if (listener != null) {
-                    listener.onIabSetupFinished(new IabResult(BILLING_RESPONSE_RESULT_OK, "Setup successful."));
-                }
-            }
-        };
         
         Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
+
         if (!mContext.getPackageManager().queryIntentServices(serviceIntent, 0).isEmpty()) {
-            // service available to handle that Intent
-            mContext.bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
+        	mServiceConn = new ServiceConnection() {
+        		@Override
+        		public void onServiceDisconnected(ComponentName name) {
+        			logDebug("Billing service disconnected.");
+        			mService = null;
+        		}
+
+        		@Override
+        		public void onServiceConnected(ComponentName name, IBinder service) {
+        			logDebug("Billing service connected.");
+        			mService = IInAppBillingService.Stub.asInterface(service);
+        			String packageName = mContext.getPackageName();
+        			try {
+        				logDebug("Checking for in-app billing 3 support.");
+
+        				// check for in-app billing v3 support
+        				int response = mService.isBillingSupported(3, packageName, ITEM_TYPE_INAPP);
+        				if (response != BILLING_RESPONSE_RESULT_OK) {
+        					if (listener != null) listener.onIabSetupFinished(new IabResult(response,
+        							"Error checking for billing v3 support."));
+
+        					// if in-app purchases aren't supported, neither are subscriptions.
+        					mSubscriptionsSupported = false;
+        					return;
+        				}
+        				logDebug("In-app billing version 3 supported for " + packageName);
+
+        				// check for v3 subscriptions support
+        				response = mService.isBillingSupported(3, packageName, ITEM_TYPE_SUBS);
+        				if (response == BILLING_RESPONSE_RESULT_OK) {
+        					logDebug("Subscriptions AVAILABLE.");
+        					mSubscriptionsSupported = true;
+        				}
+        				else {
+        					logDebug("Subscriptions NOT AVAILABLE. Response: " + response);
+        				}
+
+        				mSetupDone = true;
+        			}
+        			catch (RemoteException e) {
+        				if (listener != null) {
+        					listener.onIabSetupFinished(new IabResult(IABHELPER_REMOTE_EXCEPTION,
+        							"RemoteException while setting up in-app billing."));
+        				}
+        				e.printStackTrace();
+        				return;
+        			}
+
+        			if (listener != null) {
+        				listener.onIabSetupFinished(new IabResult(BILLING_RESPONSE_RESULT_OK, "Setup successful."));
+        			}
+        		}
+        	};
+
+          // service available to handle that Intent
+          mContext.bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
         }
         else {
             // no service available to handle that Intent
