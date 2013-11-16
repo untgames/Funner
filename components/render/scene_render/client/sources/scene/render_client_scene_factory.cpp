17b173970b73eadbddf7b2d3764b79005d173d2e
Leny Kholodov <leny.kholodov@gmail.com>
2013-11-16 14:16:20 +0000

    Working on scene render

--- components/render/scene_render/client/sources/scene/render_client_scene_factory.cpp
+++ components/render/scene_render/client/sources/scene/render_client_scene_factory.cpp
@@ -8,7 +8,7 @@ Node* SceneFactory::Create (scene_graph::Node& src_node, SceneManager& scene_man
   if (scene_graph::Scissor* node = dynamic_cast<scene_graph::Scissor*> (&src_node))
     return new Node (src_node, scene_manager, interchange::NodeType_Scissor); 
 
-  if (scene_graph::Node* node = dynamic_cast<scene_graph::Node*> (&src_node))
+  if (scene_graph::Node* node = dynamic_cast<scene_graph::Node*> (&src_node)) //don't need this!!!!
     return new Node (src_node, scene_manager);
 
   return 0;
