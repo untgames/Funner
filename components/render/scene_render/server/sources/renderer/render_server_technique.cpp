6ec04e26179cbddad1cf978eb8b88713106820a1
Leny Kholodov <leny.kholodov@gmail.com>
2013-11-27 12:44:52 +0000

    Working on technique

--- components/render/scene_render/server/sources/renderer/render_server_technique.cpp
+++ components/render/scene_render/server/sources/renderer/render_server_technique.cpp
@@ -12,7 +12,9 @@ struct Technique::Impl
   stl::string                                           name;                            //имя техники
   common::PropertyMap                                   properties;                      //свойства
   common::PropertyMap                                   default_properties;              //свойства по умолчанию
+  common::PropertyMap                                   binded_properties;               //значения свойств после связывания
   common::PropertyBindingMap                            property_bindings;               //связывание методом техники со свойствами
+  stl::auto_ptr<common::PropertyBindingMapSynchronizer> binded_properties_synchronizer;  //синхронизатор свойств после связывания
   stl::auto_ptr<common::PropertyBindingMapSynchronizer> default_properties_synchronizer; //синхронизатор свойств по умолчанию
   stl::auto_ptr<common::PropertyBindingMapSynchronizer> properties_synchronizer;         //синхронизатор свойств
   bool                                                  need_update_properties;          //флаг необходимости обновления свойств
@@ -69,7 +71,7 @@ const char* Technique::Name () const
 
 void Technique::SetDefaultProperties (const common::PropertyMap& properties)
 {
-  impl->default_properties     = properties;    
+  impl->default_properties     = properties.Clone ();
   impl->need_update_properties = true;
 
   impl->default_properties_synchronizer.reset ();
@@ -79,6 +81,8 @@ void Technique::SetDefaultProperties (const common::ParseNode& node)
 {
   try
   {
+    //TODO: cache for node -> property map
+
     SetDefaultProperties (to_properties (node));
   }
   catch (xtl::exception& e)
@@ -145,6 +149,10 @@ void Technique::UpdateFrame (RenderingContext& context, TechniquePrivateData& pr
       {        
         BindProperties (impl->property_bindings);
 
+        impl->binded_properties_synchronizer.reset (new common::PropertyBindingMapSynchronizer (impl->property_bindings, impl->binded_properties));
+        
+        impl->binded_properties_synchronizer->CopyToPropertyMap ();
+
         impl->need_bind_properties = false;
       }
 
@@ -160,7 +168,16 @@ void Technique::UpdateFrame (RenderingContext& context, TechniquePrivateData& pr
         //обновление свойств
 
       if (!impl->properties_synchronizer)
+      {
+          //применение свойств по умолчанию
+
+        impl->binded_properties_synchronizer->CopyFromPropertyMap ();
+        impl->default_properties_synchronizer->CopyFromPropertyMap ();
+
+          //созданеи синхронизатора
+
         impl->properties_synchronizer.reset (new common::PropertyBindingMapSynchronizer (impl->property_bindings, impl->properties));
+      }
 
       impl->properties_synchronizer->CopyFromPropertyMap ();
 
