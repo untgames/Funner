7e3cd6d47bd320d0cb56b0337da5e49dc85d756f
Leny Kholodov <leny.kholodov@gmail.com>
2014-01-11 12:50:15 +0000

    Working on dynamic primitives

--- components/render/manager/sources/shared.h
+++ components/render/manager/sources/shared.h
@@ -212,6 +212,7 @@ class DebugIdHolder: public xtl::noncopyable
 #include "cache_map.h"
 #include "dynamic_primitive.h"
 #include "dynamic_primitive_buffer.h"
+#include "dynamic_primitive_list.h"
 #include "dynamic_texture.h"
 #include "effect.h"
 #include "effect_loader_library.h"
