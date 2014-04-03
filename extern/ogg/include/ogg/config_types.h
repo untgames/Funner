8f87f7e311e482bdd35ec1276aa5b978290ed79e
Leny Kholodov <leny.kholodov@gmail.com>
2014-04-03 14:33:42 +0000

    Emscripten support

--- extern/ogg/include/ogg/config_types.h
+++ extern/ogg/include/ogg/config_types.h
@@ -47,6 +47,14 @@ typedef int            ogg_int32_t;
 typedef unsigned int   ogg_uint32_t;
 typedef long long      ogg_int64_t;
 
+#elif defined (EMSCRIPTEN)
+
+typedef short          ogg_int16_t;
+typedef unsigned short ogg_uint16_t;
+typedef int            ogg_int32_t;
+typedef unsigned int   ogg_uint32_t;
+typedef long long      ogg_int64_t;
+
 #else
 
 #error Unknown compiler
