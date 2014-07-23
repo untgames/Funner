d5724b4d66db6ac6a5479ae776f098f077c689fe
Ievgen Mukhin <muhineg@gmail.com>
2014-07-23 11:47:26 +0300

    Special logic for loading fonts from Luminance8 to RGBA8.

--- components/media/font/sources/xfont/fnt_xfont_font_rasterizer.cpp
+++ components/media/font/sources/xfont/fnt_xfont_font_rasterizer.cpp
@@ -122,10 +122,30 @@ void XFontFontRasterizer::BuildImage (size_t image_index, media::Image& out_imag
       out_image.Swap (pot_image);
     }
 
-    if (out_image.Format () != impl->params.image_format && impl->params.image_format != media::PixelFormat_Default)
+    media::PixelFormat original_format = out_image.Format ();
+
+    if (original_format != impl->params.image_format && impl->params.image_format != media::PixelFormat_Default)
     {
       common::Log (LOG_NAME).Printf ("Requested pixel format '%s' requires image convertation from '%s'", get_format_name (impl->params.image_format), get_format_name (out_image.Format ()));
       out_image.Convert (impl->params.image_format);
+
+      if (original_format == PixelFormat_L8 && impl->params.image_format == PixelFormat_RGBA8) //special case which requires additional processing
+      {
+        unsigned char* current_pixel = (unsigned char*)out_image.Bitmap ();
+
+        for (size_t i = 0, pixels_count = image_width * image_height; i < pixels_count; current_pixel += 4, i++)
+        {
+          if (*current_pixel)
+          {
+            current_pixel [3] = *current_pixel;
+            current_pixel [0] = 255;
+            current_pixel [1] = 255;
+            current_pixel [2] = 255;
+          }
+          else
+            current_pixel [3] = 0;
+        }
+      }
     }
   }
   catch (xtl::exception& e)
