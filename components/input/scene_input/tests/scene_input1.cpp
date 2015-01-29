3549a5427380770d94915c9b4e2caf48509ad8cb
Ievgen Mukhin <muhineg@gmail.com>
2015-01-29 18:07:18 +0200

    Test update to check clicks behind viewports.

--- components/input/scene_input/tests/scene_input1.cpp
+++ components/input/scene_input/tests/scene_input1.cpp
@@ -1,10 +1,20 @@
 #include "shared.h"
 
+const float EPS = 0.0001f;
+
 void input_notify (InputZoneModel&, const Viewport&, const InputZoneNotification notification, const InputZoneNotificationContext& context)
 {
+  InputZoneNotificationContext context_copy = context;
+
+  if (fabs (context_copy.touch_local_position.x) < EPS)
+    context_copy.touch_local_position.x = 0.f;
+
+  if (fabs (context_copy.touch_local_position.y) < EPS)
+    context_copy.touch_local_position.y = 0.f;
+
   printf ("%s: touch=%d, button=%d, world_position=[%.3f, %.3f, %.3f] local_position=[%.3f, %.3f]\n", get_name (notification), 
     context.touch_id, context.button, context.touch_world_position.x, context.touch_world_position.y, context.touch_world_position.z,
-    context.touch_local_position.x, context.touch_local_position.y);
+    context_copy.touch_local_position.x, context_copy.touch_local_position.y);
 }
 
 int main ()
@@ -33,7 +43,7 @@ int main ()
     zone->SetName ("zone1");
     
     zone->Translate (50.0f, -50.0f, 0.0f);
-    zone->SetScale (10.0f, 10.0f, 1.0f);
+    zone->SetScale (1000.0f, 1000.0f, 1.0f);
     
     zone->RegisterNotificationHandler (&input_notify);
     
@@ -56,6 +66,9 @@ int main ()
 
     manager.ProcessEvent ("CursorX axis 0");
     manager.ProcessEvent ("CursorY axis 0");
+    manager.ProcessEvent ("Mouse0 down");
+    manager.ProcessEvent ("Mouse0 up");
+    manager.ProcessEvent ("CursorX axis 0.9");
     manager.ProcessEvent ("Mouse0 down");
     manager.ProcessEvent ("Mouse0 up");
   }
