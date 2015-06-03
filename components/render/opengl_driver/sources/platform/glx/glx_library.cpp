c305bf7ffb23997cab18be81e313cea7ff4d696a
Leny Kholodov <leny.kholodov@gmail.com>
2015-06-03 16:10:52 +0300

    Build on Linux

--- components/render/opengl_driver/sources/platform/glx/glx_library.cpp
+++ components/render/opengl_driver/sources/platform/glx/glx_library.cpp
@@ -493,7 +493,7 @@ void* AdapterLibrary::GetProcAddress (const char* name, size_t search_flags)
     throw xtl::make_null_argument_exception (METHOD_NAME, "name");
   }
 
-  const void* address = 0;
+  void* address = 0;
 
   if (!address && (search_flags & EntrySearch_Context))
     address = (void*)impl->fglXGetProcAddress (name);
@@ -502,7 +502,7 @@ void* AdapterLibrary::GetProcAddress (const char* name, size_t search_flags)
     return address;
     
   if (search_flags & EntrySearch_Library)
-    address = impl->dll->GetSymbol (name);
+    address = (void*)impl->dll->GetSymbol (name);
 
   if (address)
     return address;
