3859b6e3dde8e2f569a999f7abf28a977ea44b39
Sergey Tsatsulin <tsatsulin@gmail.com>
2017-04-18 18:38:23 +0300

    Fix typo

--- extern/libiconv/tests/iconv01.sh
+++ extern/libiconv/tests/iconv01.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo Resutls of iconv01_test:
+echo Results of iconv01_test:
 for name in CP1255 CP1258 ISO-2022-CN-EXT ISO-2022-CN ISO-2022-JP-1 ISO-2022-JP-2 ISO-2022-JP ISO-2022-KR UTF-16BE UTF-16LE UTF-32 UTF-32BE UTF-32LE UTF-7
 do
   printf "Test $name... "
